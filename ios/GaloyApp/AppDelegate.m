f974fa9ed0be4ff4c12cccb95f460994b518cd41
Nicolas Burtey <nicolas.burtey@gmail.com>
2019-09-19 11:55:56 -0700

    updating ios environment. adding firebase
    finish installation of firebase?

--- ios/GaloyApp/AppDelegate.m
+++ ios/GaloyApp/AppDelegate.m
@@ -11,10 +11,16 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+@import Firebase;
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  if ([FIRApp defaultApp] == nil) {
+    [FIRApp configure];
+  }
+
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"GaloyApp"
