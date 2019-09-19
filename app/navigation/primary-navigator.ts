205c14ba951910144bb442b59a9256ba13d7832f
Nicolas Burtey <nicolas.burtey@gmail.com>
2019-09-19 14:00:31 -0700

    first test using firebase auth

--- app/navigation/primary-navigator.ts
+++ app/navigation/primary-navigator.ts
@@ -4,9 +4,11 @@ import { DemoScreen } from "../screens/demo-screen"
 import { AccountsScreen } from "../screens/accounts-screen"
 import { AccountDetailScreen } from "../screens/account-detail-screen/account-detail-screen"
 import { TransactionDetailScreen } from "../screens/transaction-detail-screen"
+import { LoginScreen } from "../screens/login-screen"
 
 export const PrimaryNavigator = createStackNavigator(
   {
+    login: { screen: LoginScreen },
     accounts: { screen: AccountsScreen },
     account_detail: { screen: AccountDetailScreen },
     transaction_detail: { screen: TransactionDetailScreen },
