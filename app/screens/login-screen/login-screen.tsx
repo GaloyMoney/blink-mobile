04caf7481623c85871db951c62c2feff0b7acce2
Nicolas Burtey <nicolas.burtey@gmail.com>
2019-09-23 17:45:53 -0700

    style for login screen

--- app/screens/login-screen/login-screen.tsx
+++ app/screens/login-screen/login-screen.tsx
@@ -2,11 +2,12 @@ import * as React from "react"
 import { observer, inject } from "mobx-react"
 import { Screen } from "../../components/screen"
 import { NavigationScreenProps, withNavigation } from "react-navigation"
-import { Button, Text, Alert } from "react-native"
-import { Input } from 'react-native-elements'
+import { Text, Alert, StyleSheet, View } from "react-native"
+import { Input, Button } from 'react-native-elements'
 
 import firebase from 'react-native-firebase'
 import { DataStore } from "../../models/data-store"
+import { color } from "../../theme"
 
 
 export interface LoginScreenProps extends NavigationScreenProps<{}> {
@@ -17,6 +18,55 @@ interface State {
   password: string
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    width: "100%",
+  },
+
+  title: {
+    color: color.primary,
+    fontSize: 48,
+    fontWeight: "bold",
+    marginTop: 128, 
+    marginBottom: 24,
+  },
+
+  sub: {
+    fontSize: 18,
+    marginBottom: 48,
+  },
+
+  form: {
+    marginHorizontal: 32,
+    marginVertical: 12,
+  },
+
+  signUp: {
+    color: color.text
+  },
+
+  signIn: {
+    backgroundColor: color.primary
+  },
+
+  bottom: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    marginBottom: 36,
+    alignItems: "center",
+    width: '70%',
+    marginLeft: 0
+  },
+
+  buttonContainer: {
+    width: "100%",
+    marginVertical: 12,
+  }
+
+})
+
 @inject("dataStore")
 @observer
 class LoginScreen extends React.Component<LoginScreenProps, State> {
@@ -25,7 +75,7 @@ class LoginScreen extends React.Component<LoginScreenProps, State> {
     super(props)
 
     this.state = {
-      password: '123456'
+      password: ''
     }
 
     this.unsubscribeHandle = null
@@ -42,7 +92,7 @@ class LoginScreen extends React.Component<LoginScreenProps, State> {
   onUserChanged(user: any) { // TODO : User type
     console.tron.log(user)
     if (user === null) {
-      this.props.dataStore.auth.set("nicolas.burtey+default@gmail.com", false, true, "")
+      this.props.dataStore.auth.set("", false, true, "")
     } else {
       this.props.dataStore.auth.set(user.email, user.emailVerified, user.isAnonymous, user.uid)
 
@@ -96,16 +146,24 @@ class LoginScreen extends React.Component<LoginScreenProps, State> {
 
     return (
 
-      <Screen>
-
+      <Screen style={styles.container}>
+        <Text style={styles.title}>Galoy</Text>
+        <Text style={styles.sub}>The bank built for crypto</Text>
         <Input placeholder="email" value={this.props.dataStore.auth.email}
-          onChangeText={email => this.props.dataStore.auth.setEmail(email)} />
-        <Input placeholder="password" value={password} onChangeText={password => this.setState({ password })} />
+          onChangeText={email => this.props.dataStore.auth.setEmail(email)}
+          inputContainerStyle={styles.form}
+        />
+        <Input placeholder="password" value={password} 
+          onChangeText={password => this.setState({ password })}
+          inputContainerStyle={styles.form} />
 
-        <Button title="Sign up" onPress={() => this.signUp()}></Button>
-        <Button title="Sign in" onPress={() => this.signIn()}></Button>
-        
-        <Text></Text>
+        <View style={styles.bottom}>
+          <Button title="Create an account" titleStyle={styles.signUp}
+            onPress={() => this.signUp()} type="clear" containerStyle={styles.buttonContainer} />
+          <Button title="Sign in" buttonStyle={styles.signIn}
+            onPress={() => this.signIn()} containerStyle={styles.buttonContainer}/>
+        </View>
+
       </Screen>
     )
   }
