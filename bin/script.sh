be65593f40c1aec75587b1832bf22c082714e40c
Nicolas Burtey <nicolas.burtey@gmail.com>
2019-12-20 08:08:12 -0800

    update script

--- bin/script.sh
+++ bin/script.sh
@@ -3,4 +3,9 @@
 
 # script only handle iOS for now
 
+<<<<<<< Updated upstream
+=======
+rm -rf ../ios/lightning/Lndmobile.framework
+mkdir ../ios/lightning/Lndmobile.framework
+>>>>>>> Stashed changes
 cp -R $GOPATH/src/github.com/lightningnetwork/lnd/mobile/build/ios/Lndmobile.framework/ ../ios/lightning/Lndmobile.framework
