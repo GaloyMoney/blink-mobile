4bdb87deb1dc2c47c9a9b8b8f1d57be24e67df83
Nicolas Burtey <nicolas.burtey@gmail.com>
2020-01-04 14:24:40 -0800

    phone auth on the side of mobile

--- app/components/phone/phone.tsx
+++ app/components/phone/phone.tsx
@@ -6,6 +6,7 @@ import { Button } from 'react-native-elements'
 import { bowserLogo } from "."
 import { withNavigation } from 'react-navigation';
 import { TextInput } from "react-native-gesture-handler"
+import { firebase } from "@react-native-firebase/auth"
 
 const styles = StyleSheet.create({
   container: {
@@ -20,8 +21,16 @@ const styles = StyleSheet.create({
   }
 })
 
-const _Phone = ({ text, next, navigation, header = "" }) => {
+const _PhoneInit = ({ text, next, navigation, header = "" }) => {
   const [phone, setPhone] = useState("");
+  
+  const send = async () => {
+    console.tron.log('initPhoneNumber')
+    firebase.functions().useFunctionsEmulator('http://localhost:5000')
+    const result = await firebase.functions().httpsCallable('initPhoneNumber')({phone})
+    console.tron.log(result)
+    navigation.navigate(next, {phone})
+  };
 
   return (
     <View style={styles.container}>
@@ -29,9 +38,34 @@ const _Phone = ({ text, next, navigation, header = "" }) => {
         <Image source={bowserLogo} />
         <Text style={styles.text}>{text}</Text>
         <TextInput style={styles.text} onChangeText={input => (setPhone(input))}>{phone}</TextInput>
-        <Button title="Next" onPress={() => navigation.navigate(next)} containerStyle={styles.buttonContainer}/>
+        <Button title="Next" onPress={() => send()} containerStyle={styles.buttonContainer}/>
   </View>
   )
 }
 
-export const Phone = withNavigation(_Phone)
+const _PhoneVerif = ({ text, next, navigation, header = "" }) => {
+  const [code, setCode] = useState("");
+  const phone = navigation.getParam('phone');
+
+  const sendVerif = async () => {
+    const data = {code, phone}
+    console.tron.log('verifyPhoneNumber', data)
+    firebase.functions().useFunctionsEmulator('http://localhost:5000')
+    const result = await firebase.functions().httpsCallable('verifyPhoneNumber')(data)
+    console.tron.log(result)
+    navigation.navigate(next)
+  };
+
+  return (
+    <View style={styles.container}>
+        <Text style={styles.text}>{header}</Text>
+        <Image source={bowserLogo} />
+        <Text style={styles.text}>{text}</Text>
+        <TextInput style={styles.text} onChangeText={input => (setCode(input))}>{code}</TextInput>
+        <Button title="Next" onPress={() => sendVerif()} containerStyle={styles.buttonContainer}/>
+  </View>
+  )
+}
+
+export const PhoneVerif = withNavigation(_PhoneVerif)
+export const PhoneInit = withNavigation(_PhoneInit)
