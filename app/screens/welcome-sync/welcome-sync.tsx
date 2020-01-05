f748df7b59430f50ccef68e5af9aa7b5fde8fd12
Nicolas Burtey <nicolas.burtey@gmail.com>
2020-01-04 19:35:34 -0800

    adding one youtube video

--- app/screens/welcome-sync/welcome-sync.tsx
+++ app/screens/welcome-sync/welcome-sync.tsx
@@ -3,14 +3,25 @@ import { Screen } from "../../components/screen"
 import { Text } from "../../components/text"
 import { observer, inject } from 'mobx-react'
 
-import * as Progress from 'react-native-progress';
+import * as Progress from 'react-native-progress'
+
+import { YouTubeStandaloneIOS } from 'react-native-youtube';
+import { Button } from "react-native-elements"
 
 export const WelcomeSyncScreen = inject("dataStore")(observer(({dataStore, otherProps}) => {
+
+  const playVideo = () => {
+    YouTubeStandaloneIOS.playVideo('SfeUQWHA3Dc')
+    .then(message => console.log(message))
+    .catch(errorMessage => console.error(errorMessage));
+  }
 
   return (
       <Screen>
         <Text>syncing data... {dataStore.lnd.percentSynced}</Text>
         <Progress.Bar progress={dataStore.lnd.percentSynced} width={200} />
+        <Button title="What is a sat" onPress={() => playVideo()} />
+        {/* <Button title="Next" onPress={() => otherProps.navigation.navigate()} /> */}
       </Screen>
   )
 }))
