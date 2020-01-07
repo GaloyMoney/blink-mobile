57bf1cf54fafed285207f93d5f09f2e13fbee3ad
Nicolas Burtey <nicolas.burtey@gmail.com>
2020-01-07 14:37:37 -0800

    adding activity indicator when we need to wait

---
+++ app/components/loader/loader.tsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  View,
+  Modal,
+  ActivityIndicator
+} from 'react-native';
+import { color } from '../../theme';
+
+export const Loader = ({loading}) => {
+  return (
+    <Modal
+      transparent={true}
+      animationType={'none'}
+      visible={loading}
+      onRequestClose={() => {console.log('close modal')}}>
+      <View style={styles.modalBackground}>
+        <View style={styles.activityIndicatorWrapper}>
+          <ActivityIndicator
+            animating={loading}
+            size="large" color={color.primary} />
+        </View>
+      </View>
+    </Modal>
+  )
+}
+
+const styles = StyleSheet.create({
+  modalBackground: {
+    flex: 1,
+    alignItems: 'center',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    backgroundColor: '#00000040'
+  },
+  activityIndicatorWrapper: {
+    backgroundColor: '#FFFFFF',
+    height: 100,
+    width: 100,
+    borderRadius: 10,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-around'
+  }
+});
