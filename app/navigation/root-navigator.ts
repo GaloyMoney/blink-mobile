cf1393f11374c33e187910f9a86ada74255e3b24
Nicolas Burtey <nicolas.burtey@gmail.com>
2020-01-06 19:29:07 -0800

    improve navigation with switch instead of stack to not allow getting back at certain point of the onboarding

--- app/navigation/root-navigator.ts
+++ app/navigation/root-navigator.ts
@@ -1,10 +1,14 @@
 import { createSwitchNavigator } from "react-navigation"
 import { PrimaryNavigator } from "./primary-navigator"
-import { AuthNavigator } from "./auth-navigator"
+import { AuthNavigator, WalletCompletedNavigator, SyncingNavigator } from "./auth-navigator"
+import { WelcomeGeneratingWalletScreen } from "../screens/welcome-sync"
 
 export const RootNavigator = createSwitchNavigator(
   {
     authStack: { screen: AuthNavigator },
     primaryStack: { screen: PrimaryNavigator },
+    syncingNavigator : { screen: SyncingNavigator },
+    welcomeGenerating: { screen: WelcomeGeneratingWalletScreen },
+    walletCompleted : { screen: WalletCompletedNavigator },
   },
 )
