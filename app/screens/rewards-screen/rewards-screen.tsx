5dba4da90b6db1992a408ddc8f395a5e71456827
Nicolas Burtey <nicolas.burtey@gmail.com>
2020-01-14 14:22:11 -0800

    improve rewards screen

--- app/screens/rewards-screen/rewards-screen.tsx
+++ app/screens/rewards-screen/rewards-screen.tsx
@@ -3,12 +3,12 @@ import { Screen } from "../../components/screen"
 import { StyleSheet, Alert, Image } from "react-native"
 import { Text } from "../../components/text"
 import { color } from "../../theme"
-import { ListItem } from 'react-native-elements'
+import { ListItem, Badge } from 'react-native-elements'
 import Icon from "react-native-vector-icons/Ionicons"
 import { useNavigation } from "react-navigation-hooks"
 import { palette } from "../../theme/palette"
 
-export const presentLogo = require("../welcome-screens/PresentLogo.png")
+export const trophyLogo = require("./TrophyLogo.png")
 
 
 const styles = StyleSheet.create({
@@ -18,13 +18,25 @@ const styles = StyleSheet.create({
         margin: 22,
     },
 
-    text: {
-        fontSize: 22,
+    textButton: {
+        fontSize: 18,
         color: palette.darkGrey
     },
 
+    smallText: {
+        fontSize: 18,
+        color: palette.darkGrey,
+        marginHorizontal: 40,
+        marginBottom: 40,
+        textAlign: 'center',
+    },
+
     button: {
-        marginLeft: 20
+        marginHorizontal: 20,
+        paddingVertical: 6,
+        // borderColor: color.primary,
+        // borderWidth: 1,
+        // borderRadius: 6,
     },
 
     icon: {
@@ -35,7 +47,7 @@ const styles = StyleSheet.create({
 
     image: {
         alignSelf: "center",
-        padding: 20,
+        marginVertical: 50,
     },
 })
 
@@ -46,35 +58,40 @@ export const RewardsScreen = () => {
     const bank = [
         {
             title: 'Backup wallet',
-            icon: 'ios-lock'
+            icon: 'ios-lock',
+            badge: "+1,000 sats",
         },
         {
             title: "Make a payment",
-            icon: 'ios-exit'
+            icon: 'ios-exit',
+            badge: "+1,000 sats",
         },
         {
             title: 'Learn about Bitcoin',
-            icon: 'ios-school'
+            icon: 'ios-school',
+            badge: "+1,000 sats",
         },
         {
             title: "Open a Galoy bank account",
-            icon: 'ios-gift'
+            icon: 'ios-gift',
+            badge: "+100,000 sats",
         }
       ]
 
     return (
         <Screen>
-            <Image source={presentLogo} style={styles.image} />
-            <Text>Complete the task below to earn more bitcoin rewards!</Text>
+            <Image source={trophyLogo} style={styles.image} />
+            <Text style={styles.smallText}>Complete the task below to earn more bitcoin rewards!</Text>
             {
                 bank.map((item, i) => (
                 <ListItem
-                    titleStyle={styles.text}
+                    titleStyle={styles.textButton}
                     style={styles.button}
                     key={i}
                     title={item.title}
                     leftIcon={<Icon name={item.icon} style={styles.icon} size={32} color={color.primary} />}
                     onPress={() => Alert.alert('TODO')}
+                    badge={{value:item.badge, badgeStyle: {backgroundColor: color.primary}}}
                     chevron
                 />
                 ))
