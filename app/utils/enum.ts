040932699594a1b5817252b696fa4d2a4e262dca
Nicolas Burtey <nicolas.burtey@gmail.com>
2020-04-22 11:22:56 -0700

    remove the notion of virtual bitcoin in the app

--- app/utils/enum.ts
+++ app/utils/enum.ts
@@ -1,9 +1,7 @@
 export enum AccountType {
   Bank = "Cash Account",
   Bitcoin = "Bitcoin Wallet",
-  VirtualBitcoin = "Bitcoin Rewards",
   BankAndBitcoin = "BankAndBitcoin",
-  BankAndVirtualBitcoin = "All", // TODO find better naming
 }
 
 export enum CurrencyType {
