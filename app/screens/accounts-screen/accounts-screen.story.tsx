44b039cdf277f6afe07fe19000bfb976f719b973
Nicolas Burtey <nicolas.burtey@gmail.com>
2020-04-23 16:36:55 -0700

    main account screen update

--- app/screens/accounts-screen/accounts-screen.story.tsx
+++ app/screens/accounts-screen/accounts-screen.story.tsx
@@ -11,21 +11,28 @@ storiesOf("AccountItem", module)
   .add("Style Presets", () => (
     <Story>
       <UseCase text="Initial loading">
-        <AccountItem account={AccountType.Bank} icon={"ios-cash"} />
+        <AccountItem 
+          account={AccountType.Bank}
+          icon={"ios-cash"} />
       </UseCase>
       <UseCase text="After loading">
         <AccountItem
           account={AccountType.Bank}
-          icon={"ios-cash"}
           amount={1234}
         />
       </UseCase>
       <UseCase text="Open bank account">
         <AccountItem
           account={AccountType.Bank}
-          icon={"ios-cash"}
           amount={1234}
-          title={"Open bank account"}
+          title={"Open cash account"}
+        />
+      </UseCase>
+      <UseCase text="Open bank account">
+        <AccountItem
+          account={AccountType.Bitcoin}
+          amount={1234}
+          title={"Bitcoin wallet"}
         />
       </UseCase>
     </Story>
