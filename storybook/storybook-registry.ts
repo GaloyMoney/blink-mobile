b762d2ef6e4ce6c7bf6e4416804aaba227820c5f
Nicolas Burtey <nicolas.burtey@gmail.com>
2020-04-23 15:30:20 -0700

    reimplementing transaction detail screen

--- storybook/storybook-registry.ts
+++ storybook/storybook-registry.ts
@@ -6,4 +6,5 @@ require("../app/screens/splash-screen/splash-screen.story")
 require("../app/screens/accounts-screen/accounts-screen.story")
 require("../app/screens/bank-onboarding/bank-onboarding.story")
 require("../app/screens/rewards-map-screen/rewards-map-screen.story")
+require("../app/screens/transaction-detail-screen/transaction-detail-screen.story")
 
