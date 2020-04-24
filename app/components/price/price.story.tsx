f50d667060670f4e56012e435a3e6af15bf862c3
Nicolas Burtey <nicolas.burtey@gmail.com>
2020-04-23 19:05:17 -0700

    first iteration of the Price module

---
+++ app/components/price/price.story.tsx
@@ -0,0 +1,25 @@
+import * as React from "react"
+import { storiesOf } from "@storybook/react-native"
+import { StoryScreen, Story, UseCase } from "../../../storybook/views"
+import { Price } from "./price"
+
+declare let module
+
+const data = [
+  { x: 0, y: 0 },
+  { x: 1, y: 2 },
+  { x: 2, y: 1 },
+  { x: 3, y: 4 },
+  { x: 4, y: 3 },
+  { x: 5, y: 5 }
+];
+
+storiesOf("Price Component", module)
+  .addDecorator((fn) => <StoryScreen>{fn()}</StoryScreen>)
+  .add("Style Presets", () => (
+    <Story>
+      <UseCase text="BTC" usage="The primary." noBackground={true}>
+        <Price price={6874.25} delta={0.70} data={data} />
+      </UseCase>
+    </Story>
+  ))
