68fca1e0f7cea8b25a6326e3ec5157ae2a1cf13a
Nicolas Burtey <nicolas.burtey@gmail.com>
2020-05-12 15:37:38 -0700

    misc update

--- app/navigation/primary-navigator.tsx
+++ app/navigation/primary-navigator.tsx
@@ -307,7 +307,7 @@ export const RootStackScreen = () => {
         component={TransactionDetailScreen}
         options={{ 
           headerShown: false,
-          cardStyleInterpolator: CardStyleInterpolators.forModalPresentationIOS,
+          // cardStyleInterpolator: CardStyleInterpolators.forModalPresentationIOS,
         }}  
       />
 
