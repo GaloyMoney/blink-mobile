88ca84f7f52ef7075dc349c902885bfda2929d06
Nicolas Burtey <nicolas.burtey@gmail.com>
2020-05-14 11:52:02 -0700

    this was used when non custodial

--- app/utils/helper.ts
+++ app/utils/helper.ts
@@ -22,49 +22,6 @@ export const capitalize = (s) => {
   return s.charAt(0).toUpperCase() + s.slice(1)
 }
 
-export const showFundingTx = async (fundingTx) => {
-  const url = `https://blockstream.info/testnet/tx/${fundingTx}`
-
-  try {
-    if (await InAppBrowser.isAvailable()) {
-      const result = await InAppBrowser.open(url, {
-        // iOS Properties
-        dismissButtonStyle: "close",
-        // preferredBarTintColor: '#453AA4',
-        // preferredControlTintColor: 'white',
-        readerMode: false,
-        // animated: true,
-        modalPresentationStyle: "fullScreen",
-        // modalTransitionStyle: 'partialCurl',
-        modalEnabled: true,
-        enableBarCollapsing: false,
-        // Android Properties
-        showTitle: true,
-        // toolbarColor: '#6200EE',
-        secondaryToolbarColor: "black",
-        enableUrlBarHiding: true,
-        enableDefaultShare: true,
-        forceCloseOnRedirection: false,
-        // Specify full animation resource identifier(package:anim/name)
-        // or only resource name(in case of animation bundled with app).
-        // animations: {
-        //   startEnter: 'slide_in_right',
-        //   startExit: 'slide_out_left',
-        //   endEnter: 'slide_in_left',
-        //   endExit: 'slide_out_right'
-        // },
-        headers: {
-          "my-custom-header": "my custom header value",
-        },
-      })
-      // Alert.alert(JSON.stringify(result))
-    } else Linking.openURL(url)
-  } catch (error) {
-    Alert.alert(error.message)
-  }
-}
-
-
 // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 export const shuffle = (array) => {
   let currentIndex = array.length
