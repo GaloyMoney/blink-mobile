86bd02808cb9edcda3d1e7a81de72e2aa16befa3
Nicolas Burtey <nicolas.burtey@gmail.com>
2020-08-08 16:37:24 -0500

    finally build succesfully with local fastlane!

--- android/app/src/main/java/com/galoyapp/MainApplication.java
+++ android/app/src/main/java/com/galoyapp/MainApplication.java
@@ -30,8 +30,6 @@ public class MainApplication extends Application implements ReactApplication {
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
 
-          packages.add(new RCTQRCodeLocalImagePackage());
-
           return packages;
         }
 
