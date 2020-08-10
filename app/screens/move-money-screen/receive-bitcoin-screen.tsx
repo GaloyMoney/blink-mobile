14859bb6d9d3b027ac31140fb4fca89dc1087acc
Nicolas Burtey <nicolas.burtey@gmail.com>
2020-08-10 17:46:48 -0500

    monitoring back transaction in the receive bitcoin tab. also add the copy invoice functionality

--- app/screens/move-money-screen/receive-bitcoin-screen.tsx
+++ app/screens/move-money-screen/receive-bitcoin-screen.tsx
@@ -4,7 +4,7 @@ import { observer } from "mobx-react"
 import * as React from "react"
 import { useEffect, useState } from "react"
 import Clipboard from "@react-native-community/clipboard"
-import { ActivityIndicator, Alert, Dimensions, Share, Text, View } from "react-native"
+import { ActivityIndicator, Alert, Dimensions, Pressable, Share, Text, View } from "react-native"
 import { Button, ButtonGroup } from "react-native-elements"
 import EStyleSheet from "react-native-extended-stylesheet"
 import { ScrollView } from "react-native-gesture-handler"
@@ -74,22 +74,33 @@ export const ReceiveBitcoinScreen = observer(({ navigation }) => {
   const [amount, setAmount] = useState(0)
   const [loading, setLoading] = useState(false)
 
-  const [networkIndex, setNetworkIndex] = useState(1)
+  const [networkIndex, setNetworkIndex] = useState(0)
   const [data, setData] = useState(" ")
+
+
+  // a list of the hash that has been added to the receive bitcoin tab.
+  // because we are creating a 
+  // TODO: send notification from the server would be a better architecture
+  const [hashes, setHashes] = useState([])
+  const [counter, setCounter] = useState(0)
+
 
   useEffect(() => {
     update()
   }, [networkIndex])
 
   const update = async () => {
-    // TODO: cancel the network call when switching from one network to the other
-
     if (networkIndex === 0) {
       await createInvoice()
     } else {
       const uri = `bitcoin:${values(store.lastOnChainAddresses)[0].id}`
       setData(amount === 0 ? uri : uri + `?amount=${amount * 10 ** 8}`)
     }
+  }
+
+  const copyInvoice = () => {
+    Clipboard.setString(data)
+    Alert.alert("Invoice has been copied in the clipboard")
   }
 
   const shareInvoice = async () => {
@@ -115,6 +126,8 @@ export const ReceiveBitcoinScreen = observer(({ navigation }) => {
   const createInvoice = async () => {
     setLoading(true)
 
+    console.tron.log("createInvoice")
+
     let invoice
 
     try {
@@ -136,13 +149,74 @@ export const ReceiveBitcoinScreen = observer(({ navigation }) => {
     try {
       const invoiceDecoded = lightningPayReq.decode(invoice)
       const hash = getHash(invoiceDecoded)
+      setHashes(array => [...array, hash])
 
+      console.tron.log({hash})
+      
       setData(invoice)
     } catch (err) {
       Alert.alert(err.toString())
     } finally {
       setLoading(false)
     }
+  }
+
+  useEffect(() => {
+    const fetchInvoice = async () => {
+      try {
+
+      // lightning
+      const query = `mutation updatePendingInvoice($hash: String) {
+        invoice {
+          updatePendingInvoice(hash: $hash)
+        }
+      }`
+
+      for (const hash of hashes) { 
+        const result = await request(query, {hash})
+
+        if (result.invoice.updatePendingInvoice === true) {
+          success()
+        }
+      }
+  
+      // on-chain
+      // TODO
+
+      } catch (err) {
+        console.tron.warn(`can't fetch invoice ${err}`)
+      }
+    }
+  
+    console.tron.log({counter, hashes})
+    fetchInvoice()
+  },  [counter])
+
+  // this is to trigger the fetchInvoice function. not sure why we need the counter.
+  // but running to issue without where hashes is not populated
+  // as if it was taking the initial context but not 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter(counter => counter + 1)
+    }, 2500)
+    return () => clearInterval(interval)
+  }, [])
+
+  const success = () => {
+    const options = {
+      enableVibrateFallback: true,
+      ignoreAndroidSystemSettings: false,
+    }
+
+    store.queryWallet()
+
+    ReactNativeHapticFeedback.trigger("notificationSuccess", options)
+    Alert.alert("success", "This invoice has been paid", [{
+      text: translate("common.ok"),
+      onPress: () => {
+        navigation.goBack(false)
+      },
+    }])
   }
 
   return (
@@ -175,8 +249,10 @@ export const ReceiveBitcoinScreen = observer(({ navigation }) => {
         </View> */}
         <View style={styles.qr} >
           {!loading && 
-            <QRCode size={280} value={data} logoBackgroundColor='white' ecl="M"
-            logo={Icon.getImageSourceSync(networkIndex === 0 ? "ios-flash" : "logo-bitcoin", 64, palette.orange)} />
+            <Pressable onPress={copyInvoice}>
+              <QRCode size={280} value={data} logoBackgroundColor='white' ecl="M"
+              logo={Icon.getImageSourceSync(networkIndex === 0 ? "ios-flash" : "logo-bitcoin", 64, palette.orange)} />
+            </Pressable>
           }
           {loading && 
             <View style={{ width: 280, height: 280, 
@@ -187,6 +263,7 @@ export const ReceiveBitcoinScreen = observer(({ navigation }) => {
               <ActivityIndicator size="large" color={palette.blue} />
             </View>
           }
+          {loading && <Text> </Text> || <Text>Tap QR Code to Copy</Text>}
         </View>
         <View style={{ alignContent: "center", alignItems: "center", marginHorizontal: 48 }}>
           <Button
@@ -202,99 +279,3 @@ export const ReceiveBitcoinScreen = observer(({ navigation }) => {
     </Screen>
   )
 })
-
-export const ShowQRCode = ({ route, navigation }) => {
-  const { data, type, hash, amount } = route.params
-
-  const store = React.useContext(StoreContext)
-
-
-  const copyInvoice = () => {
-    Clipboard.setString(data)
-    Alert.alert("Invoice has been copied in the clipboard")
-  }
-
-  useEffect(() => {
-    const interval = setInterval(async () => {
-      try {
-
-        if (type === "lightning") {
-          const query = `mutation updatePendingInvoice($hash: String) {
-            invoice {
-              updatePendingInvoice(hash: $hash)
-            }
-          }`
-    
-          const result = await request(query, {hash})
-    
-          if (result.invoice.updatePendingInvoice === true) {
-            success()
-          }
-        }
-
-        if (type === "onchain") {
-          // TODO
-        }
-
-      } catch (err) {
-        console.tron.warn(`can't ferch invoice ${err}`)
-      }
-    }, 2000)
-    return () => clearInterval(interval)
-  }, [])
-
-  const success = () => {
-    const options = {
-      enableVibrateFallback: true,
-      ignoreAndroidSystemSettings: false,
-    }
-
-    store.queryWallet()
-
-    ReactNativeHapticFeedback.trigger("notificationSuccess", options)
-    Alert.alert("success", "This invoice has been paid", [
-      {
-        text: translate("common.ok"),
-        onPress: () => {
-          navigation.goBack(false)
-        },
-      },
-    ])
-  }
-
-  const textAmount = amount !== 0 ? 
-    `Receive ${amount} sats / \$${(amount * store.rate(CurrencyType.BTC)).toFixed(2)}` : " "
-
-  return (
-    <Screen backgroundColor={palette.lighterGrey}>
-      <ScrollView style={{ flex: 1, paddingTop: 32 }}>
-        <View style={{ alignItems: "center" }}>
-          <IconTransaction type={"receive"} size={75} color={palette.orange} />
-        </View>
-        <View style={styles.qr} >
-          <QRCode size={280} value={data} logoBackgroundColor='white' ecl="M"
-          logo={Icon.getImageSourceSync(type === "onchain" ? "logo-bitcoin" : "ios-flash", 32, palette.orange)} />
-        </View>
-        <View style={{ marginHorizontal: 48 }}>
-          <Text style={{ fontSize: 16, alignSelf: "center" }}>{textAmount}</Text>
-          <Button
-            buttonStyle={styles.buttonStyle}
-            disabledStyle={styles.buttonStyle}
-            containerStyle={{width: "100%"}}
-            title="Share"
-            onPress={shareInvoice}
-            titleStyle={{ fontWeight: "bold" }}
-          />
-          <Button
-            buttonStyle={styles.buttonStyle}
-            disabledStyle={styles.buttonStyle}
-            containerStyle={{width: "100%"}}
-            title="Copy"
-            onPress={copyInvoice}
-            titleStyle={{ fontWeight: "bold" }}
-          />
-        </View>
-      </ScrollView>
-    </Screen>
-  )
-}
