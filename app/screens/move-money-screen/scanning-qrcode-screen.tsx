f21d096762c47d3144453e53f5d224a0ff572804
Nicolas Burtey <nicolas.burtey@gmail.com>
2020-08-12 14:24:38 -0500

    refactoring scanning QRCode and sendbitcoin to 2 different screens

---
+++ app/screens/move-money-screen/scanning-qrcode-screen.tsx
@@ -0,0 +1,119 @@
+import { useIsFocused, useNavigation } from "@react-navigation/native"
+import * as React from "react"
+import { Alert, Dimensions, Platform, Pressable, View, ViewStyle } from "react-native"
+import { RNCamera } from "react-native-camera"
+import EStyleSheet from "react-native-extended-stylesheet"
+import ImagePicker from 'react-native-image-picker'
+import Svg, { Circle } from "react-native-svg"
+import Icon from "react-native-vector-icons/Ionicons"
+import { Screen } from "../../components/screen"
+import { color } from "../../theme"
+import { palette } from "../../theme/palette"
+import { validInvoice } from "../../utils/parsing"
+const LocalQRCode = require('@remobile/react-native-qrcode-local-image');
+
+
+const CAMERA: ViewStyle = {
+  width: "100%",
+  height: "100%",
+  position: "absolute",
+}
+
+const { width: screenWidth } = Dimensions.get("window")
+const { height: screenHeight } = Dimensions.get("window")
+
+const styles = EStyleSheet.create({
+  rectangleContainer: {
+    position: 'absolute', 
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+	rectangle: {
+    height: screenWidth * .65,
+    width: screenWidth * .65,
+		borderWidth: 2,
+		borderColor: palette.blue,
+		backgroundColor: 'transparent',
+  },
+})
+
+
+export const ScanningQRCodeScreen = () => {
+  const { navigate, goBack } = useNavigation()
+
+  const decodeInvoice = async (data) => {
+    try {
+      const [valid, errorMessage, invoice, amount, amountless, note] = validInvoice(data)
+      if (!valid) {
+        Alert.alert(errorMessage)
+        return
+      }
+
+      navigate("sendBitcoin", { invoice, amount, amountless, note })
+    } catch (err) {
+      Alert.alert(err.toString())
+    }
+  }
+
+  const showImagePicker = () => {
+    ImagePicker.launchImageLibrary({
+      title: null,
+      mediaType: 'photo',
+      takePhotoButtonTitle: null,
+    },
+    response => {
+      if (response.uri) {
+        const uri = Platform.OS === 'ios' ? response.uri.toString().replace('file://', '') : response.path.toString();
+        LocalQRCode.decode(uri, (error, result) => {
+          if (!error) {
+            decodeInvoice( result );
+          } else {
+            if (error.message === "Feature size is zero!") {
+              Alert.alert("we could not find a QR code in the image");
+            } else {
+              console.tron.log({error})
+              Alert.alert(error.message);
+            }
+          }
+        });
+      }
+    },
+  )}
+
+  return (
+    <Screen unsafe={true}>
+      {useIsFocused() &&
+      <RNCamera
+        style={CAMERA}
+        captureAudio={false}
+        onBarCodeRead={(event) => {
+          const qr = event.data
+          decodeInvoice(qr)
+        }}
+        onTap={(r) => console.tron.log({r})}
+        >
+        <View style={styles.rectangleContainer}>
+          <View style={[styles.rectangle]} />
+        </View>
+        <Pressable onPress={goBack}>
+          <View style={{width: 64, height: 64, alignSelf: "flex-end", marginTop: 40, marginRight: 16}}>
+            <Svg viewBox="0 0 100 100">
+              <Circle cx={50} cy={50} r={50} fill={palette.white} opacity={.5} />
+            </Svg>
+            <Icon name="ios-close" size={64} style={{position: "absolute", top: -2}} />
+          </View>
+        </Pressable>
+        <View style={{position: "absolute", width: screenWidth, height: 128, top: screenHeight - 96, left: 32}}>
+          <Pressable onPress={showImagePicker}>
+            <Icon name="image" size={64} color={palette.lightGrey} style={{opacity: .8}} />
+          </Pressable>
+        </View>
+      </RNCamera>}
+    </Screen>
+  )
+}
--- app/screens/move-money-screen/send-bitcoin-screen.tsx
+++ app/screens/move-money-screen/send-bitcoin-screen.tsx
@@ -1,13 +1,12 @@
-import { useIsFocused, useNavigation } from "@react-navigation/native"
+import analytics from '@react-native-firebase/analytics'
+import { useNavigation } from "@react-navigation/native"
+import LottieView from 'lottie-react-native'
 import * as React from "react"
 import { useEffect, useState } from "react"
-import { Alert, Dimensions, Platform, Pressable, ScrollView, Text, View, ViewStyle } from "react-native"
-import { RNCamera } from "react-native-camera"
+import { Dimensions, ScrollView, Text, View, ViewStyle } from "react-native"
 import { Button, Input } from "react-native-elements"
 import EStyleSheet from "react-native-extended-stylesheet"
 import ReactNativeHapticFeedback from "react-native-haptic-feedback"
-import ImagePicker from 'react-native-image-picker'
-import Svg, { Circle } from "react-native-svg"
 import Icon from "react-native-vector-icons/Ionicons"
 import { InputPaymentDataInjected } from "../../components/input-payment"
 import { Screen } from "../../components/screen"
@@ -15,24 +14,14 @@ import { translate } from "../../i18n"
 import { StoreContext } from "../../models"
 import { color } from "../../theme"
 import { palette } from "../../theme/palette"
-import { validInvoice } from "../../utils/parsing"
 import { request } from "../../utils/request"
-const LocalQRCode = require('@remobile/react-native-qrcode-local-image');
-import analytics from '@react-native-firebase/analytics'
 
-import LottieView from 'lottie-react-native'
 const successLottie = require('./success_lottie.json')
 const errorLottie = require('./error_lottie.json')
 const pendingLottie = require('./pending_lottie.json')
 
-const CAMERA: ViewStyle = {
-  width: "100%",
-  height: "100%",
-  position: "absolute",
-}
 
 const { width: screenWidth } = Dimensions.get("window")
-const { height: screenHeight } = Dimensions.get("window")
 
 const styles = EStyleSheet.create({
   buttonStyle: {
@@ -96,102 +85,10 @@ const styles = EStyleSheet.create({
     height: 70,
     width: 70,
   },
-
-  rectangleContainer: {
-    position: 'absolute', 
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-
-	rectangle: {
-    height: screenWidth * .65,
-    width: screenWidth * .65,
-		borderWidth: 2,
-		borderColor: palette.blue,
-		backgroundColor: 'transparent',
-  },
   
   lottie: {height: 200}
 })
 
-export const ScanningQRCodeScreen = () => {
-  const { navigate, goBack } = useNavigation()
-
-  const decodeInvoice = async (data) => {
-    try {
-      const [valid, errorMessage, invoice, amount, amountless, note] = validInvoice(data)
-      if (!valid) {
-        Alert.alert(errorMessage)
-        return
-      }
-
-      navigate("sendBitcoin", { invoice, amount, amountless, note })
-    } catch (err) {
-      Alert.alert(err.toString())
-    }
-  }
-
-  const showImagePicker = () => {
-    ImagePicker.launchImageLibrary({
-      title: null,
-      mediaType: 'photo',
-      takePhotoButtonTitle: null,
-    },
-    response => {
-      if (response.uri) {
-        const uri = Platform.OS === 'ios' ? response.uri.toString().replace('file://', '') : response.path.toString();
-        LocalQRCode.decode(uri, (error, result) => {
-          if (!error) {
-            decodeInvoice( result );
-          } else {
-            if (error.message === "Feature size is zero!") {
-              Alert.alert("we could not find a QR code in the image");
-            } else {
-              console.tron.log({error})
-              Alert.alert(error.message);
-            }
-          }
-        });
-      }
-    },
-  )}
-
-  return (
-    <Screen unsafe={true}>
-      {useIsFocused() &&
-      <RNCamera
-        style={CAMERA}
-        captureAudio={false}
-        onBarCodeRead={(event) => {
-          const qr = event.data
-          decodeInvoice(qr)
-        }}
-        onTap={(r) => console.tron.log({r})}
-        >
-        <View style={styles.rectangleContainer}>
-          <View style={[styles.rectangle]} />
-        </View>
-        <Pressable onPress={goBack}>
-          <View style={{width: 64, height: 64, alignSelf: "flex-end", marginTop: 40, marginRight: 16}}>
-            <Svg viewBox="0 0 100 100">
-              <Circle cx={50} cy={50} r={50} fill={palette.white} opacity={.5} />
-            </Svg>
-            <Icon name="ios-close" size={64} style={{position: "absolute", top: -2}} />
-          </View>
-        </Pressable>
-        <View style={{position: "absolute", width: screenWidth, height: 128, top: screenHeight - 96, left: 32}}>
-          <Pressable onPress={showImagePicker}>
-            <Icon name="image" size={64} color={palette.lightGrey} style={{opacity: .8}} />
-          </Pressable>
-        </View>
-      </RNCamera>}
-    </Screen>
-  )
-}
 
 export const SendBitcoinScreen: React.FC = ({ route }) => {
   const store = React.useContext(StoreContext)
