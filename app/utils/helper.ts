f4ecd1d38929aa78a2f890d66fe45c3d115aaf97
Nicolas Burtey <nicolas.burtey@gmail.com>
2020-08-18 16:53:42 -0500

    no longer used?

--- app/utils/helper.ts
+++ app/utils/helper.ts
@@ -1,17 +1,5 @@
 import { NativeModules } from "react-native"
 
-/**
- * Convert bytes to a hex encoded string
- * @param  {Buffer|Uint8Array} buf The input as bytes or base64 string
- * @return {string}            The output as hex
- */
-export const toHex = (buf) => {
-  if (!Buffer.isBuffer(buf) && !(buf instanceof Uint8Array)) {
-    throw new Error("Invalid input!")
-  }
-  return Buffer.from(buf).toString("hex")
-}
-
 export const emailIsValid = (email) => {
   return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
 }
