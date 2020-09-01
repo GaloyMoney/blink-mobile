c4414b0374dc2be5b23265da877946e6e0420ca1
Nicolas Burtey <nicolas.burtey@gmail.com>
2020-09-01 15:17:42 -0500

    different improvement for android layout

--- app/components/input-payment/input-payment.tsx
+++ app/components/input-payment/input-payment.tsx
@@ -10,8 +10,6 @@ import { palette } from "../../theme/palette"
 import { CurrencyType } from "../../utils/enum"
 import { TextCurrency } from "../text-currency/text-currency"
 
-
-
 const styles = EStyleSheet.create({
   amount: {
     alignItems: "center",
@@ -45,12 +43,19 @@ const styles = EStyleSheet.create({
 
   textStyle: {
     fontSize: 24,
-    color: palette.darkGrey
-  }
+    color: palette.darkGrey,
+  },
 })
 
-
-const InputCurrency = ({ amount, setAmount, currency, onBlur, forceKeyboard, appendDot, editable }) => {
+const InputCurrency = ({
+  amount,
+  setAmount,
+  currency,
+  onBlur,
+  forceKeyboard,
+  appendDot,
+  editable,
+}) => {
   const inputRef = React.useRef()
 
   React.useEffect(() => {
@@ -59,66 +64,72 @@ const InputCurrency = ({ amount, setAmount, currency, onBlur, forceKeyboard, app
 
   // TODO: show "an amount is needed" in red
   function keyboardFocus() {
-    if (forceKeyboard && (amount == "" || amount == "." || +amount == 0.)) {
+    if (forceKeyboard && (amount == "" || amount == "." || +amount == 0)) {
       inputRef?.current.focus()
     }
   }
 
-  let value 
+  let value
   if (amount == 0 || isNaN(amount)) {
     value = ""
   } else {
     value = String(+amount)
   }
 
-  // only add dot for for non-sats. 
+  // only add dot for for non-sats.
   if ((currency === CurrencyType.USD || currency === CurrencyType.BTC) && appendDot) {
     value += "."
   }
 
   React.useEffect(() => {
-    Keyboard.addListener("keyboardDidHide", _keyboardDidHide);
+    Keyboard.addListener("keyboardDidHide", _keyboardDidHide)
 
     // cleanup function
     return () => {
-      Keyboard.removeListener("keyboardDidHide", _keyboardDidHide);
-    };
-  }, []);
+      Keyboard.removeListener("keyboardDidHide", _keyboardDidHide)
+    }
+  }, [])
 
   const _keyboardDidHide = () => {
-    onBlur()
-  };
+    inputRef?.current.blur()
+  }
 
-  return <Input
-    ref={inputRef}
-    placeholder={"set an amount"}
-    autoFocus={forceKeyboard}
-    value={value}
-    leftIcon={currency === CurrencyType.USD ? <Text style={styles.textStyle}>$</Text> : null}
-    rightIcon={currency === CurrencyType.BTC ? 
-      <Text style={styles.textStyle}>BTC</Text> :
-      currency === "sats" ?
-        <Text style={styles.textStyle}>sats</Text> :
-        null}
-    inputContainerStyle={{width: 250}}
-    inputStyle={[styles.textStyle, {textAlign: "center"}]}
-    onChangeText={setAmount}
-    keyboardType={currency === "sats" ? "number-pad": "decimal-pad"}
-    onBlur={(event) => {onBlur(event); keyboardFocus()}}
-    enablesReturnKeyAutomatically={true}
-    returnKeyLabel="Update"
-    returnKeyType="done" 
-    editable={editable}
-    onEndEditing={onBlur}
-  />
+  return (
+    <Input
+      ref={inputRef}
+      placeholder={"set an amount"}
+      autoFocus={forceKeyboard}
+      value={value}
+      leftIcon={currency === CurrencyType.USD ? <Text style={styles.textStyle}>$</Text> : null}
+      rightIcon={
+        currency === CurrencyType.BTC ? (
+          <Text style={styles.textStyle}>BTC</Text>
+        ) : currency === "sats" ? (
+          <Text style={styles.textStyle}>sats</Text>
+        ) : null
+      }
+      inputContainerStyle={{ width: 250 }}
+      inputStyle={[styles.textStyle, { textAlign: "center" }]}
+      onChangeText={setAmount}
+      keyboardType={currency === "sats" ? "number-pad" : "decimal-pad"}
+      onBlur={(event) => {
+        onBlur(event)
+        keyboardFocus()
+      }}
+      enablesReturnKeyAutomatically={true}
+      returnKeyLabel="Update"
+      returnKeyType="done"
+      editable={editable}
+      onEndEditing={onBlur}
+    />
+  )
 }
 
-
 export interface InputPaymentDataInjectedProps {
-  editable: boolean,
+  editable: boolean
   onUpdateAmount(number): void
   onBlur?(): void
-  forceKeyboard: boolean,
+  forceKeyboard: boolean
   initAmount?: number
   currencyPreference?: string // "sats" | "BTC" | "usd"
 }
@@ -126,7 +137,7 @@ export interface InputPaymentDataInjectedProps {
 export const InputPaymentDataInjected = (props: InputPaymentDataInjectedProps) => {
   const store = React.useContext(StoreContext)
   const price = store.rate(CurrencyType.BTC)
-  
+
   return <InputPayment price={price} {...props} />
 }
 
@@ -139,7 +150,6 @@ export const InputPayment = ({
   initAmount = 0, // in sats
   currencyPreference = "USD",
 }) => {
-
   React.useEffect(() => {
     setAmount(initAmount)
   }, [initAmount])
@@ -149,28 +159,29 @@ export const InputPayment = ({
   const [appendDot, setAppendDot] = React.useState(false)
 
   const mapping = {
-    "USD": {
+    USD: {
       primary: "USD",
       // TODO refactor: other place could use those conversions
-      conversion: sats => (sats * price < 0.01) ? (sats * price).toFixed(4) : (sats * price).toFixed(2),
-      reverse: usd => usd / price,
+      conversion: (sats) =>
+        sats * price < 0.01 ? (sats * price).toFixed(4) : (sats * price).toFixed(2),
+      reverse: (usd) => usd / price,
       secondary: "sats",
-      secondaryConversion: sats => sats
+      secondaryConversion: (sats) => sats,
     },
-    "sats": {
+    sats: {
       primary: "sats",
-      conversion: sats => sats.toFixed(0),
-      reverse: sats => sats,
+      conversion: (sats) => sats.toFixed(0),
+      reverse: (sats) => sats,
       secondary: "USD",
-      secondaryConversion: sats => sats * price
+      secondaryConversion: (sats) => sats * price,
     },
-    "BTC": {
+    BTC: {
       primary: "BTC",
-      conversion: sats => (sats / 10 ** 8).toFixed(8), // BigNum?
-      reverse: btc => btc * 10 ** 8,
+      conversion: (sats) => (sats / 10 ** 8).toFixed(8), // BigNum?
+      reverse: (btc) => btc * 10 ** 8,
       secondary: "USD",
-      secondaryConversion: sats => sats * price
-    }
+      secondaryConversion: (sats) => sats * price,
+    },
   }
 
   const next = () => {
@@ -182,16 +193,16 @@ export const InputPayment = ({
 
   return (
     // FIXME style
-    <View style={{ flexDirection: "row", alignItems: "center", width: "100%", paddingLeft: 32 }}> 
+    <View style={{ flexDirection: "row", alignItems: "center", width: "100%", paddingLeft: 32 }}>
       <View style={styles.header}>
         <InputCurrency
-          amount={mapping[unit].conversion(amount)} 
+          amount={mapping[unit].conversion(amount)}
           currency={mapping[unit].primary}
           appendDot={appendDot}
-          setAmount={amount => {
+          setAmount={(amount) => {
             function endByDot(s) {
-              var rgx = /^[0-9]*\.{1}$/;
-              return s.match(rgx);
+              var rgx = /^[0-9]*\.{1}$/
+              return s.match(rgx)
             }
             setAppendDot(!!endByDot(amount))
             const newAmount = mapping[unit].reverse(+amount)
@@ -201,13 +212,14 @@ export const InputPayment = ({
             }
           }}
           editable={editable}
-          onBlur={onBlur} 
+          onBlur={onBlur}
           forceKeyboard={forceKeyboard}
         />
         <TextCurrency
-          amount={mapping[unit].secondaryConversion(amount)} 
+          amount={mapping[unit].secondaryConversion(amount)}
           currency={mapping[unit].secondary}
-          style={styles.subCurrencyText} />
+          style={styles.subCurrencyText}
+        />
       </View>
       <TouchableOpacity onPress={next}>
         <Icon name={"ios-swap-vertical"} size={32} />
