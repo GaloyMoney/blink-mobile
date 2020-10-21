fe50357b4eae1b8734b7dc80e7ea89667b44953a
Nicolas Burtey <nicolas.burtey@gmail.com>
2020-10-20 19:06:42 -0500

    have a send Max button when sending bitcoin onchain

--- app/screens/move-money-screen/send-bitcoin-screen.story.tsx
+++ app/screens/move-money-screen/send-bitcoin-screen.story.tsx
@@ -7,7 +7,7 @@ import { SendBitcoinScreenJSX } from "./send-bitcoin-screen";
 
 const noop = () => {}
 const defaultParams = {setStatus: noop, setAmount: noop, goBack: noop, pay: noop, 
-  price: 10000, prefCurrency: "BTC", nextPrefCurrency: noop, 
+  price: 10000, prefCurrency: "sats", nextPrefCurrency: noop, 
   invoice: "lnbc1p0kxle6pp5f936pgsmu7qjk0tph7uw8v9vhqlq3k37k3w6tulhy3rkf9ecqqcsdqu2askcmr9wssx7e3q2dshgmmndp5scqzpgxqyz5vqsp5z2hre6tcqrs67emaqvlewhf8klzm7hx5ly3scs7lkd4xlrgp52gs9qy9qsq0k704xkrj82sueam0zthx95luwdpeg60307x48gq3mrrwdnz6g0y2vpluy5z8keep463gdenrfy23f23mayhevs27ffjvu5nh0napacqjdeesl",
   address: "bcrt1qttw6a49hcayl0nx0xm7yur4pw9szftaanxpmd5"
 }
@@ -57,20 +57,21 @@ storiesOf("Send bitcoin Screen", module)
   .add("bitcoin", () => (
     <SendBitcoinScreenJSX 
       status={"idle"} 
-      paymentType={"bitcoin"} 
+      paymentType={"onchain"} 
       amountless={false}
       initAmount={1000}
       note={null}
       err={null}
       amount={1000} 
       fee={textCurrencyFormatting(1234, 9.3927e-05, "USD")}
+      onSendMax={() => {}}
       {...defaultParams}
     />
     ))
     .add("bitcoin fee loading", () => (
     <SendBitcoinScreenJSX 
       status={"idle"}
-      paymentType={"bitcoin"}
+      paymentType={"onchain"}
       amountless={false}
       initAmount={1000}
       note={null}
