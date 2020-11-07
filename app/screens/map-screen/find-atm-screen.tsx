227d3bc4587149699b86c15d101d2056c7a83973
Nicolas Burtey <nicolas.burtey@gmail.com>
2020-11-07 16:35:49 -0600

    new build with android authorization

--- app/screens/map-screen/find-atm-screen.tsx
+++ app/screens/map-screen/find-atm-screen.tsx
@@ -3,10 +3,11 @@ import { useFocusEffect } from '@react-navigation/native'
 import { observer } from "mobx-react"
 import * as React from "react"
 import { useCallback, useState } from "react"
-import { StyleSheet } from "react-native"
+import { PermissionsAndroid, StyleSheet } from "react-native"
 import MapView, { Marker } from "react-native-maps"
 import { Screen } from "../../components/screen"
 import { StoreContext } from "../../models"
+import { isIos } from "../../utils/helper"
 
 
 const styles = StyleSheet.create({
@@ -20,8 +21,41 @@ export const MapScreen: React.FC = observer(({ }) => {
   const store = React.useContext(StoreContext)
 
   const [currentLocation, setCurrentLocation] = useState(null)
+  const [grantedPermission, setGrantedPermission] = useState(isIos ? true: false)
+
+  const requestCameraPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: "Cool Photo App Camera Permission",
+          message:
+            "Cool Photo App needs access to your camera " +
+            "so you can take awesome pictures.",
+          buttonNeutral: "Ask Me Later",
+          buttonNegative: "Cancel",
+          buttonPositive: "OK"
+        }
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        setGrantedPermission(true)
+        console.tron.log("You can use the camera");
+      } else {
+        console.tron.log("Camera permission denied");
+      }
+    } catch (err) {
+      console.tron.warn(err);
+    }
+  };
+  
 
   useFocusEffect(useCallback(() => {
+    requestCameraPermission()
+
+    if (!grantedPermission) {
+      return
+    }
+
     const watchId = Geolocation.watchPosition(info => {
       // console.tron.log(info)
       setCurrentLocation(<Marker 
@@ -35,7 +69,7 @@ export const MapScreen: React.FC = observer(({ }) => {
     return () => {
       Geolocation.clearWatch(watchId);
     }
-  }, []))
+  }, [grantedPermission]))
 
   // React.useLayoutEffect(() => {
   //   navigation.setOptions(
