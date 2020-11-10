af659f3839946c81bf73387a8e45b6e424b4de45
Nicolas Burtey <nicolas.burtey@gmail.com>
2020-11-10 14:11:43 -0600

    attempt to use invoices library but it relies on crypto from nodejs which is not available

--- app/utils/bolt11.ts
+++ app/utils/bolt11.ts
@@ -1,6 +1,19 @@
 import * as lightningPayReq from "bolt11"
+// const {parsePaymentRequest} = require('invoices');
 
 // helper for bolt11
+
+// export const getHashFromInvoice = (request) => {
+//   return parsePaymentRequest({request}).id
+// }
+
+// export const getDescription = (request): number | undefined => {
+//   return parsePaymentRequest({request}).description
+// }
+
+// export const getAmount = (request): number | undefined => {
+//   return parsePaymentRequest({request}).tokens
+// }
 
 export const getDescription = (decoded) => 
   decoded.tags.find(value => value.tagName === "description")?.data
