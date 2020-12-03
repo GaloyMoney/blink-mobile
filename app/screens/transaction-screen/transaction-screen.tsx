9da247c94e3d98ef13be8567b9b13fea3addbaa9
Nicolas Burtey <nicolas.burtey@gmail.com>
2020-12-03 14:56:21 -0600

    refactoring the transaction processing from the transaction screen

--- app/screens/transaction-screen/transaction-screen.tsx
+++ app/screens/transaction-screen/transaction-screen.tsx
@@ -8,9 +8,8 @@ import { TouchableOpacity } from "react-native-gesture-handler"
 import { IconTransaction } from "../../components/icon-transactions"
 import { Screen } from "../../components/screen"
 import { translate } from "../../i18n"
-import { StoreContext, useQuery } from "../../models"
+import { useQuery } from "../../models"
 import { palette } from "../../theme/palette"
-import { sameDay, sameMonth } from "../../utils/date"
 import { CurrencyType } from "../../utils/enum"
 import Icon from "react-native-vector-icons/Ionicons"
 
@@ -105,82 +104,20 @@ const styles = EStyleSheet.create({
   },
 })
 
-const formatTransactions = (transactions, prefCurrency) => {
-  const sections = []
-  const today = []
-  const yesterday = []
-  const thisMonth = []
-  const before = []
 
-  // FIXME: probably no longer necessary?
-  transactions = transactions.slice().sort((a, b) => (a.date > b.date ? -1 : 1)) // warning without slice?
-
-  const isToday = (tx) => {
-    return sameDay(tx.date, new Date())
-  }
-
-  const isYesterday = (tx) => {
-    return sameDay(tx.date, new Date().setDate(new Date().getDate() - 1))
-  }
-
-  const isThisMonth = (tx) => {
-    return sameMonth(tx.date, new Date())
-  }
-
-  while (transactions.length) {
-    // this could be optimized
-    let tx = transactions.shift()
-
-    // FIXME why is this needed?
-    // those are views and it seems are not available after some sort of serialization
-    tx = {...tx, date: tx.date, text: tx.text(prefCurrency), isReceive: tx.isReceive, date_format: tx.date_format}
-
-    // console.tron.log({tx})
-
-    if (isToday(tx)) {
-      today.push(tx)
-    } else if (isYesterday(tx)) {
-      yesterday.push(tx)
-    } else if (isThisMonth(tx)) {
-      thisMonth.push(tx)
-    } else {
-      before.push(tx)
-    }
-  }
-
-  if (today.length > 0) {
-    sections.push({ title: translate("PriceScreen.today"), data: today })
-  }
-
-  if (yesterday.length > 0) {
-    sections.push({ title: translate("PriceScreen.yesterday"), data: yesterday })
-  }
-
-  if (thisMonth.length > 0) {
-    sections.push({ title: translate("PriceScreen.thisMonth"), data: thisMonth })
-  }
-
-  if (before.length > 0) {
-    sections.push({ title: translate("PriceScreen.prevMonths"), data: before })
-  }
-
-  // console.tron.log({sections})
-  return sections
-}
 
 
 export const TransactionScreenDataInjected = observer(({navigation, route}) => {
-
-  const store = React.useContext(StoreContext)
-  const { query, error, loading, setQuery } = useQuery()
+  const { store, error, loading, setQuery } = useQuery()
 
   const refreshQuery = async () => {
     console.tron.log("refresh query from transaction screen")
     setQuery(store => store.mainQuery())
-    // await query.refetch()
   }
 
   const currency = "sat" // FIXME
+
+  console.tron.log({sections: store.transactionsSections()})
 
   return <TransactionScreen 
     navigation={navigation} 
@@ -190,7 +127,7 @@ export const TransactionScreenDataInjected = observer(({navigation, route}) => {
     prefCurrency={store.prefCurrency}
     nextPrefCurrency={store.nextPrefCurrency}
     onRefresh={refreshQuery}
-    transactions={store.wallets.get("BTC").transactions} 
+    sections={store.transactionsSections()}
   />
 })
 
@@ -217,35 +154,33 @@ const AccountDetailItem: React.FC<AccountDetailItemProps> = ({tx, navigation}) =
   />)
 }
 
-export const TransactionScreen = ({ transactions, refreshing, navigation, currency, onRefresh, error, prefCurrency, nextPrefCurrency }) => {
-  const sections = formatTransactions(transactions, prefCurrency)
-
-  return (
-    <Screen style={styles.screen}>
-      <SectionList
-        refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
-        renderItem={({ item, index, section }) => (
-          <AccountDetailItem currency={currency} navigation={navigation} tx={item} />
-        )}
-        ListHeaderComponent={() => <>
-          {error?.response?.errors?.map(({ message }) => 
-           <Text style={{color: palette.red, alignSelf: "center", paddingBottom: 18}} selectable={true}>{message}</Text>
-        )}
-        </>}
-        renderSectionHeader={({ section: { title } }) => (
-          <View style={styles.sectionHeaderContainer}>
-            <Text style={styles.sectionHeaderText}>{title}</Text>
-            <TouchableOpacity style={styles.row} onPress={nextPrefCurrency}>
-              <Text style={styles.sectionHeaderText}>{prefCurrency} </Text>
-              <Icon name={"ios-swap-vertical"} size={32} style={{top: -4}} />
-            </TouchableOpacity>
-          </View>
-        )}
-        ListEmptyComponent={<View style={styles.noTransactionView}>
-          <Text style={styles.noTransactionText}>{translate("TransactionScreen.noTransaction")}</Text>
-        </View>}
-        sections={sections}
-        keyExtractor={(item, index) => item + index}
-      />
-    </Screen>
-)}
+export const TransactionScreen = 
+  ({ refreshing, navigation, currency, onRefresh, error, prefCurrency, nextPrefCurrency, sections }) =>
+  <Screen style={styles.screen}>
+    <SectionList
+      refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
+      renderItem={({ item, index, section }) => (
+        <AccountDetailItem currency={currency} navigation={navigation} tx={item} />
+      )}
+      ListHeaderComponent={() => <>
+        {error?.response?.errors?.map(({ message }) => 
+          <Text style={{color: palette.red, alignSelf: "center", paddingBottom: 18}} selectable={true}>{message}</Text>
+      )}
+      </>}
+      initialNumToRender={12}
+      renderSectionHeader={({ section: { title } }) => (
+        <View style={styles.sectionHeaderContainer}>
+          <Text style={styles.sectionHeaderText}>{title}</Text>
+          <TouchableOpacity style={styles.row} onPress={nextPrefCurrency}>
+            <Text style={styles.sectionHeaderText}>{prefCurrency} </Text>
+            <Icon name={"ios-swap-vertical"} size={32} style={{top: -4}} />
+          </TouchableOpacity>
+        </View>
+      )}
+      ListEmptyComponent={<View style={styles.noTransactionView}>
+        <Text style={styles.noTransactionText}>{translate("TransactionScreen.noTransaction")}</Text>
+      </View>}
+      sections={sections}
+      keyExtractor={(item, index) => item + index}
+    />
+  </Screen>
