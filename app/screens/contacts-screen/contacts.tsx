6b5e70a6a8a959d0527d8f7e420af35ba2f5b517
Nicolas Burtey <nicolas.burtey@gmail.com>
2020-12-11 11:12:24 -0600

    first version working

--- app/screens/contacts-screen/contacts.tsx
+++ app/screens/contacts-screen/contacts.tsx
@@ -1,7 +1,7 @@
 import { observer } from "mobx-react"
 import * as React from "react"
-import { SectionList, Text, View } from "react-native"
-import { Avatar, ListItem } from "react-native-elements"
+import { Text, View } from "react-native"
+import { ListItem } from "react-native-elements"
 import EStyleSheet from "react-native-extended-stylesheet"
 import { FlatList } from "react-native-gesture-handler"
 import { Screen } from "../../components/screen"
@@ -22,16 +22,17 @@ export const ContactsScreenJSX = ({ list, navigation }) => {
   return (
   <Screen backgroundColor={"white"}>
     <FlatList
+      style={{paddingTop: 18}}
       data={list}
-      ListHeaderComponent={() => 
+      ListEmptyComponent={() => 
       <View style={{marginHorizontal: 12, marginTop: 32}}>
-        <Text style={{fontSize: 18}}>{"No contact yet.\n\nSend or receive payment with a username. They'll automatically be added here."}</Text>
+        <Text style={{fontSize: 18}}>{"No contact yet.\n\nSend or receive payment with a username. Usernames will automatically be added here."}</Text>
       </View>}
       renderItem={({ item }) => (
-        <ListItem bottomDivider onPress={() => navigation.navigate("sendBitcoin", {username: item.username})}>
+        <ListItem bottomDivider onPress={() => navigation.navigate("sendBitcoin", {username: item.id})}>
           {/* <Avatar source={{uri: .avatar_url}} /> */}
           <ListItem.Content>
-            <ListItem.Title>{item.username}</ListItem.Title>
+            <ListItem.Title>{item.id}</ListItem.Title>
             {/* <ListItem.Subtitle>{l.id}</ListItem.Subtitle> */}
           </ListItem.Content>
         </ListItem>
