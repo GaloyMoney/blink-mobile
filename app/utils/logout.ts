fe075fce76aa7818dcd8ddf03879b5c8e6a62a46
Nicolas Burtey <nicolas.burtey@gmail.com>
2020-12-18 15:41:36 -0600

    fix logout issue?

--- app/utils/logout.ts
+++ app/utils/logout.ts
@@ -1,15 +1,10 @@
-import AsyncStorage from "@react-native-community/async-storage"
-import { ROOT_STATE_STORAGE_KEY } from "../models"
-import { NETWORK_STRING } from "./network"
-import { Token } from "./token"
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 export const resetDataStore = async () => {
   try {
-    await AsyncStorage.multiRemove([ROOT_STATE_STORAGE_KEY, NETWORK_STRING]) // use storage.ts wrapper
-    const token = new Token()
-    await token.delete()
-    // TOKEN_KEY is stored at a separate location
-  } catch(e) {
-    console.tron.log(`error resetting RootStore: ${e}`)
+    await AsyncStorage.clear()
+    // await AsyncStorage.multiRemove([ROOT_STATE_STORAGE_KEY, NETWORK_STRING, TOKEN_KEY]) // use storage.ts wrapper
+  } catch(err) {
+    console.tron.log({err}, `error resetting RootStore`)
   }
 }
