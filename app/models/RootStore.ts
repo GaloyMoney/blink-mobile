9cd264e11547dc6994487126a497610586e1c5a9
Nicolas Burtey <nicolas.burtey@gmail.com>
2020-12-17 18:17:46 -0600

    upgrading from deprecated package

--- app/models/RootStore.ts
+++ app/models/RootStore.ts
@@ -1,4 +1,4 @@
-import AsyncStorage from "@react-native-community/async-storage"
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import analytics from '@react-native-firebase/analytics'
 import { filter, indexOf, map, sumBy } from "lodash"
 import { get, keys, set, values } from "mobx"
