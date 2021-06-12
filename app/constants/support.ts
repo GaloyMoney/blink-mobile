03765b26ebc3d60762acdcb2366351ba173b4e04
Guilherme Salomão <cryptosalomao@gmail.com>
2021-06-12 07:36:33 -0300

    fix: update to real contact number

--- app/constants/support.ts
+++ app/constants/support.ts
@@ -1,2 +1,2 @@
-export const WHATSAPP_CONTACT_NUMBER = '+5599985049935'
+export const WHATSAPP_CONTACT_NUMBER = '+50369835117'
 export const WHATSAPP_DEFAULT_CONTACT_MESSAGE = 'Hey there! I need some help with Bitcoin Beach Wallet'
