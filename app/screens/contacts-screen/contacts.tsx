d04dea7ce3431afd3c550acfef79a4c1cb35a452
Justin <justinbuhl93@gmail.com>
2021-06-15 10:17:44 -0500

    add search bar to contacts screen

--- app/screens/contacts-screen/contacts.tsx
+++ app/screens/contacts-screen/contacts.tsx
@@ -1,11 +1,18 @@
 import * as React from "react"
+import { useState } from "react"
 import { Text, View } from "react-native"
 import { ListItem } from "react-native-elements"
 import EStyleSheet from "react-native-extended-stylesheet"
+<<<<<<< HEAD
 import { FlatList } from "react-native-gesture-handler"
 import Icon from "react-native-vector-icons/Ionicons"
 import { useQuery, gql } from "@apollo/client"
+=======
+import { TextInput, FlatList } from "react-native-gesture-handler"
+import { InputSearch } from "../../components/input-search"
+>>>>>>> add search bar to contacts screen
 import { Screen } from "../../components/screen"
+import { translate } from "../../i18n"
 import { palette } from "../../theme/palette"
 
 const styles = EStyleSheet.create({
@@ -39,37 +46,97 @@ export const ContactsScreen = ({ navigation }: Props) => {
   `)
 
   const contacts = data?.me?.contacts ?? []
+  const [matchingContacts, setMatchingContacts] = useState(contacts)
+
+  const updateMatchingContacts = (searchText: String) => {
+    if (searchText) {
+      let splitSearchText = searchText.split(" ").filter(text => text.trim().length > 0)
+      let matchingContacts = contacts.filter((contact) => {
+        return searchTextArrayMatchesContact(splitSearchText, contact)
+      })
+      setMatchingContacts(matchingContacts)
+    } else {
+      setMatchingContacts(contacts)
+    }
+  }
+
+  const searchTextArrayMatchesContact = (searchTextArray: String[], contact) => {
+    return searchTextArray.some((searchWord) => {
+      return searchWordMatchesContact(searchWord, contact)
+    })
+  }
+
+  const searchWordMatchesContact = (searchWord: String, contact) => {
+    let contactNameMatchesSearchWord
+    let contactPrettyNameMatchesSearchWord
+
+    if (contact.name === null) {
+      contactNameMatchesSearchWord = false
+    } else {
+      contactNameMatchesSearchWord = contact.name.toLowerCase().includes(searchWord.toLocaleLowerCase())
+    }
+
+    if (contact.prettyName === null) {
+      contactPrettyNameMatchesSearchWord = false
+    } else {
+      contactPrettyNameMatchesSearchWord = contact.prettyName.toLowerCase().includes(searchWord.toLocaleLowerCase())
+    }
+
+    return contactNameMatchesSearchWord || contactPrettyNameMatchesSearchWord
+  }
+
+  let searchBarContent
+  let listEmptyContent
+
+  if (contacts.length > 0) {
+    searchBarContent = (
+      <View style={{paddingTop: 8}}>
+        <InputSearch
+          onUpdateText={text => updateMatchingContacts(text)}
+        />
+      </View>
+    )
+  } else {
+    searchBarContent = (null)
+  }
+
+  if (contacts.length > 0) {
+    listEmptyContent = (
+      <View style={{marginHorizontal: 26, marginTop: 8}}>
+        <Text style={{fontSize: 18}}>{translate("ContactsScreen.noMatchingContacts")}</Text>
+      </View>
+    )
+  } else {
+    listEmptyContent = (
+      <View style={{marginHorizontal: 12, marginTop: 32}}>
+        <Text style={{fontSize: 18}}>{translate("ContactsScreen.noContactsYet")}</Text>
+      </View>
+    )
+  }
 
   return (
-    <Screen backgroundColor={palette.lighterGrey}>
+  <Screen backgroundColor={palette.lighterGrey}>
+      {searchBarContent}
       <FlatList
-        style={styles.list}
-        data={contacts}
-        ListEmptyComponent={() => (
-          <View style={styles.emptyList}>
-            <Text style={styles.emptyListTitle}>
-              {
-                "No contact yet.\n\nSend or receive payment with a username. Usernames will automatically be added here."
-              }
-            </Text>
-          </View>
-        )}
-        renderItem={({ item }) => (
-          <ListItem
-            underlayColor={palette.lighterGrey}
-            activeOpacity={0.7}
-            style={styles.item}
-            containerStyle={styles.itemContainer}
-            onPress={() => navigation.navigate("contactDetail", { contact: item })}
-          >
-            {/* <Avatar source={{uri: .avatar_url}} /> */}
-            <Icon name="ios-person-outline" size={24} color={palette.green} />
-            <ListItem.Content>
-              <ListItem.Title>{item.prettyName}</ListItem.Title>
-            </ListItem.Content>
-          </ListItem>
-        )}
-        keyExtractor={(item) => item.id}
+      data={matchingContacts}
+      ListEmptyComponent={() => 
+        listEmptyContent
+      }
+      renderItem={({ item }) => (
+        <ListItem 
+          underlayColor={palette.lighterGrey}
+          activeOpacity={0.7}
+          style={{ marginHorizontal: 32, marginVertical: 8 }}
+          containerStyle={{borderRadius: 8}}
+          onPress={() => navigation.navigate("contactDetail", {contact: item})}>
+          {/* <Avatar source={{uri: .avatar_url}} /> */}
+          <Icon name={"ios-person-outline"} size={24} color={palette.green} />
+          <ListItem.Content>
+            <ListItem.Title>{item.prettyName}</ListItem.Title>
+          </ListItem.Content>
+        </ListItem>
+      )}
+      keyExtractor={(item) => item.id}
       />
     </Screen>
   )
