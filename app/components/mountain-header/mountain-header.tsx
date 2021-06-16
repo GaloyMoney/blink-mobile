497818b10b14e3ddab3b2f7a3dfc598f4433e6b5
Justin <justinbuhl93@gmail.com>
2021-06-16 12:55:27 -0500

    Spanish quiz translation integration

--- app/components/mountain-header/mountain-header.tsx
+++ app/components/mountain-header/mountain-header.tsx
@@ -2,6 +2,7 @@ import * as React from "react"
 import { View, Text } from "react-native";
 import Montain from "./mointains-cloud-01.svg"
 import EStyleSheet from "react-native-extended-stylesheet";
+import { translate } from "../../i18n"
 import { palette } from "../../theme/palette";
 
 const styles = EStyleSheet.create({
@@ -29,7 +30,7 @@ export const MountainHeader = ({amount, color}) => (
 <View style={{backgroundColor: color}}>
   <View style={[styles.topView]}>
     <View style={{alignItems: "center", paddingBottom: 16}}>
-      <Text style={styles.headerSection}>You Earned</Text>
+      <Text style={styles.headerSection}>{translate("EarnScreen.youEarned")}</Text>
       <Text style={styles.titleSection}>{amount} sats</Text>
     </View>
   </View>
