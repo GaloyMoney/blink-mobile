8968db6bb0070e26b900a1f3bed62524b19b3ffe
Guilherme Salomao <cryptosalomao@gmail.com>
2021-06-25 14:21:54 -0300

    chore: configure eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,29 @@
 module.exports = {
   env: {
     es2021: true,
-    'react-native/react-native': true,
+    "react-native/react-native": true,
   },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react-native/all',
-    'plugin:@typescript-eslint/recommended',
+    "prettier",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-native/all",
+    "plugin:@typescript-eslint/recommended",
   ],
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
   },
-  plugins: [
-    'react',
-    'react-native',
-    '@typescript-eslint',
-    'prettier',
-  ],
+  plugins: ["react", "react-native", "@typescript-eslint", "prettier"],
   rules: {
     "prettier/prettier": "error",
     "import/prefer-default-export": "off",
+    "@typescript-eslint/no-extra-semi": "off",
   },
   settings: {
-    'react-native/style-sheet-object-names': ['EStyleSheet'],
+    "react-native/style-sheet-object-names": ["EStyleSheet"],
   },
-};
+}
