8d6fe0bc30e5cdc8c63ef81b1b22918bb0ce09c3
Guilherme Salomao <cryptosalomao@gmail.com>
2021-06-29 11:21:26 -0300

    Add LNUrl payment status

--- app/constants/lnurl.ts
+++ app/constants/lnurl.ts
@@ -3,3 +3,9 @@ export const LNURL_PAY = "payRequest"
 export const SUPPORTED_PROTOCOLS = {
   [LNURL_PAY]: LNURL_PAY,
 }
+
+export enum PAYMENT_STATUS {
+  Error = "Error",
+  Pending = "Pending",
+  Success = "Success",
+}
