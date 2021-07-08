6b96a3204edbc48dbfd39dfaa212732a7835ee6a
Justin <justinbuhl93@gmail.com>
2021-07-08 10:28:40 -0500

    Rebase and use SearchBar

--- app/screens/contacts-screen/contacts.tsx
+++ app/screens/contacts-screen/contacts.tsx
@@ -1,30 +1,59 @@
 import * as React from "react"
 import { useState } from "react"
 import { Text, View } from "react-native"
-import { ListItem } from "react-native-elements"
+import { ListItem, SearchBar } from "react-native-elements"
 import EStyleSheet from "react-native-extended-stylesheet"
-<<<<<<< HEAD
 import { FlatList } from "react-native-gesture-handler"
 import Icon from "react-native-vector-icons/Ionicons"
 import { useQuery, gql } from "@apollo/client"
-=======
-import { TextInput, FlatList } from "react-native-gesture-handler"
-import { InputSearch } from "../../components/input-search"
->>>>>>> add search bar to contacts screen
+
 import { Screen } from "../../components/screen"
 import { translate } from "../../i18n"
 import { palette } from "../../theme/palette"
 
 const styles = EStyleSheet.create({
-  emptyList: { marginHorizontal: 12, marginTop: 32 },
+  emptyListNoContacts: {
+    marginHorizontal: 12,
+    marginTop: 32,
+  },
 
-  emptyListTitle: { fontSize: 18 },
+  emptyListNoMatching: {
+    marginHorizontal: 26,
+    marginTop: 8,
+  },
 
-  item: { marginHorizontal: 32, marginVertical: 8 },
+  emptyListTitle: {
+    fontSize: 18,
+  },
+
+  item: {
+    marginHorizontal: 32,
+    marginVertical: 8,
+  },
 
   itemContainer: { borderRadius: 8 },
 
-  list: { paddingTop: 18 },
+  searchBarContainer: {
+    backgroundColor: palette.lighterGrey,
+    borderBottomWidth: 0,
+    borderTopWidth: 0,
+    marginHorizontal: 26,
+    marginVertical: 8,
+    paddingTop: 8,
+  },
+
+  searchBarInputContainerStyle: {
+    backgroundColor: palette.white,
+  },
+
+  searchBarRightIconStyle: {
+    padding: 8,
+  },
+
+  searchBarText: {
+    color: palette.black,
+    textDecorationLine: "none",
+  },
 })
 
 type Props = {
@@ -47,11 +76,15 @@ export const ContactsScreen = ({ navigation }: Props) => {
 
   const contacts = data?.me?.contacts ?? []
   const [matchingContacts, setMatchingContacts] = useState(contacts)
+  const [searchText, setSearchText] = useState("")
 
-  const updateMatchingContacts = (searchText: String) => {
-    if (searchText) {
-      let splitSearchText = searchText.split(" ").filter(text => text.trim().length > 0)
-      let matchingContacts = contacts.filter((contact) => {
+  const updateMatchingContacts = (newSearchText: string) => {
+    setSearchText(newSearchText)
+    if (newSearchText) {
+      const splitSearchText = newSearchText
+        .split(" ")
+        .filter((text) => text.trim().length > 0)
+      const matchingContacts = contacts.filter((contact) => {
         return searchTextArrayMatchesContact(splitSearchText, contact)
       })
       setMatchingContacts(matchingContacts)
@@ -60,26 +93,30 @@ export const ContactsScreen = ({ navigation }: Props) => {
     }
   }
 
-  const searchTextArrayMatchesContact = (searchTextArray: String[], contact) => {
+  const searchTextArrayMatchesContact = (searchTextArray: string[], contact) => {
     return searchTextArray.some((searchWord) => {
       return searchWordMatchesContact(searchWord, contact)
     })
   }
 
-  const searchWordMatchesContact = (searchWord: String, contact) => {
+  const searchWordMatchesContact = (searchWord: string, contact) => {
     let contactNameMatchesSearchWord
     let contactPrettyNameMatchesSearchWord
 
     if (contact.name === null) {
       contactNameMatchesSearchWord = false
     } else {
-      contactNameMatchesSearchWord = contact.name.toLowerCase().includes(searchWord.toLocaleLowerCase())
+      contactNameMatchesSearchWord = contact.name
+        .toLowerCase()
+        .includes(searchWord.toLocaleLowerCase())
     }
 
     if (contact.prettyName === null) {
       contactPrettyNameMatchesSearchWord = false
     } else {
-      contactPrettyNameMatchesSearchWord = contact.prettyName.toLowerCase().includes(searchWord.toLocaleLowerCase())
+      contactPrettyNameMatchesSearchWord = contact.prettyName
+        .toLowerCase()
+        .includes(searchWord.toLocaleLowerCase())
     }
 
     return contactNameMatchesSearchWord || contactPrettyNameMatchesSearchWord
@@ -90,53 +127,65 @@ export const ContactsScreen = ({ navigation }: Props) => {
 
   if (contacts.length > 0) {
     searchBarContent = (
-      <View style={{paddingTop: 8}}>
-        <InputSearch
-          onUpdateText={text => updateMatchingContacts(text)}
-        />
-      </View>
+      <SearchBar
+        placeholder={translate("common.search")}
+        onChangeText={(text) => updateMatchingContacts(text)}
+        value={searchText}
+        platform="default"
+        round
+        lightTheme
+        showCancel={false}
+        showLoading={false}
+        containerStyle={styles.searchBarContainer}
+        inputContainerStyle={styles.searchBarInputContainerStyle}
+        inputStyle={styles.searchBarText}
+        rightIconContainerStyle={styles.searchBarRightIconStyle}
+      />
     )
   } else {
-    searchBarContent = (null)
+    searchBarContent = null
   }
 
   if (contacts.length > 0) {
     listEmptyContent = (
-      <View style={{marginHorizontal: 26, marginTop: 8}}>
-        <Text style={{fontSize: 18}}>{translate("ContactsScreen.noMatchingContacts")}</Text>
+      <View style={styles.emptyListNoMatching}>
+        <Text style={styles.emptyListTitle}>
+          {translate("ContactsScreen.noMatchingContacts")}
+        </Text>
       </View>
     )
   } else {
     listEmptyContent = (
-      <View style={{marginHorizontal: 12, marginTop: 32}}>
-        <Text style={{fontSize: 18}}>{translate("ContactsScreen.noContactsYet")}</Text>
+      <View style={styles.emptyListNoContacts}>
+        <Text style={styles.emptyListTitle}>
+          {translate("ContactsScreen.noContactsYet")}
+        </Text>
       </View>
     )
   }
 
   return (
-  <Screen backgroundColor={palette.lighterGrey}>
+    <Screen backgroundColor={palette.lighterGrey}>
       {searchBarContent}
       <FlatList
-      data={matchingContacts}
-      ListEmptyComponent={() => 
-        listEmptyContent
-      }
-      renderItem={({ item }) => (
-        <ListItem 
-          underlayColor={palette.lighterGrey}
-          activeOpacity={0.7}
-          style={{ marginHorizontal: 32, marginVertical: 8 }}
-          containerStyle={{borderRadius: 8}}
-          onPress={() => navigation.navigate("contactDetail", {contact: item})}>
-          {/* <Avatar source={{uri: .avatar_url}} /> */}
-          <Icon name={"ios-person-outline"} size={24} color={palette.green} />
-          <ListItem.Content>
-            <ListItem.Title>{item.prettyName}</ListItem.Title>
-          </ListItem.Content>
-        </ListItem>
-      )}
-      keyExtractor={(item) => item.id}
+        data={matchingContacts}
+        ListEmptyComponent={() => listEmptyContent}
+        renderItem={({ item }) => (
+          <ListItem
+            underlayColor={palette.lighterGrey}
+            activeOpacity={0.7}
+            style={styles.item}
+            containerStyle={styles.itemContainer}
+            onPress={() => navigation.navigate("contactDetail", { contact: item })}
+          >
+            {/* <Avatar source={{uri: .avatar_url}} /> */}
+            <Icon name={"ios-person-outline"} size={24} color={palette.green} />
+            <ListItem.Content>
+              <ListItem.Title>{item.prettyName}</ListItem.Title>
+            </ListItem.Content>
+          </ListItem>
+        )}
+        keyExtractor={(item) => item.id}
       />
     </Screen>
   )
