ee4b6c6b69633055d605cf4d313fd135ec6b6578
Justin <justinbuhl93@gmail.com>
2021-07-15 16:38:14 -0500

    Run prettier more broadly

--- e2e/List.e2e.js
+++ e2e/List.e2e.js
@@ -1,25 +1,24 @@
-describe('PostList', () => {
+describe("PostList", () => {
   beforeEach(async () => {
-    await device.reloadReactNative();
-  });
+    await device.reloadReactNative()
+  })
 
-  it('render a tappable list of posts', async () => {
+  it("render a tappable list of posts", async () => {
     // await expect(element(by.id('post-list'))).toBeVisible();
     // await waitFor(element(by.id('post-row-0')))
     //   .toBeVisible()
     //   .withTimeout(2000);
     // await element(by.id('post-row-0')).tap();
     // await expect(element(by.id('post-title'))).toBeVisible();
-  });
+  })
 
-  it('should have get started', async () => {
+  it("should have get started", async () => {
     expect(true).toBeTruthy()
     // console.log("get started start")
     // await expect(element(by.id('getStarted'))).toBeVisible();
     // console.log("get start middle")
     // const imagePath = await device.takeScreenshot('opened general section');
-
-  });
+  })
 
   // it('should show hello screen after tap', async () => {
   //   await element(by.id('hello_button')).tap();
@@ -30,4 +29,4 @@ describe('PostList', () => {
   //   await element(by.id('world_button')).tap();
   //   await expect(element(by.text('World!!!'))).toBeVisible();
   // });
-});
+})
--- utils/parseCSV.js
+++ utils/parseCSV.js
@@ -1,45 +1,46 @@
-const parse = require('csv-parse/lib/sync')
-var fs = require('fs');
-const { title } = require('process');
-const util = require('util')
+const parse = require("csv-parse/lib/sync")
+var fs = require("fs")
+const { title } = require("process")
+const util = require("util")
 
-const input = fs.readFileSync(__dirname + "/Galoy Illustrations Progress - Sheet1.csv", 
-  { encoding: 'utf8' }
-);
+const input = fs.readFileSync(__dirname + "/Galoy Illustrations Progress - Sheet1.csv", {
+  encoding: "utf8",
+})
 
 const records = parse(input, {
   columns: true,
-  skip_empty_lines: true
+  skip_empty_lines: true,
 })
 
 // console.log(records2[0])
 
-const formatArray = input => input.map(index => {
-  return {
-    id: index.ID_Screen,
-    type: "Text",
-    title: index["Screen Name"].trim(),
-    text: index.Content.trim(),
-    question: index.Question.trim(),
-    answers: [
-      index["Answer A"].trim(),
-      index["Answer B"].trim(),
-      index["Answer C"].trim(),
-    ],
-    feedback: [
-      index["Answer A Feedback"].trim(),
-      index["Answer B Feedback"].trim(),
-      index["Answer C Feedback"].trim(),
-    ]
-  }
-})
+const formatArray = (input) =>
+  input.map((index) => {
+    return {
+      id: index.ID_Screen,
+      type: "Text",
+      title: index["Screen Name"].trim(),
+      text: index.Content.trim(),
+      question: index.Question.trim(),
+      answers: [
+        index["Answer A"].trim(),
+        index["Answer B"].trim(),
+        index["Answer C"].trim(),
+      ],
+      feedback: [
+        index["Answer A Feedback"].trim(),
+        index["Answer B Feedback"].trim(),
+        index["Answer C Feedback"].trim(),
+      ],
+    }
+  })
 
 const section = (input) => ({
-  "meta": {
+  meta: {
     id: input[0].ID_Section,
     title: input[0].Section,
   },
-  "content": formatArray(input)
+  content: formatArray(input),
 })
 
 collections = new Set()
@@ -48,16 +49,14 @@ collections.delete("")
 
 const outputEarn = []
 
-collections.forEach(collection => {
-  const sectionArray = records.filter(index => index.ID_Section.includes(collection))
+collections.forEach((collection) => {
+  const sectionArray = records.filter((index) => index.ID_Section.includes(collection))
 
-  outputEarn.push(
-    section(sectionArray)
-  )
+  outputEarn.push(section(sectionArray))
 })
 
 const outputFilePath = __dirname + "/../app/i18n/en.json"
-const outputFile = fs.readFileSync(outputFilePath, { encoding: 'utf8' })
+const outputFile = fs.readFileSync(outputFilePath, { encoding: "utf8" })
 
 let finalJSON = JSON.parse(outputFile)
 finalJSON.EarnScreen.earns = outputEarn
