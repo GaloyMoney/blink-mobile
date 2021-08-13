3bcb098f2f8eba73ce52fdaad4baa1b599f957cd
Dan Webb <dan.webb8706@gmail.com>
2021-08-12 22:02:11 -0500

    Updated balance-header to render only after re-focus to not display balance for split second after changing settings added apollo GQL queries to abstract away direct input for async storage and added functions to call actions Added delay for loading tool for better user experience

--- app/graphql/client-only-query.ts
+++ app/graphql/client-only-query.ts
@@ -10,9 +10,70 @@ export const nextPrefCurrency = (): void => {
 }
 
 export const modalClipboardVisibleVar = makeVar(false)
+import { gql } from "@apollo/client"
+import { remove, save } from "../utils/storage"
+import { cache } from "./cache"
 
 export const LAST_CLIPBOARD_PAYMENT = gql`
   query LastClipboardPayment {
     lastClipboardPayment @client
   }
 `
+
+export const HIDE_BALANCE = gql`
+  query HideBalance {
+    hideBalanceSettings @client
+  }
+`
+
+export const WALKTHROUGH_TOOL_TIP = gql`
+  query WalkThroughToolTip {
+    walkThroughToolTipSettings @client
+  }
+`
+
+export const saveHideBalanceSettings = (status: boolean): boolean => {
+  try {
+    cache.writeQuery({
+      query: HIDE_BALANCE,
+      data: {
+        hideBalanceSettings: save("HIDE_BALANCE", true) ? status : remove("HIDE_BALANCE"),
+      },
+    })
+    if (!status) {
+      cache.evict({
+        id: "HideBalance",
+        fieldName: "hideBalanceSettings",
+        broadcast: false,
+      })
+      cache.gc()
+    }
+    return status
+  } catch {
+    return false
+  }
+}
+
+export const saveWalkThroughToolTipSettings = (status: boolean): boolean => {
+  try {
+    cache.writeQuery({
+      query: WALKTHROUGH_TOOL_TIP,
+      data: {
+        walkThroughToolTipSettings: save("WALKTHROUGH_TOOL_TIP", true)
+          ? status
+          : remove("WALKTHROUGH_TOOL_TIP"),
+      },
+    })
+    if (!status) {
+      cache.evict({
+        id: "WalkThroughToolTip",
+        fieldName: "walkThroughToolTipSettings",
+        broadcast: false,
+      })
+      cache.gc()
+    }
+    return status
+  } catch {
+    return false
+  }
+}
