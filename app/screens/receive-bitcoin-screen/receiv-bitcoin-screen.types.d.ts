16f43f6bab412ab28c51efb6715d071cd64ae48d
Marco Ferreira <m@rco.cc>
2021-08-22 22:09:37 +0200

    Cleanup receive bitcoin screen

--- app/screens/receive-bitcoin-screen/receiv-bitcoin-screen.types.d.ts
+++ app/screens/receive-bitcoin-screen/receiv-bitcoin-screen.types.d.ts
@@ -1,5 +1,8 @@
 type GetFullUriInput = {
   input: string
+  amount?: number
+  memo?: string
   uppercase?: boolean
   prefix?: boolean
+  type?: 'bitcoin' | 'lightning'
 }
---
+++ app/screens/receive-bitcoin-screen/utils.ts
@@ -0,0 +1,33 @@
+const TYPE_BITCOIN = "bitcoin"
+const TYPE_LIGHTNING = "lightning"
+
+const prefixByType = {
+  [TYPE_BITCOIN]: "bitcoin:",
+}
+
+export const getFullUri = ({
+  input,
+  amount,
+  memo,
+  uppercase = false,
+  prefix = true,
+  type = TYPE_BITCOIN
+}: GetFullUriInput): string => {
+  if (type === TYPE_LIGHTNING) {
+    // TODO add lightning:
+    return uppercase ? input.toUpperCase() : input
+  }
+
+  const uriPrefix = prefix ? prefixByType[type] : ""
+  const uri = `${uriPrefix}${input}`
+  const params = new URLSearchParams()
+
+  if (amount) params.append("amount", `${amount / 10 ** 8}`)
+
+  if (memo) {
+    params.append("message", encodeURI(memo))
+    return `${uri}?${params.toString()}`
+  }
+
+  return params.toString() ? `${uri}?${params.toString()}` : `${uri}`
+}
