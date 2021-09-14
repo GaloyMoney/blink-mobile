88ef9d3c287ddfc0f57bb10919b8a2e368fb6986
Samer Buna <samer@jscomplete.com>
2021-09-14 13:45:06 -0700

    Declare *.png/*.json for TypeScript

--- utils/declaration.d.ts
+++ utils/declaration.d.ts
@@ -4,3 +4,13 @@ declare module "*.svg" {
   const content: React.FC<SvgProps>
   export default content
 }
+
+declare module "*.png" {
+  const content: string
+  export default content
+}
+
+declare module "*.json" {
+  const content: string
+  export default content
+}
