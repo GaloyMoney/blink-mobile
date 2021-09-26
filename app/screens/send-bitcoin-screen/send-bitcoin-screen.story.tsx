f9aa9946c1ecdb2afbe4ef759ee10b87b1c72f80
daviroo <david.borthwick@gmail.com>
2021-09-26 17:27:15 +0100

    Fixed send bitcoin story

--- app/screens/send-bitcoin-screen/send-bitcoin-screen.story.tsx
+++ app/screens/send-bitcoin-screen/send-bitcoin-screen.story.tsx
@@ -17,6 +17,7 @@ const defaultParams = {
   invoice:
     "lnbc1p0kxle6pp5f936pgsmu7qjk0tph7uw8v9vhqlq3k37k3w6tulhy3rkf9ecqqcsdqu2askcmr9wssx7e3q2dshgmmndp5scqzpgxqyz5vqsp5z2hre6tcqrs67emaqvlewhf8klzm7hx5ly3scs7lkd4xlrgp52gs9qy9qsq0k704xkrj82sueam0zthx95luwdpeg60307x48gq3mrrwdnz6g0y2vpluy5z8keep463gdenrfy23f23mayhevs27ffjvu5nh0napacqjdeesl",
   address: "bcrt1qttw6a49hcayl0nx0xm7yur4pw9szftaanxpmd5",
+  destination: "test",
 }
 
 declare let module
@@ -33,6 +34,14 @@ storiesOf("Send bitcoin Screen", module)
       note=""
       err=""
       amount={1000}
+      primaryAmount={{
+        value: 1000,
+        currency: "BTC",
+      }}
+      secondaryAmount={{
+        value: 1000,
+        currency: "USD",
+      }}
       {...defaultParams}
     />
   ))
@@ -45,6 +54,14 @@ storiesOf("Send bitcoin Screen", module)
       note="this is my note"
       err={null}
       amount={1000}
+      primaryAmount={{
+        value: 1000,
+        currency: "BTC",
+      }}
+      secondaryAmount={{
+        value: 1000,
+        currency: "USD",
+      }}
       {...defaultParams}
     />
   ))
@@ -57,6 +74,14 @@ storiesOf("Send bitcoin Screen", module)
       note={null}
       err={null}
       amount={1000}
+      primaryAmount={{
+        value: 1000,
+        currency: "BTC",
+      }}
+      secondaryAmount={{
+        value: 1000,
+        currency: "USD",
+      }}
       {...defaultParams}
     />
   ))
@@ -69,6 +94,14 @@ storiesOf("Send bitcoin Screen", module)
       note={null}
       err={null}
       amount={1000}
+      primaryAmount={{
+        value: 1000,
+        currency: "BTC",
+      }}
+      secondaryAmount={{
+        value: 1000,
+        currency: "USD",
+      }}
       fee={textCurrencyFormatting(1234, 9.3927e-5, "USD")}
       {...defaultParams}
     />
@@ -83,6 +116,14 @@ storiesOf("Send bitcoin Screen", module)
       err={null}
       amount={1000}
       fee={undefined}
+      primaryAmount={{
+        value: 1000,
+        currency: "BTC",
+      }}
+      secondaryAmount={{
+        value: 1000,
+        currency: "USD",
+      }}
       {...defaultParams}
     />
   ))
