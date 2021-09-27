0c4609641f868231c1ec03ce59695bf4df5ac53e
daviroo <david.borthwick@gmail.com>
2021-09-27 12:15:54 +0100

    Fix username transaction translations (#207)
    Fixed translations

--- app/screens/transaction-detail-screen/transaction-detail-screen.tsx
+++ app/screens/transaction-detail-screen/transaction-detail-screen.tsx
@@ -136,7 +136,9 @@ export const TransactionDetailScreen: ScreenType = ({ route, navigation }: Props
         <Row entry={translate("common.date")} value={date_format} />
         {!isReceive && <Row entry={translate("common.fees")} value={feeEntry} />}
         <Row entry={translate("common.description")} value={description} />
-        {username && <Row entry="Paid to/from" value={username} />}
+        {username && (
+          <Row entry={translate("TransactionDetailScreen.paid")} value={username} />
+        )}
         <Row entry={translate("common.type")} value={type} />
         {hash && <Row entry="Hash" value={hash} />}
         {id && <Row entry="id" value={id} />}
