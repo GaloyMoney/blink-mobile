85644cc4f66741beed97533050eb06d86e87fc55
Sandipan Dey <hey@sandipan.dev>
2022-03-21 14:24:59 +0530

    test: added first test

--- __tests__/browserstack/first_test.spec.js
+++ __tests__/browserstack/first_test.spec.js
@@ -3,6 +3,6 @@ const assert = require('assert');
 describe("Home Page", () => {
   it("there is version headline in home page initial", async () => {
     const getStartedVersionHeadlineEl = await $('~GetStartedScreen.VersionHeadline');
-    await getStartedVersionHeadlineEl.waitForDisplayed({ timeout: 30000 });
+    await getStartedVersionHeadlineEl.waitForDisplayed({ timeout: 120 * 1000 });
   })
 })
