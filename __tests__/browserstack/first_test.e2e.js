e58207a427311fb4c5590093e5edefd06061ca2a
Sandipan Dey <hey@sandipan.dev>
2022-03-23 11:51:45 +0530

    test: increase timeout

---
+++ __tests__/browserstack/first_test.e2e.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+
+describe("Home Page", () => {
+  it("there is get started button in get started page", async () => {
+    const getStartedButtonEl = await $('~getStarted');
+    await getStartedButtonEl.waitForDisplayed({ timeout: 5 * 60 * 1000 });
+  })
+})
