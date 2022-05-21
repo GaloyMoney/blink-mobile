cb4ac5cb692cfd8f2f271286663b3dbd7279be9d
daviroo <david.borthwick@gmail.com>
2022-05-21 18:28:03 +0100

    feat: add transfer api requests (#451)

--- app/screens/transfer-screen/types.d.ts
+++ app/screens/transfer-screen/types.d.ts
@@ -1,3 +1,25 @@
 type TransferScreenProps = {
-  navigation: StackNavigationProp<TransferScreenParamList, "TransferScreen">
+  fromWallet: Wallet,
+  setFromWallet: (wallet: Wallet) => void,
+  toWallet: Wallet,
+  setToWallet: (wallet: Wallet) => void,
+  satAmount: number,
+  setSatAmount: (amount: number) => void,
+  satAmountInUsd: number,
+  setSatAmountInUsd: (amount: number) => void,
+  dollarAmount: number,
+  setDollarAmount: (amount: number) => void,
+  amountCurrency: string,
+  setAmountCurrency: (currency: string) => void,
+  nextStep: () => void,
+}
+
+type TransferConfirmationScreenProps = {
+  fromWallet: Wallet,
+  toWallet: Wallet,
+  satAmount: number,
+  satAmountInUsd: number,
+  dollarAmount: number,
+  amountCurrency: string,
+  nextStep: () => void,
 }
