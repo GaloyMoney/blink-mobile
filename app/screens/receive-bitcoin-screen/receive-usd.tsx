092674b6fc0746d01bcf12c9b4a117202b2b518a
Samer Buna <75209+samerbuna@users.noreply.github.com>
2022-06-02 03:01:51 -0700

    feat: input and memo input in receive screens (#477)
    * feat: show input amount on its own screen
    
    * feat: display invoice amount under QR code
    
    * feat: add memo to receive screens
    
    Co-authored-by: Samer Buna <samerbuna@users.noreply.github.com>

--- app/screens/receive-bitcoin-screen/receive-usd.tsx
+++ app/screens/receive-bitcoin-screen/receive-usd.tsx
@@ -3,21 +3,23 @@ import { useMySubscription } from "@app/hooks"
 import useMainQuery from "@app/hooks/use-main-query"
 import { palette } from "@app/theme"
 import { getFullUri, TYPE_LIGHTNING_USD } from "@app/utils/wallet"
-import { translateUnknown as translate, useMutation } from "@galoymoney/client"
-import debounce from "lodash.debounce"
-import React, { useCallback, useEffect, useMemo, useState } from "react"
-import { ActivityIndicator, Alert, Pressable, Share, View } from "react-native"
-import { Text } from "react-native-elements"
+import { GaloyGQL, translateUnknown as translate, useMutation } from "@galoymoney/client"
+import React, { useCallback, useEffect, useState } from "react"
+import { ActivityIndicator, Alert, Pressable, Share, TextInput, View } from "react-native"
+import { Button, Text } from "react-native-elements"
 import EStyleSheet from "react-native-extended-stylesheet"
 import QRView from "./qr-view"
 import Icon from "react-native-vector-icons/Ionicons"
 import Clipboard from "@react-native-community/clipboard"
-import CalculatorIcon from "@app/assets/icons/calculator.svg"
-import ChevronIcon from "@app/assets/icons/chevron.svg"
 import { FakeCurrencyInput } from "react-native-currency-input"
 import { CountdownCircleTimer } from "react-native-countdown-circle-timer"
 import Toast from "react-native-toast-message"
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view"
+import { usdAmountDisplay } from "@app/utils/currencyConversion"
+
+import CalculatorIcon from "@app/assets/icons/calculator.svg"
+import ChevronIcon from "@app/assets/icons/chevron.svg"
+import NoteIcon from "@app/assets/icons/note.svg"
 
 const styles = EStyleSheet.create({
   fieldsContainer: {
@@ -27,10 +29,18 @@ const styles = EStyleSheet.create({
   },
   field: {
     padding: 10,
-    height: "50rem",
     backgroundColor: palette.white,
     borderRadius: 10,
-    marginTop: 10,
+    marginBottom: 12,
+  },
+  inputForm: {
+    marginVertical: 20,
+  },
+
+  invoiceDisplay: {
+    padding: 15,
+    backgroundColor: palette.white,
+    borderRadius: 10,
   },
   infoText: {
     color: palette.midGrey,
@@ -38,10 +48,12 @@ const styles = EStyleSheet.create({
   },
   copyInvoiceContainer: {
     flex: 2,
+    marginLeft: 10,
   },
   shareInvoiceContainer: {
     flex: 2,
     alignItems: "flex-end",
+    marginRight: 10,
   },
   textContainer: {
     marginTop: 10,
@@ -81,87 +93,102 @@ const styles = EStyleSheet.create({
     alignItems: "center",
     marginTop: "20rem",
   },
+  button: {
+    height: 60,
+    borderRadius: 10,
+    marginTop: 40,
+  },
+  activeButtonStyle: {
+    backgroundColor: palette.lightBlue,
+  },
+  activeButtonTitleStyle: {
+    color: palette.white,
+    fontWeight: "bold",
+  },
+  invoiceInfo: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    marginHorizontal: 20,
+    marginTop: 5,
+  },
+  primaryAmount: {
+    fontWeight: "bold",
+  },
+  fieldTitleText: {
+    fontWeight: "bold",
+    color: palette.lapisLazuli,
+    marginBottom: 5,
+  },
 })
+
 const ReceiveUsd = () => {
   const [loading, setLoading] = useState(true)
   const [err, setErr] = useState("")
   const [lnNoAmountInvoiceCreate] = useMutation.lnNoAmountInvoiceCreate()
   const [lnUsdInvoiceCreate] = useMutation.lnUsdInvoiceCreate()
   const { usdWalletId } = useMainQuery()
-  const [invoice, setInvoice] = useState<{
-    paymentHash: string
-    paymentRequest: string
-  } | null>(null)
+  const [invoice, setInvoice] = useState<
+    GaloyGQL.LnInvoice | GaloyGQL.LnNoAmountInvoice | null
+  >(null)
   const [usdAmount, setUsdAmount] = useState(0)
-  const [memo] = useState("") // FIXME
+  const [memo, setMemo] = useState("")
   const { lnUpdate } = useMySubscription()
-  const [isAmountless, setIsAmountless] = useState(true)
+  const [showMemoInput, setShowMemoInput] = useState(false)
+  const [showAmountInput, setShowAmountInput] = useState(false)
 
-  const updateUsdValue = (newValue) => {
-    // eslint-disable-next-line no-negated-condition
-    if (!newValue) {
-      setUsdAmount(0)
-    } else {
-      setUsdAmount(newValue)
-    }
-  }
-  const updateInvoice = useMemo(
-    () =>
-      debounce(
-        async ({ walletId, usdAmount, memo }) => {
-          setLoading(true)
-          try {
-            if (usdAmount === 0) {
-              const {
-                data: {
-                  lnNoAmountInvoiceCreate: { invoice, errors },
-                },
-              } = await lnNoAmountInvoiceCreate({
-                variables: { input: { walletId, memo } },
-              })
-              if (errors && errors.length !== 0) {
-                console.error(errors, "error with lnNoAmountInvoiceCreate")
-                setErr(translate("ReceiveBitcoinScreen.error"))
-                return
-              }
-              setInvoice(invoice)
-            } else {
-              const {
-                data: {
-                  lnUsdInvoiceCreate: { invoice, errors },
-                },
-              } = await lnUsdInvoiceCreate({
-                variables: {
-                  input: { walletId, amount: parseFloat(usdAmount) * 100, memo },
-                },
-              })
-              if (errors && errors.length !== 0) {
-                console.error(errors, "error with lnInvoiceCreate")
-                setErr(translate("ReceiveBitcoinScreen.error"))
-                return
-              }
-              setInvoice(invoice)
-            }
-          } catch (err) {
-            console.error(err, "error with AddInvoice")
-            setErr(`${err}`)
-            throw err
-          } finally {
-            setLoading(false)
+  const updateInvoice = useCallback(
+    async ({ walletId, usdAmount, memo }) => {
+      setLoading(true)
+      try {
+        if (usdAmount === 0) {
+          const {
+            data: {
+              lnNoAmountInvoiceCreate: { invoice, errors },
+            },
+          } = await lnNoAmountInvoiceCreate({
+            variables: { input: { walletId, memo } },
+          })
+          if (errors && errors.length !== 0) {
+            console.error(errors, "error with lnNoAmountInvoiceCreate")
+            setErr(translate("ReceiveBitcoinScreen.error"))
+            return
           }
-        },
-        1000,
-        { trailing: true },
-      ),
+          setInvoice(invoice)
+        } else {
+          const {
+            data: {
+              lnUsdInvoiceCreate: { invoice, errors },
+            },
+          } = await lnUsdInvoiceCreate({
+            variables: {
+              input: { walletId, amount: parseFloat(usdAmount) * 100, memo },
+            },
+          })
+
+          if (errors && errors.length !== 0) {
+            console.error(errors, "error with lnInvoiceCreate")
+            setErr(translate("ReceiveBitcoinScreen.error"))
+            return
+          }
+          setInvoice(invoice)
+        }
+      } catch (err) {
+        console.error(err, "error with AddInvoice")
+        setErr(`${err}`)
+        throw err
+      } finally {
+        setLoading(false)
+      }
+    },
     [lnNoAmountInvoiceCreate, lnUsdInvoiceCreate],
   )
 
   useEffect((): void | (() => void) => {
-    if (usdWalletId) {
+    if (usdWalletId && !showAmountInput && !showMemoInput) {
       updateInvoice({ walletId: usdWalletId, usdAmount, memo })
-      return () => updateInvoice.cancel()
     }
-  }, [usdAmount, memo, updateInvoice, usdWalletId])
+  }, [usdAmount, memo, updateInvoice, usdWalletId, showAmountInput, showMemoInput])
 
   const invoicePaid =
     lnUpdate?.paymentHash === invoice?.paymentHash && lnUpdate?.status === "PAID"
@@ -196,6 +223,76 @@ const ReceiveUsd = () => {
     })
   }
 
+  if (showAmountInput) {
+    return (
+      <View style={styles.inputForm}>
+        <View style={styles.fieldsContainer}>
+          <Text style={styles.fieldTitleText}>{translate("Invoice Amount")}</Text>
+          <View style={styles.field}>
+            <FakeCurrencyInput
+              value={usdAmount}
+              onChangeValue={(newValue) => setUsdAmount(newValue ?? 0)}
+              prefix="$"
+              delimiter=","
+              separator="."
+              precision={2}
+              suffix=""
+              minValue={0}
+              style={styles.amountInput}
+              autoFocus
+            />
+          </View>
+
+          <Button
+            title={translate("Update Invoice")}
+            buttonStyle={[styles.button, styles.activeButtonStyle]}
+            titleStyle={styles.activeButtonTitleStyle}
+            onPress={() => setShowAmountInput(false)}
+          />
+        </View>
+      </View>
+    )
+  }
+
+  if (showMemoInput) {
+    return (
+      <View style={styles.inputForm}>
+        <View style={styles.fieldsContainer}>
+          <Text style={styles.fieldTitleText}>{translate("SendBitcoinScreen.note")}</Text>
+          <View style={styles.field}>
+            <TextInput
+              style={styles.noteInput}
+              placeholder={translate("SendBitcoinScreen.note")}
+              onChangeText={(note) => setMemo(note)}
+              value={memo}
+              multiline={true}
+              numberOfLines={3}
+              autoFocus
+            />
+          </View>
+
+          <Button
+            title={translate("Update Invoice")}
+            buttonStyle={[styles.button, styles.activeButtonStyle]}
+            titleStyle={styles.activeButtonTitleStyle}
+            onPress={() => setShowMemoInput(false)}
+          />
+        </View>
+      </View>
+    )
+  }
+
+  const displayAmount = () => {
+    if (!usdAmount) {
+      return undefined
+    }
+    return (
+      <>
+        <Text style={styles.primaryAmount}>{usdAmountDisplay(usdAmount)}</Text>
+      </>
+    )
+  }
+
   return (
     <KeyboardAwareScrollView>
       <Pressable onPress={copyToClipboard}>
@@ -209,8 +306,11 @@ const ReceiveUsd = () => {
           err={err}
         />
       </Pressable>
+
+      <View style={styles.invoiceInfo}>{displayAmount()}</View>
+
       <View style={styles.fieldsContainer}>
-        <View style={styles.field}>
+        <View style={styles.invoiceDisplay}>
           {!loading && <PaymentDestinationDisplay data={invoice?.paymentRequest} />}
           {loading && <ActivityIndicator />}
         </View>
@@ -235,9 +335,9 @@ const ReceiveUsd = () => {
         </View>
 
         <View style={styles.optionsContainer}>
-          <View style={styles.field}>
-            {isAmountless && (
-              <Pressable onPress={() => setIsAmountless(false)}>
+          {!showAmountInput && (
+            <View style={styles.field}>
+              <Pressable onPress={() => setShowAmountInput(true)}>
                 <View style={styles.fieldContainer}>
                   <View style={styles.fieldIconContainer}>
                     <CalculatorIcon />
@@ -252,23 +352,29 @@ const ReceiveUsd = () => {
                   </View>
                 </View>
               </Pressable>
-            )}
-            {!isAmountless && (
-              <FakeCurrencyInput
-                value={usdAmount}
-                onChangeValue={updateUsdValue}
-                prefix="$"
-                delimiter=","
-                separator="."
-                precision={2}
-                suffix=""
-                minValue={0}
-                style={styles.amountInput}
-              />
-            )}
-          </View>
+            </View>
+          )}
+
+          {!showMemoInput && (
+            <View style={styles.field}>
+              <Pressable onPress={() => setShowMemoInput(true)}>
+                <View style={styles.fieldContainer}>
+                  <View style={styles.fieldIconContainer}>
+                    <NoteIcon />
+                  </View>
+                  <View style={styles.fieldTextContainer}>
+                    <Text style={styles.fieldText}>{translate("Set a note/label")}</Text>
+                  </View>
+                  <View style={styles.fieldArrowContainer}>
+                    <ChevronIcon />
+                  </View>
+                </View>
+              </Pressable>
+            </View>
+          )}
         </View>
-        {!isAmountless && invoice?.paymentRequest && Boolean(usdAmount) && (
+
+        {invoice?.paymentRequest && Boolean(usdAmount) && (
           <View style={styles.countdownTimerContainer}>
             <CountdownCircleTimer
               key={invoice?.paymentRequest}
@@ -276,7 +382,9 @@ const ReceiveUsd = () => {
               duration={120}
               colors={"#004777"}
               size={80}
-              onComplete={() => updateInvoice({ walletId: usdWalletId, usdAmount, memo })}
+              onComplete={() => {
+                updateInvoice({ walletId: usdWalletId, usdAmount, memo })
+              }}
             >
               {({ remainingTime }) => <Text>{remainingTime}</Text>}
             </CountdownCircleTimer>
