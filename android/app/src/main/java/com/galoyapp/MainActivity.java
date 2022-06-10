affb8de491d5aca701c89e0020b41e63cd5512a2
daviroo <david.borthwick@gmail.com>
2022-06-10 10:38:22 +0100

    chore: Migrating off RNGHEnabledRootView

--- android/app/src/main/java/com/galoyapp/MainActivity.java
+++ android/app/src/main/java/com/galoyapp/MainActivity.java
@@ -6,17 +6,13 @@ import com.facebook.react.ReactActivity;
 
       import com.facebook.react.ReactActivityDelegate;
       import com.facebook.react.ReactRootView;
-      import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
     return new ReactActivityDelegate(this, getMainComponentName()) {
-      @Override
-      protected ReactRootView createRootView() {
-       return new RNGestureHandlerEnabledRootView(MainActivity.this);
-      }
+      // https://docs.swmansion.com/react-native-gesture-handler/docs/guides/migrating-off-rnghenabledroot/
     };
   }
 
