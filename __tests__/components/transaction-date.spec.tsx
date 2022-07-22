dd005a9093faba9b20afa38aa7293a5d8307fefe
daviroo <david.borthwick@gmail.com>
2022-07-22 10:22:22 +0100

    chore: Removed context from test

--- __tests__/components/transaction-date.spec.tsx
+++ __tests__/components/transaction-date.spec.tsx
@@ -1,15 +1,9 @@
 import * as React from "react"
 import { render } from "@testing-library/react-native"
 import { TransactionDate } from "../../app/components/transaction-date"
-import {
-  AppConfiguration,
-  AppConfigurationContext,
-  defaultConfiguration,
-} from "../../app/context/app-configuration"
 import { GaloyGQL, translateUnknown } from "@galoymoney/client"
 import { createMock } from "ts-auto-mock"
 import moment from "moment"
-import i18n from "i18n-js"
 
 describe("Display the createdAt date for a transaction", () => {
   it("Displays pending for a pending onchain transaction", () => {
@@ -18,37 +12,18 @@ describe("Display the createdAt date for a transaction", () => {
       createdAt: new Date().getDate(),
     })
 
-    const { queryAllByText } = render(
-      <AppConfigurationContext.Provider
-        value={{
-          appConfig: defaultConfiguration,
-          setAppConfig: (config: AppConfiguration) => {},
-        }}
-      >
-        <TransactionDate tx={mockedTransaction} />
-      </AppConfigurationContext.Provider>,
-    )
+    const { queryAllByText } = render(<TransactionDate tx={mockedTransaction} />)
     expect(queryAllByText(translateUnknown("common.pending"))).not.toBeNull()
   })
 
   it("Displays non-friendly date which adapts to timezone", () => {
-    i18n.locale = "en-GB"
-    const testTransactionCreatedAtDate = moment("2022-07-15T14:15:00+01:00")
+    const testTransactionCreatedAtDate = moment.utc("2022-07-15T14:15:00+00:00")
     const mockedTransaction = createMock<GaloyGQL.Transaction>({
       createdAt: testTransactionCreatedAtDate.unix(),
     })
 
-    const { queryByText } = render(
-      <AppConfigurationContext.Provider
-        value={{
-          appConfig: defaultConfiguration,
-          setAppConfig: (config: AppConfiguration) => {},
-        }}
-      >
-        <TransactionDate tx={mockedTransaction} />
-      </AppConfigurationContext.Provider>,
-    )
-    expect(queryByText("15 July 2022 14:15")).not.toBeNull()
+    const { queryByText } = render(<TransactionDate tx={mockedTransaction} />)
+    expect(queryByText("July 15, 2022 3:15 PM")).not.toBeNull()
   })
 
   it("Displays friendly date", () => {
@@ -58,14 +33,7 @@ describe("Display the createdAt date for a transaction", () => {
     })
 
     const { queryByText } = render(
-      <AppConfigurationContext.Provider
-        value={{
-          appConfig: defaultConfiguration,
-          setAppConfig: (config: AppConfiguration) => {},
-        }}
-      >
-        <TransactionDate tx={mockedTransaction} diffDate={true} friendly={true} />
-      </AppConfigurationContext.Provider>,
+      <TransactionDate tx={mockedTransaction} diffDate={true} friendly={true} />,
     )
     expect(
       queryByText(
