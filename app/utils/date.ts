5f6cbfad6351ecb60710fad3a8c091e520c854f1
daviroo <david.borthwick@gmail.com>
2022-07-23 11:56:58 +0100

    fix: Fixed moment locale issue (#580)

--- app/utils/date.ts
+++ app/utils/date.ts
@@ -1,5 +1,7 @@
 /* eslint-disable no-param-reassign */
 
+import moment from "moment"
+
 // refactor these utils
 export const sameDay = (d1: number, d2: number | Date): boolean => {
   const parsedD1 = new Date(1000 * d1) // XXX FIXME
@@ -40,3 +42,13 @@ export const parseDate = (timeStamp: number): Date => {
 }
 
 export const unixTime = (): number => Math.floor(Date.now() / 1000)
+
+export const toMomentLocale = (locale) => {
+  const newLocale = locale.replace("_", "-").toLowerCase()
+  const tryLocales = [newLocale, newLocale.split("-")[0]]
+  // eslint-disable-next-line @typescript-eslint/prefer-for-of, no-plusplus
+  for (let i = 0; i < tryLocales.length; i++) {
+    if (moment.locales().indexOf(tryLocales[i]) >= 0) return tryLocales[i]
+  }
+  return "en-us"
+}
