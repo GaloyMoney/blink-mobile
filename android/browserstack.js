a378e2a23df3d025083c3a8ca2fa16403a4f8cab
daviroo <david.borthwick@gmail.com>
2022-07-27 15:58:35 +0100

    chore: Fixed end to end testing framework

--- android/browserstack.js
+++ android/browserstack.js
@@ -5,7 +5,7 @@ exports.config = {
   key: process.env.BROWSERSTACK_ACCESS_KEY,
 
   updateJob: false,
-  specs: ['__tests__/browserstack/*.e2e.js'],
+  specs: ['../../__tests__/browserstack/*.e2e.js'],
   exclude: [],
 
   capabilities: [{
@@ -15,7 +15,6 @@ exports.config = {
     device: 'Google Pixel 3',
     os_version: "9.0",
     app: process.env.BROWSERSTACK_APP_ID,
-    'browserstack.local': true,
     'browserstack.debug': true
   }],
 
