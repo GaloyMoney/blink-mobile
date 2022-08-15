c30e8438c61bd7a0e939d19347b70fb5ef7dc1c4
daviroo <david.borthwick@gmail.com>
2022-08-15 10:39:05 +0100

    chore: Testing

--- __tests__/browserstack/unauthenticated/unauthenticated.e2e.js
+++ __tests__/browserstack/unauthenticated/unauthenticated.e2e.js
@@ -2,28 +2,30 @@ var client = require("@galoymoney/client")
 
 describe("Unauthenticated navigation renders correctly", () => {
   it("Welcome flow", async () => {
-    await $(`~${translateUnknown("GetStartedScreen.getStarted")}`).waitForExist()
-    await $(`~${translateUnknown("GetStartedScreen.getStarted")}`).click()
+    await $(`~${client.translateUnknown("GetStartedScreen.getStarted")}`).waitForExist()
+    await $(`~${client.translateUnknown("GetStartedScreen.getStarted")}`).click()
     expect(await $("~Bitcoin:").isDisplayed()).toBeTruthy()
     expect(
-      await $(`~${translateUnknown("WelcomeFirstScreen.care")}`).isDisplayed(),
+      await $(`~${client.translateUnknown("WelcomeFirstScreen.care")}`).isDisplayed(),
     ).toBeTruthy()
 
     browser.swipe({ x: 800, y: 1500 }, { x: 150, y: 1500 })
     expect(
-      await $(`~${translateUnknown("WelcomeFirstScreen.bank")}`).isDisplayed(),
+      await $(`~${client.translateUnknown("WelcomeFirstScreen.bank")}`).isDisplayed(),
     ).toBeTruthy()
     browser.swipe({ x: 800, y: 1500 }, { x: 150, y: 1500 })
     expect(
-      await $(`~${translateUnknown("WelcomeFirstScreen.before")}`).isDisplayed(),
+      await $(`~${client.translateUnknown("WelcomeFirstScreen.before")}`).isDisplayed(),
     ).toBeTruthy()
     browser.swipe({ x: 800, y: 1500 }, { x: 150, y: 1500 })
     expect(
-      await $(`~${translateUnknown("WelcomeFirstScreen.learn")}`).isDisplayed(),
+      await $(`~${client.translateUnknown("WelcomeFirstScreen.learn")}`).isDisplayed(),
     ).toBeTruthy()
     expect(
-      await $(`~${translateUnknown("WelcomeFirstScreen.learnToEarn")}`).isDisplayed(),
+      await $(
+        `~${client.translateUnknown("WelcomeFirstScreen.learnToEarn")}`,
+      ).isDisplayed(),
     ).toBeTruthy()
-    await $(`~${translateUnknown("WelcomeFirstScreen.learnToEarn")}`).click()
+    await $(`~${client.translateUnknown("WelcomeFirstScreen.learnToEarn")}`).click()
   })
 })
