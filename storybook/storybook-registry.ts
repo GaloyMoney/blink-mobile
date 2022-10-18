10f436263e6bf6fd3037b5374283208ee6ba0420
daviroo <david.borthwick@gmail.com>
2022-10-18 10:43:44 +0100

    chore: Removed story which wasn't working

--- storybook/storybook-registry.ts
+++ storybook/storybook-registry.ts
@@ -4,6 +4,5 @@ require("../app/components/large-button/large-button.story")
 require("../app/components/wallet-overview/wallet-overview.story")
 require("../app/components/wallet-summary/wallet-summary.story")
 require("../app/components/galoy-text-input/galoy-text-input.story")
-require("../app/screens/address-screen/address-screen.story")
 require("../app/components/galoy-button/galoy-button.story")
 export {}
