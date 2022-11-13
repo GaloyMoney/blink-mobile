5d4539c4dabecb58629c224bbfd47c205603056e
PhuongNguyen <ngocphuong.nguyen@soniqservices.de>
2022-11-13 19:03:13 +0700

    Change network, fastlane, ios Project config

--- app/config/galoy-instances.ts
+++ app/config/galoy-instances.ts
@@ -1,6 +1,14 @@
 import { INetwork } from "@app/types/network"
 import { scriptHostname } from "@app/utils/helper"
 
+const GRAPHQL_REGTEST_URI = `http://${scriptHostname()}:4002/graphql`
+const GRAPHQL_TESTNET_URI = "https://api.pvbtc1.staging.pvbtc.cloud/graphql"
+const GRAPHQL_MAINNET_URI = "https://api.production.pvbtc.cloud/graphql"
+
+const GRAPHQL_REGTEST_WS_URI = `ws://${scriptHostname()}:4002/graphql`
+const GRAPHQL_TESTNET_WS_URI = "wss://api.pvbtc1.staging.pvbtc.cloud/graphql"
+const GRAPHQL_MAINNET_WS_URI = "wss://api.production.pvbtc.cloud/graphql"
+
 export type GaloyInstance = {
   name: "BBW" | "Staging" | "Local"
   network: INetwork
@@ -12,19 +20,19 @@ export const GALOY_INSTANCES: GaloyInstance[] = [
   {
     name: "BBW",
     network: "mainnet",
-    graphqlUri: "https://api.mainnet.galoy.io/graphql",
-    graphqlWsUri: "wss://api.mainnet.galoy.io/graphql",
+    graphqlUri: GRAPHQL_MAINNET_URI,
+    graphqlWsUri: GRAPHQL_MAINNET_WS_URI,
   },
   {
     name: "Staging",
     network: "signet",
-    graphqlUri: "https://api.staging.galoy.io/graphql",
-    graphqlWsUri: "wss://api.staging.galoy.io/graphql",
+    graphqlUri: GRAPHQL_TESTNET_URI,
+    graphqlWsUri: GRAPHQL_TESTNET_WS_URI,
   },
   {
     name: "Local",
     network: "regtest",
-    graphqlUri: `http://${scriptHostname()}:4002/graphql`,
-    graphqlWsUri: `ws://${scriptHostname()}:4002/graphql`,
+    graphqlUri: GRAPHQL_REGTEST_URI,
+    graphqlWsUri: GRAPHQL_REGTEST_WS_URI,
   },
 ]
