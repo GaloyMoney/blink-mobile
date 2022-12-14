f8cc68f40f79ae949502937a12e2ef175d63eb11
Sam Peters <unclesamtoshi@protonmail.com>
2022-12-14 15:22:47 -0600

    feat: add JSON translation support

---
+++ app/i18n/export.ts
@@ -0,0 +1,32 @@
+import { readTranslationFromDisk } from 'typesafe-i18n/exporter'
+import fs from 'fs'
+import { BaseTranslation } from 'typesafe-i18n'
+
+const DEFAULT_RAW_EXPORT_PATH = `${__dirname}/raw-translations`
+
+const writeToFile = async (translation: BaseTranslation, locale: string, directory: string) => {
+    const filePath = `${directory}/${locale}.json`
+    const data = JSON.stringify(translation, null, 4) + '\n'
+    fs.writeFile(filePath, data, (err) => {
+        if (err) {
+            console.error(err)
+            throw err
+        }
+    })
+}
+
+const exportTranslationsForLocale = async (locale: string, directory: string) : Promise<boolean> => {
+    const mapping = await readTranslationFromDisk(locale)
+    const translation = Array.isArray(mapping.translations) ? mapping.translations[0] : mapping.translations
+    if (translation) {
+        await writeToFile(translation, locale, directory)
+        return true
+    }
+    return false
+}
+
+const locale = process.argv[2] || 'en'
+const exportDirectory = process.argv[3] || DEFAULT_RAW_EXPORT_PATH
+
+
+exportTranslationsForLocale(locale, exportDirectory)
