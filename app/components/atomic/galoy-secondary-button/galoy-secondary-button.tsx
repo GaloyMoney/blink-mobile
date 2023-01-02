c5dfe4697ae9719c109769c7bfbd46f3bd890909
Sam Peters <unclesamtoshi@protonmail.com>
2023-01-01 22:15:32 -0600

    chore: refactor to use makeStyles

--- app/components/atomic/galoy-secondary-button/galoy-secondary-button.tsx
+++ app/components/atomic/galoy-secondary-button/galoy-secondary-button.tsx
@@ -1,6 +1,6 @@
 import React, { FunctionComponent, PropsWithChildren } from "react"
-import { Button, ButtonProps, useTheme } from "@rneui/themed"
-import { StyleProp, TextStyle, TouchableHighlight } from "react-native"
+import { Button, ButtonProps, makeStyles, useTheme } from "@rneui/themed"
+import { TouchableHighlight } from "react-native"
 import { GaloyIcon, IconNamesType } from "../galoy-icon"
 
 declare module "@rneui/themed" {
@@ -9,46 +9,24 @@ declare module "@rneui/themed" {
   }
 }
 
-type AdditionalProps = {
+type GaloySecondaryButtonProps = PropsWithChildren<ButtonProps> & {
   iconName?: IconNamesType
   grey?: boolean
 }
 
-export const GaloySecondaryButton: FunctionComponent<
-  PropsWithChildren<ButtonProps> & AdditionalProps
-> = (props) => {
+export const GaloySecondaryButton: FunctionComponent<GaloySecondaryButtonProps> = (
+  props,
+) => {
   const { iconName, grey, ...remainingProps } = props
   const { theme } = useTheme()
-  const disabledStyle = {
-    opacity: 0.3,
-  }
-
-  const buttonStyle = {
-    backgroundColor: "transparent",
-  }
-
-  const buttonTitleStyle: StyleProp<TextStyle> = {
-    color: grey ? theme.colors.grey5 : theme.colors.primary,
-    fontSize: 18,
-    lineHeight: 22,
-    fontWeight: "600",
-  }
-
-  const disabledTitleStyle = {
-    color: grey ? theme.colors.grey5 : theme.colors.primary,
-  }
-
-  const iconStyle = {
-    marginRight: remainingProps.iconPosition === "right" ? 0 : 10,
-    marginLeft: remainingProps.iconPosition === "left" ? 0 : 10,
-  }
+  const styles = useStyles(props)
 
   const icon = iconName ? (
     <GaloyIcon
       name={iconName}
       size={18}
       color={grey ? theme.colors.grey5 : theme.colors.primary}
-      style={iconStyle}
+      style={styles.iconStyle}
     />
   ) : null
 
@@ -59,10 +37,32 @@ export const GaloySecondaryButton: FunctionComponent<
       activeOpacity={1}
       icon={icon}
       TouchableComponent={TouchableHighlight}
-      buttonStyle={buttonStyle}
-      disabledStyle={disabledStyle}
-      titleStyle={buttonTitleStyle}
-      disabledTitleStyle={disabledTitleStyle}
+      buttonStyle={styles.buttonStyle}
+      disabledStyle={styles.disabledStyle}
+      titleStyle={styles.buttonTitleStyle}
+      disabledTitleStyle={styles.disabledTitleStyle}
     />
   )
 }
+
+const useStyles = makeStyles((theme, props: GaloySecondaryButtonProps) => ({
+  disabledStyle: {
+    opacity: 0.3,
+  },
+  buttonStyle: {
+    backgroundColor: "transparent",
+  },
+  buttonTitleStyle: {
+    color: props.grey ? theme.colors.grey5 : theme.colors.primary,
+    fontSize: 18,
+    lineHeight: 22,
+    fontWeight: "600",
+  },
+  disabledTitleStyle: {
+    color: props.grey ? theme.colors.grey5 : theme.colors.primary,
+  },
+  iconStyle: {
+    marginRight: props.iconPosition === "right" ? 0 : 10,
+    marginLeft: props.iconPosition === "left" ? 0 : 10,
+  },
+}))
