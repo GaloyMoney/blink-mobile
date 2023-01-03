4ecea005818f9794812c4f3b00f6787c525b675c
Theophilus Isah <theophilus@Theophiluss-MBP.lan>
2023-01-03 15:20:55 +0100

    feat: add slider callback fn and other styles

--- app/components/atomic/galoy-slider-button/galoy-slider-button.story.tsx
+++ app/components/atomic/galoy-slider-button/galoy-slider-button.story.tsx
@@ -1,14 +1,27 @@
 import React from "react"
+
 import { storiesOf } from "@storybook/react-native"
+
 import { Story, StoryScreen, UseCase } from "../../../../storybook/views"
-import { Sliders } from "."
+import { Sliders } from "./"
+
+const onSlidingComplete = () => {
+  console.log("callback called!")
+  return null
+}
 
 storiesOf("Galoy Slider Button", module)
   .addDecorator((fn) => <StoryScreen>{fn()}</StoryScreen>)
   .add("Style Presets", () => (
     <Story>
       <UseCase text="Default">
-        <Sliders />
+        <Sliders callback={onSlidingComplete} text={"send"} />
+      </UseCase>
+      <UseCase text="Sent">
+        <Sliders callback={onSlidingComplete} text={"sent!"} />
+      </UseCase>
+      <UseCase text="Disabled">
+        <Sliders disabled={true} />
       </UseCase>
     </Story>
   ))
--- app/components/atomic/galoy-slider-button/galoy-slider-button.tsx
+++ app/components/atomic/galoy-slider-button/galoy-slider-button.tsx
@@ -1,95 +1,158 @@
 import React, { useState } from "react"
 import { View } from "react-native"
 
-import colors from "@app/rne-theme/colors"
 import { makeStyles, Slider, Text, useTheme } from "@rneui/themed"
 
 import { GaloyIcon } from "../galoy-icon"
 
-// eslint-disable-next-line @typescript-eslint/ban-types
-type SlidersComponentProps = {}
+type GaloySliderComponentProps = {
+  disabled?: boolean
+  callback?: () => void
+  text?: string
+}
 
-export const Sliders: React.FunctionComponent<SlidersComponentProps> = () => {
+enum sliderTextValues {
+  send = "slide to send",
+  sending = "send",
+  sent = "sent!",
+  failed = "failed",
+}
+
+const sliderStartValue = 6
+const sliderMaxValue = 100
+
+export const Sliders: React.FunctionComponent<GaloySliderComponentProps> = (props) => {
   const { theme } = useTheme()
-  const styles = useStyles(theme)
-  const [value, setValue] = useState(0)
+  const styles = useStyles(props)
 
-  //   const interpolate = (start: number, end: number) => {
-  //     let k = (value - 0) / 10 // 0 =>min  && 10 => MAX
-  //     return Math.ceil((1 - k) * start + k * end) % 256
-  //   }
+  const [value, setValue] = useState(sliderStartValue)
+  const [showIcon, setShowIcon] = useState<boolean>(true)
+  const [sliderText, setSliderText] = useState<sliderTextValues>(sliderTextValues.send)
 
-  //   const color = () => {
-  //     let r = interpolate(255, 0)
-  //     let g = interpolate(0, 255)
-  //     let b = interpolate(0, 0)
-  //     return `rgb(${r},${g},${b})`
-  //   }
+  React.useEffect(() => {
+    if (value === sliderStartValue) {
+      setSliderText(sliderTextValues.send)
+    }
+    if (value >= sliderMaxValue / 2) {
+      setValue(sliderMaxValue)
+    }
+  }, [value])
+
+  const onSlidingComplete = async () => {
+    if (value <= sliderMaxValue / 2) {
+      setValue(sliderStartValue)
+      return
+    }
+    setShowIcon(false)
+    if (props.callback) {
+      props.callback()
+      setSliderText(() => {
+        switch (props.text?.toLowerCase()) {
+          case sliderTextValues.send:
+            return sliderTextValues.send
+          case sliderTextValues.sending:
+            return sliderTextValues.sending
+          case sliderTextValues.sent:
+            return sliderTextValues.sent
+          case sliderTextValues.failed:
+            return sliderTextValues.failed
+          default:
+            return sliderTextValues.send
+        }
+      })
+    }
+  }
 
   return (
     <>
       <View style={styles.contentView}>
         <Slider
+          disabled={props.disabled}
+          onSlidingComplete={onSlidingComplete}
           value={value}
           onValueChange={setValue}
-          maximumValue={30}
+          maximumValue={100}
           minimumValue={0}
           step={1}
           maximumTrackTintColor={theme.colors.primary3}
           minimumTrackTintColor={theme.colors.primary5}
-          allowTouchTrack
+          thumbTintColor={theme.colors.primary5}
+          allowTouchTrack={false}
           trackStyle={styles.trackStyle}
           thumbStyle={styles.thumbStyle}
-          thumbTintColor={theme.colors.primary5}
           thumbTouchSize={styles.thumbTouchSize}
           thumbProps={{
             children: (
               <View style={styles.IconContainerStyle}>
-                <GaloyIcon name={"arrow-right"} size={30} color={colors.error4} />
+                {showIcon ? (
+                  <GaloyIcon name={"arrow-right"} size={30} color={theme.colors.white} />
+                ) : null}
               </View>
             ),
           }}
         />
-        <Text style={styles.SlideTextStyle}>Slide to send</Text>
+        <View style={styles.SlideTextContainer}>
+          <Text style={styles.SliderTextStyle}>{sliderText}</Text>
+          {sliderText === sliderTextValues.sending && (
+            <Text>
+              <GaloyIcon name={"arrow-right"} size={30} color={theme.colors.white} />
+            </Text>
+          )}
+        </View>
       </View>
-      {/* <Text style={{ paddingTop: 20, bottom: 50, color: colors.white }}>
-        Value: {value}
-      </Text> */}
     </>
   )
 }
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles((theme, props: GaloySliderComponentProps) => ({
   contentView: {
-    padding: 20,
+    padding: 0,
     width: "100%",
     justifyContent: "center",
     alignItems: "stretch",
   },
   trackStyle: {
     height: 40,
-    borderRadius: 40,
+    borderRadius: 100,
+    opacity: props.disabled ? 0.2 : undefined,
   },
   IconContainerStyle: {
-    bottom: -5,
-    right: -8,
+    bottom: 10,
+    left: props.disabled ? -12 : -10.5,
+    backgroundColor: props.disabled ? theme.colors.primary7 : theme.colors.primary5,
+    height: 20,
+    width: 20,
+    padding: 20,
+    borderRadius: 100,
+    justifyContent: "center",
+    alignItems: "center",
   },
   thumbStyle: {
-    height: 40,
-    width: 45,
-    color: theme.colors.verticalBlue,
-    borderRadius: 40,
-  },
-  SlideTextStyle: {
-    color: colors.white,
-    paddingTop: 20,
-    bottom: 55,
-    left: 115,
-    fontWeight: "600",
-    fontSize: 20,
+    height: 20,
+    width: 20,
+    left: -10,
   },
   thumbTouchSize: {
     width: 20,
     height: 20,
+  },
+  SlideTextContainer: {
+    width: "50%",
+    textAlign: "center",
+    alignItems: "flex-start",
+    justifyContent: "center",
+    paddingTop: 20,
+    bottom: 53.5,
+    left: 100,
+    fontWeight: "600",
+    fontSize: 20,
+    flexDirection: "row",
+  },
+  SliderTextStyle: {
+    color: theme.colors.white,
+    textAlign: "center",
+    fontWeight: "600",
+    fontSize: 20,
+    paddingRight: 10,
   },
 }))
