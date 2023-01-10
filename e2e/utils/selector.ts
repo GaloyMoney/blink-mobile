626ceac97b3d7dfe528c9dd2b32457f98b71b136
Nicolas Burtey <nicolas.burtey@gmail.com>
2023-01-10 19:00:10 +0000

    chore: not assume Button as default for ElementType
    I feel this doesn't really help on the learning curve.
    
    The selector will very often not be Button (more than 50% of the time). not putting an explicit "Button" for now. will refactor more as tests expand.

--- e2e/utils/selector.ts
+++ e2e/utils/selector.ts
@@ -1,6 +1,6 @@
-export function selector(id: string, iosType?: string, iosExtraXPath?: string) {
+export function selector(id: string, iosType: string, iosExtraXPath?: string) {
   if (process.env.E2E_DEVICE === "ios") {
-    return `//XCUIElementType${iosType ?? "Button"}[@name="${id}"]${iosExtraXPath ?? ""}`
+    return `//XCUIElementType${iosType}[@name="${id}"]${iosExtraXPath ?? ""}`
   }
   return `~${id}`
 }
