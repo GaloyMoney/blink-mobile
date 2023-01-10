26ec1d7e2655da7606aa500cb36e3cdc7c172db8
Theophilus <60826700+Extheoisah@users.noreply.github.com>
2023-01-10 02:34:20 +0100

    feat: add checkbox and radio button (#789)
    * feat: add checkbox and radio button
    
    * feat: add CheckBox Props
    
    * fix: remove duplicate checked state

---
+++ app/components/atomic/galoy-checkbox-button/galoy-checkbox-button.story.tsx
@@ -0,0 +1,37 @@
+import React from "react"
+
+import { storiesOf } from "@storybook/react-native"
+
+import { Story, StoryScreen, UseCase } from "../../../../storybook/views"
+import { GaloyCheckboxButton } from "./galoy-checkbox-button"
+
+const onPress = () => {
+  console.log("pressed")
+}
+
+storiesOf("Galoy Checkbox Button", module)
+  .addDecorator((fn) => <StoryScreen>{fn()}</StoryScreen>)
+  .add("Style Presets", () => (
+    <Story>
+      <UseCase text="Radio unchecked">
+        <GaloyCheckboxButton checked={false} buttonType={"radio"} onPress={onPress} />
+      </UseCase>
+      <UseCase text="Radio">
+        <GaloyCheckboxButton checked buttonType={"radio"} onPress={onPress} />
+      </UseCase>
+      <UseCase text="Checkbox unchecked">
+        <GaloyCheckboxButton checked={false} onPress={onPress} buttonType={"checkbox"} />
+      </UseCase>
+      <UseCase text="Checkbox checked">
+        <GaloyCheckboxButton checked buttonType={"checkbox"} onPress={onPress} />
+      </UseCase>
+      <UseCase text="Disabled Checkbox">
+        <GaloyCheckboxButton
+          checked={false}
+          buttonType={"checkbox"}
+          disabled={true}
+          title={"disabled"}
+        />
+      </UseCase>
+    </Story>
+  ))
