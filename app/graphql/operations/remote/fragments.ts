77abecb3b1e850c4833ba6be52fd9396bdb407dd
Sam Peters <unclesamtoshi@protonmail.com>
2023-01-14 15:52:08 -0600

    chore: add tx fragment

--- app/graphql/operations/remote/fragments.ts
+++ app/graphql/operations/remote/fragments.ts
@@ -1,6 +1,52 @@
 import { gql } from "@apollo/client"
 
 export default gql`
+  fragment Transaction on Transaction {
+    __typename
+    id
+    status
+    direction
+    memo
+    createdAt
+
+    settlementAmount
+    settlementFee
+    settlementCurrency
+    settlementPrice {
+      base
+      offset
+      currencyUnit
+      formattedAmount
+    }
+
+    initiationVia {
+      __typename
+      ... on InitiationViaIntraLedger {
+        counterPartyWalletId
+        counterPartyUsername
+      }
+      ... on InitiationViaLn {
+        paymentHash
+      }
+      ... on InitiationViaOnChain {
+        address
+      }
+    }
+    settlementVia {
+      __typename
+      ... on SettlementViaIntraLedger {
+        counterPartyWalletId
+        counterPartyUsername
+      }
+      ... on SettlementViaLn {
+        paymentSecret
+      }
+      ... on SettlementViaOnChain {
+        transactionHash
+      }
+    }
+  }
+
   fragment TransactionList on TransactionConnection {
     pageInfo {
       hasNextPage
@@ -11,49 +57,7 @@ export default gql`
     edges {
       cursor
       node {
-        __typename
-        id
-        status
-        direction
-        memo
-        createdAt
-
-        settlementAmount
-        settlementFee
-        settlementCurrency
-        settlementPrice {
-          base
-          offset
-          currencyUnit
-          formattedAmount
-        }
-
-        initiationVia {
-          __typename
-          ... on InitiationViaIntraLedger {
-            counterPartyWalletId
-            counterPartyUsername
-          }
-          ... on InitiationViaLn {
-            paymentHash
-          }
-          ... on InitiationViaOnChain {
-            address
-          }
-        }
-        settlementVia {
-          __typename
-          ... on SettlementViaIntraLedger {
-            counterPartyWalletId
-            counterPartyUsername
-          }
-          ... on SettlementViaLn {
-            paymentSecret
-          }
-          ... on SettlementViaOnChain {
-            transactionHash
-          }
-        }
+        ...Transaction
       }
     }
   }
