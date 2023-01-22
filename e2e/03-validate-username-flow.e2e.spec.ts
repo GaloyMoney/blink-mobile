a5c89164488290103b2a41538192fb1ab6a5c52a
Theophilus <60826700+Extheoisah@users.noreply.github.com>
2023-01-22 19:11:01 +0100

    chore: add contact validation (#937)
    * chore: add username validation
    
    * chore: rename variables and function

--- e2e/03-validate-username-flow.e2e.spec.ts
+++ e2e/03-validate-username-flow.e2e.spec.ts
@@ -1,6 +1,7 @@
 import { i18nObject } from "../app/i18n/i18n-util"
 import { loadLocale } from "../app/i18n/i18n-util.sync"
 import { selector, enter, goBack } from "./utils"
+import { checkContact } from "./utils/graphql"
 
 describe("Validate Username Flow", async () => {
   loadLocale("en")
@@ -41,10 +42,14 @@ describe("Validate Username Flow", async () => {
     const confirmButton = await $(
       selector(LL.SendBitcoinDestinationScreen.confirmModal.confirmButton(), "Button"),
     )
-    await checkBoxButton.waitForDisplayed({ timeout })
-    await checkBoxButton.click()
-    await confirmButton.isEnabled()
-    await confirmButton.click()
+    const isContactAvailable = await checkContact(username)
+    if (!isContactAvailable) {
+      if (checkBoxButton.isDisplayed() || confirmButton.isEnabled()) {
+        await checkBoxButton.click()
+        await confirmButton.isEnabled()
+        await confirmButton.click()
+      }
+    }
   })
 
   it("Go back home", async () => {
