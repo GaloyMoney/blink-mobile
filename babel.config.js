afaa0fb844da8b1aaa85aa1bb8b5abdbc91ce903
Nicolas Burtey <nicolas.burtey@gmail.com>
2023-02-03 12:06:25 +0000

    chore: upgrading RNReanimated. need to make VisionCamera work on Android

--- babel.config.js
+++ babel.config.js
@@ -2,10 +2,11 @@ module.exports = {
   presets: ["module:metro-react-native-babel-preset"],
   plugins: [
     [
-      "react-native-reanimated/plugin",
       {
         globals: ["__scanCodes"],
       },
+      "@babel/plugin-proposal-export-namespace-from",
+      "react-native-reanimated/plugin",
     ],
     [
       "module-resolver",
