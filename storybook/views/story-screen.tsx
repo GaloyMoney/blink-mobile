90636b7ef16370abdf3cdc628f2852d269f7bd10
Nicolas Burtey <nicolas.burtey@gmail.com>
2023-02-06 11:42:41 +0000

    chore: remove mock apollo client library
    this features is already given by apollo

--- storybook/views/story-screen.tsx
+++ storybook/views/story-screen.tsx
@@ -1,13 +1,11 @@
 import * as React from "react"
 import { ViewStyle, KeyboardAvoidingView, Platform } from "react-native"
-import { ApolloProvider } from "@apollo/client"
-import { createMockClient } from "mock-apollo-client"
+import { MockedProvider } from "@apollo/client/testing"
 const ROOT: ViewStyle = { backgroundColor: "#f0f0f0", flex: 1 }
 
 export interface StoryScreenProps {
   children?: React.ReactNode
 }
-const mockClient = createMockClient()
 const behavior = Platform.OS === "ios" ? "padding" : null
 export const StoryScreen = (props: StoryScreenProps) => (
   <KeyboardAvoidingView
@@ -15,6 +13,6 @@ export const StoryScreen = (props: StoryScreenProps) => (
     behavior={behavior || undefined}
     keyboardVerticalOffset={50}
   >
-    <ApolloProvider client={mockClient}>{props.children}</ApolloProvider>
+    <MockedProvider>{props.children}</MockedProvider>
   </KeyboardAvoidingView>
 )
