8f89674fabe795c6be4b9c60ff28031db6535b93
Nicolas Burtey <nicolas.burtey@gmail.com>
2023-02-08 08:14:44 +0000

    chore: use best practice for settings screen

--- app/screens/settings-screen/settings-screen.stories.tsx
+++ app/screens/settings-screen/settings-screen.stories.tsx
@@ -1,32 +1,26 @@
-import { withKnobs } from "@storybook/addon-knobs"
-import { storiesOf } from "@storybook/react-native"
 import * as React from "react"
 import { StoryScreen } from "../../../.storybook/views"
 import { SettingsScreenJSX } from "./settings-screen"
-import { action } from "@storybook/addon-actions"
 
 export default {
   title: 'Settings screen',
   component: SettingsScreenJSX,
 };
 
-const propsDefault = {
-  navigation: () => {},
-  language: 'en',
-  bankName: 'Galoy',
-  csvAction: () => {},
-  securityAction: () => {},
-  loadingCsvTransactions: false,
-};
+const Template = (args) => <StoryScreen>
+  <SettingsScreenJSX {...args} navigation={() => {}}
+    language={'en'}
+    bankName={'Galoy'}
+    csvAction={() => {}}
+    securityAction={() => {}}
+    loadingCsvTransactions={false} />
+</StoryScreen>
 
-export const NotLoggedIn = () => (<StoryScreen>
-    <SettingsScreenJSX isAuthed={false} phone={undefined} username={undefined} {...propsDefault} />
-  </StoryScreen>)
+export const NotLoggedIn = Template.bind({})
+NotLoggedIn.args = {isAuthed:false, phone:undefined, username: undefined}
 
-export const LoggedInButNoUsername = () => (<StoryScreen>
-  <SettingsScreenJSX phone="+16505551234" isAuthed={true} username={undefined} {...propsDefault} />
-    </StoryScreen>)
+export const LoggedInButNoUsername = Template.bind({})
+LoggedInButNoUsername.args = {isAuthed:true, phone:"+16505551234", username: undefined}
 
-export const LoggedInWithUsername = () =>  (<StoryScreen>
-    <SettingsScreenJSX isAuthed={true} phone="+16505551234" username="Joe" {...propsDefault} />
-  </StoryScreen>)
+export const LoggedInWithUsername = Template.bind({})
+LoggedInWithUsername.args = {isAuthed:true, phone:"+16505551234", username: "Joe"}
