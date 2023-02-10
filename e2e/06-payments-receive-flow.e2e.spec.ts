6429fbd0333d97067c87cacaddc683115e4dedb7
Theophilus <isahtheophilus@gmail.com>
2023-02-10 13:59:48 +0100

    chore: fix usd payment e2e test

--- e2e/06-payments-receive-flow.e2e.spec.ts
+++ e2e/06-payments-receive-flow.e2e.spec.ts
@@ -3,7 +3,7 @@ import { loadLocale } from "../app/i18n/i18n-util.sync"
 import { goBack, selector } from "./utils"
 import { payInvoice } from "./utils/graphql"
 
-describe("USD Receive Payment Flow", () => {
+describe("Receive Payment Flow", () => {
   loadLocale("en")
   const LL = i18nObject("en")
   const timeout = 30000
@@ -24,7 +24,7 @@ describe("USD Receive Payment Flow", () => {
         await okButton.click()
       }
       const allowButton = await $(selector("Allow", "Button"))
-      await allowButton.waitForDisplayed({ timeout: 8000 })
+      await allowButton.waitForDisplayed({ timeout })
       await allowButton.click()
     } catch (err) {
       // keep going, it might have already been clicked
@@ -32,13 +32,7 @@ describe("USD Receive Payment Flow", () => {
     }
   })
 
-  it("Click USD invoice button", async () => {
-    const usdInvoiceButton = await $(selector("USD Invoice Button", "Other"))
-    await usdInvoiceButton.waitForDisplayed({ timeout: 8000 })
-    await usdInvoiceButton.click()
-  })
-
-  it("Click Copy Invoice", async () => {
+  it("Click Copy BTC Invoice", async () => {
     let copyInvoiceButton
     if (process.env.E2E_DEVICE === "ios") {
       copyInvoiceButton = await $('(//XCUIElementTypeOther[@name="Copy Invoice"])[2]')
@@ -49,7 +43,64 @@ describe("USD Receive Payment Flow", () => {
     await copyInvoiceButton.click()
   })
 
-  it("Get Invoice from clipboard (android) or share link (ios)", async () => {
+  it("Get BTC Invoice from clipboard (android) or share link (ios)", async () => {
+    if (process.env.E2E_DEVICE === "ios") {
+      // on ios, get invoice from share link because copy does not
+      // work on physical device for security reasons
+      const shareButton = await $('(//XCUIElementTypeOther[@name="Share Invoice"])[2]')
+      await shareButton.waitForDisplayed({ timeout })
+      await shareButton.click()
+      const invoiceSharedScreen = await $('//*[contains(@name,"lntbs")]')
+      await invoiceSharedScreen.waitForDisplayed({
+        timeout: 8000,
+      })
+      invoice = await invoiceSharedScreen.getAttribute("name")
+      const closeShareButton = await $(selector("Close", "Button"))
+      await closeShareButton.waitForDisplayed({ timeout })
+      await closeShareButton.click()
+    } else {
+      // get from clipboard in android
+      const invoiceBase64 = await browser.getClipboard()
+      invoice = Buffer.from(invoiceBase64, "base64").toString()
+      expect(invoice).toContain("lntbs")
+    }
+  })
+
+  it("External User Pays the BTC Invoice through API", async () => {
+    const payResult = await payInvoice({ invoice, walletType: "BTC" })
+    if (payResult.data) {
+      if ("lnNoAmountInvoicePaymentSend" in payResult.data) {
+        paymentStatus = payResult.data?.lnNoAmountInvoicePaymentSend.status
+      }
+    }
+    expect(paymentStatus).toBe("SUCCESS")
+    expect(payResult).toBeTruthy()
+  })
+
+  it("Wait for Green check for BTC Payment", async () => {
+    const successCheck = await $(selector("Success Icon", "Other"))
+    await successCheck.waitForDisplayed({ timeout })
+    expect(await successCheck.isDisplayed()).toBeTruthy()
+  })
+
+  it("Click USD invoice button", async () => {
+    const usdInvoiceButton = await $(selector("USD Invoice Button", "Other"))
+    await usdInvoiceButton.waitForDisplayed({ timeout })
+    await usdInvoiceButton.click()
+  })
+
+  it("Click Copy USD Invoice", async () => {
+    let copyInvoiceButton
+    if (process.env.E2E_DEVICE === "ios") {
+      copyInvoiceButton = await $('(//XCUIElementTypeOther[@name="Copy Invoice"])[2]')
+    } else {
+      copyInvoiceButton = await $(selector("Copy Invoice", "Button"))
+    }
+    await copyInvoiceButton.waitForDisplayed({ timeout })
+    await copyInvoiceButton.click()
+  })
+
+  it("Get USD Invoice from clipboard (android) or share link (ios)", async () => {
     await browser.pause(2000)
     if (process.env.E2E_DEVICE === "ios") {
       // on ios, get invoice from share link because copy does not
@@ -73,7 +124,7 @@ describe("USD Receive Payment Flow", () => {
     }
   })
 
-  it("External User Pays the Invoice through API", async () => {
+  it("External User Pays the USD Invoice through API", async () => {
     const payResult = await payInvoice({ invoice, walletType: "USD" })
     if (payResult.data) {
       if ("lnNoAmountUsdInvoicePaymentSend" in payResult.data) {
@@ -84,15 +135,13 @@ describe("USD Receive Payment Flow", () => {
     expect(payResult).toBeTruthy()
   })
 
-  it.skip("Wait for Green check", async () => {
-    // TODO: we should wait for the green check
-    // but it is flaky on browserstack and not showing up in the UI
+  it("Wait for Green Check for USD Payment", async () => {
     const successCheck = await $(selector("Success Icon", "Other"))
     await successCheck.waitForDisplayed({ timeout })
     expect(await successCheck.isDisplayed()).toBeTruthy()
   })
 
-  it("go back to main screen", async () => {
+  it("Go back to main screen", async () => {
     const backButton = await $(goBack())
     await backButton.waitForDisplayed({ timeout })
     await backButton.click()
