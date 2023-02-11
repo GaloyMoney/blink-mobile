d1e2ff9e0c51938840ecd8aa000327b9f18f3790
Nicolas Burtey <nb@galoy.io>
2023-02-11 21:20:26 +0000

    chore: adding __tynename on the operations
    this make it easier to get some mock data from Postman

--- utils/create-gql.ts
+++ utils/create-gql.ts
@@ -2,6 +2,12 @@
 const { gqlPluckFromCodeStringSync } = require("@graphql-tools/graphql-tag-pluck")
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const fs = require("fs")
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { gql } = require("@apollo/client")
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { addTypenameToDocument } = require("@apollo/client/utilities")
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { print: printGql } = require("graphql")
 
 const filepath = "app/graphql/generated.ts"
 
@@ -11,4 +17,6 @@ const asStr = result
   .sort()
   .join("")
 
-fs.writeFileSync("app/graphql/generated.gql", asStr)
+const withTypename = printGql(addTypenameToDocument(gql(asStr)))
+
+fs.writeFileSync("app/graphql/generated.gql", withTypename)
