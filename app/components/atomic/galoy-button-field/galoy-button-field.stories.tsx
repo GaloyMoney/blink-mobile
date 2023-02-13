5711f0aec56d4822b0563dd041c52ad2abdc81c2
Nicolas Burtey <nb@galoy.io>
2023-02-13 12:03:27 +0000

    chore: better layout for storybook components

--- app/components/atomic/galoy-button-field/galoy-button-field.stories.tsx
+++ app/components/atomic/galoy-button-field/galoy-button-field.stories.tsx
@@ -1,66 +1,58 @@
 import React from "react"
-import { storiesOf } from "@storybook/react-native"
-import { Story, StoryScreen, UseCase } from "../../../../.storybook/views"
 import { GaloyButtonField } from "."
 import { View } from "react-native"
+import { palette } from "@app/theme"
+import { ComponentMeta } from "@storybook/react"
+import { Text } from "@rneui/base"
 
-const useCases = [
-  {
-    name: "Default",
-    props: {
-      placeholder: "Tap to select amount",
-      iconName: "pencil" as const,
-    },
-  },
-  {
-    name: "Disabled",
-    props: {
-      placeholder: "Tap to select amount",
-      iconName: "pencil" as const,
-      disabled: true,
-    },
-  },
-  {
-    name: "LN Invoice",
-    props: {
-      placeholder: "Tap to select amount",
-      value:
-        "lntb1u1pwz5w78pp5e8w8cr5c30xzws92v36sk45znhjn098rtc4pea6ertnmvu25ng3sdpywd6hyetyvf5hgueqv3jk6meqd9h8vmmfvdjsxqrrssy29mzkzjfq27u67evzu893heqex737dhcapvcuantkztg6pnk77nrm72y7z0rs47wzc09vcnugk2ve6sr2ewvcrtqnh3yttv847qqvqpvv398",
-      iconName: "info" as const,
-      highlightEnding: true,
-    },
-  },
-  {
-    name: "Secondary Value",
-    props: {
-      placeholder: "Tap to select amount",
-      value: "Bitcoin Account",
-      secondaryValue: "13,079,508 sats  (~$2,500.00 USD)",
-      iconName: "pencil" as const,
-    },
-  },
-  {
-    name: "Error",
-    props: {
-      placeholder: "Tap to select amount",
-      value: "Bitcoin Account",
-      secondaryValue: "13,079,508 sats  (~$2,500.00 USD)",
-      iconName: "pencil" as const,
-      error: true,
-    },
-  },
-]
+export default {
+  title: "Button/Field",
+  component: GaloyButtonField,
+  decorators: [
+    (Story) => <View style={{ padding: 10, margin: 10, width: 300, backgroundColor: palette.white }}>{Story()}</View>
+  ]
+} as ComponentMeta<typeof GaloyButtonField>
 
-storiesOf("Galoy Button Field", module)
-  .addDecorator((fn) => <StoryScreen>{fn()}</StoryScreen>)
-  .add("Style Presets", () => (
-    <Story>
-      {useCases.map(({ name, props }) => (
-        <UseCase text={name} key="1">
-          <View style={{ width: 300 }}>
-            <GaloyButtonField {...props} />
-          </View>
-        </UseCase>
-      ))}
-    </Story>
-  ))
+const defaultProps = {
+  placeholder: "Tap to select amount",
+  iconName: "pencil" as const,
+};
+
+const disabledProps = {
+  ...defaultProps,
+  disabled: true,
+};
+
+const lnInvoiceProps = {
+  ...defaultProps,
+  value: "lntb1u1pwz5w78pp5e8w8cr5c30xzws92v36sk45znhjn098rtc4pea6ertnmvu25ng3sdpywd6hyetyvf5hgueqv3jk6meqd9h8vmmfvdjsxqrrssy29mzkzjfq27u67evzu893heqex737dhcapvcuantkztg6pnk77nrm72y7z0rs47wzc09vcnugk2ve6sr2ewvcrtqnh3yttv847qqvqpvv398",
+  iconName: "info" as const,
+  highlightEnding: true,
+};
+
+const secondaryValueProps = {
+  ...defaultProps,
+  value: "Bitcoin Account",
+  secondaryValue: "13,079,508 sats  (~$2,500.00 USD)",
+  iconName: "pencil" as const,
+};
+
+const errorProps = {
+  ...secondaryValueProps,
+  error: true,
+};
+
+const Wrapper = ({children, text}) => <View style={{ marginVertical: 10 }}>
+  <Text style={{marginBottom: 10, marginTop: 5}}>{text}</Text>
+  {children}
+</View>
+
+export const Default = () => (
+  <View >
+    <Wrapper text="Default"><GaloyButtonField {...defaultProps} /></Wrapper>
+    <Wrapper text="Disaled"><GaloyButtonField {...disabledProps} /></Wrapper>
+    <Wrapper text="LN Invoice"><GaloyButtonField {...lnInvoiceProps} /></Wrapper>
+    <Wrapper text="Secondary Value"><GaloyButtonField {...secondaryValueProps} /></Wrapper>
+    <Wrapper text="Error"><GaloyButtonField {...errorProps} /></Wrapper>
+  </View>
+);
