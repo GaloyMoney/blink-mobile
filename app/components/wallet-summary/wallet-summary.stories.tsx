813f5b7d66c66ea603f3d48b706d2246d93a07c4
Nicolas Burtey <nb@galoy.io>
2023-02-13 22:45:04 +0000

    chore: misc update storybook

--- app/components/wallet-summary/wallet-summary.stories.tsx
+++ app/components/wallet-summary/wallet-summary.stories.tsx
@@ -3,27 +3,28 @@ import { storiesOf } from "@storybook/react-native"
 import { Story, StoryScreen, UseCase } from "../../../.storybook/views"
 import { WalletSummary } from "./wallet-summary"
 import { WalletType } from "@app/utils/enum"
+import { MockedProvider } from "@apollo/client/testing"
 
 declare let module
 
 storiesOf("Wallet Summary", module)
-  .addDecorator((fn) => <StoryScreen>{fn()}</StoryScreen>)
+  .addDecorator((fn) => <MockedProvider><StoryScreen>{fn()}</StoryScreen></MockedProvider>)
   .add("Style Presets", () => (
     <Story>
-      <UseCase text="BTC" usage="The default.">
-        <WalletSummary
-          amountType="SEND"
-          walletType={WalletType.BTC}
-          usdBalanceInDollars={129.2}
-          btcBalanceInSats={2000}
-        />
-      </UseCase>
-      <UseCase text="USD" usage="The default.">
-        <WalletSummary
-          amountType="SEND"
-          walletType={WalletType.USD}
-          usdBalanceInDollars={129.2}
-        />
-      </UseCase>
+        <UseCase text="BTC" usage="The default.">
+          <WalletSummary
+            amountType="SEND"
+            walletType={WalletType.BTC}
+            usdBalanceInDollars={129.2}
+            btcBalanceInSats={2000}
+          />
+        </UseCase>
+        <UseCase text="USD" usage="The default.">
+          <WalletSummary
+            amountType="SEND"
+            walletType={WalletType.USD}
+            usdBalanceInDollars={129.2}
+          />
+        </UseCase>
     </Story>
   ))
