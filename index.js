1c42ffc38f134441757170a3be2fbbcc59cdafae
Sam Peters <unclesamtoshi@protonmail.com>
2023-02-15 17:00:45 -0600

    chore: add logbox igore for known cases

--- index.js
+++ index.js
@@ -8,9 +8,14 @@
 //
 // It's easier just to leave it here.
 
-import { AppRegistry } from "react-native"
+import { AppRegistry, LogBox } from "react-native"
 import { App } from "./app/app.tsx"
 import * as React from "react"
+
+const ignoreLogs = [
+  /Non-serializable values were found in the navigation state. Check:\s*sendBitcoinDetails/, // SendBitcoin navigation values are not serializable to prevent boiler plate serialization and deserialization across the flow.
+]
+LogBox.ignoreLogs(ignoreLogs)
 
 /**
  * This needs to match what's found in your app_delegate.m and MainActivity.java.
