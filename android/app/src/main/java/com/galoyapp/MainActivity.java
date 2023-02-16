00c1216737a6af30017f3b4cba3a745af5dbb55d
nicolasburtey <nicolasburtey@users.noreply.github.com>
2023-02-16 10:23:39 +0000

    fix: crash on android when going to brackground (#1156)
    Co-authored-by: Nicolas Burtey <nb@galoy.io>

--- android/app/src/main/java/com/galoyapp/MainActivity.java
+++ android/app/src/main/java/com/galoyapp/MainActivity.java
@@ -5,6 +5,9 @@ import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
+// for react-native-screens
+import android.os.Bundle;
+
 public class MainActivity extends ReactActivity {
 
   /**
@@ -14,6 +17,12 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "GaloyApp";
+  }
+
+  //react-native-screens override
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(null);
   }
 
   /**
