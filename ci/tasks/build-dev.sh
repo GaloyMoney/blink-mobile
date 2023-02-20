46eb75361537822ee764610775995326093d016c
Kartik Shah <krtk.6160@gmail.com>
2023-02-20 13:29:05 +0530

    chore: tweak build csript

--- ci/tasks/build-dev.sh
+++ ci/tasks/build-dev.sh
@@ -2,4 +2,14 @@
 
 set -eu
 
+ref=$(cat repo/.git/ref)
+
+pipeline_id=$(curl -s --request POST \
+--url https://circleci.com/api/v2/project/gh//GaloyMoney/galoy-mobile/pipeline \
+--header "Circle-Token: $CIRCLECI_TOKEN" \
+--header 'content-type: application/json' \
+--data "{'branch':'circleci-job-for-concourse','parameters':{'version':"$ref"}}" | jq -r '.id')
+
+cat $pipeline_id
+
 sleep 50000
