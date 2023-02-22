5dd450b84ae641567e3f54a40bc1ba280397d9a6
Nicolas Burtey <nb@galoy.io>
2023-02-22 14:24:57 +0000

    chore: trying Sentry

--- app/app.tsx
+++ app/app.tsx
@@ -30,6 +30,7 @@ import { ErrorScreen } from "./screens/error-screen"
 import { PersistentStateProvider } from "./store/persistent-state"
 import { detectDefaultLocale } from "./utils/locale-detector"
 import { ThemeSyncGraphql } from "./utils/theme-sync"
+import DeviceInfo from "react-native-device-info"
 
 // FIXME should we only load the currently used local?
 // this would help to make the app load faster
@@ -39,27 +40,41 @@ import { ThemeSyncGraphql } from "./utils/theme-sync"
 // alternatively, could try loadAllLocalesAsync()
 loadAllLocales()
 
+import * as Sentry from "@sentry/react-native"
+
+DeviceInfo.isEmulator().then((isEmulator) => {
+  Sentry.init({
+    dsn: "https://e9dacf60cf5b489e900c502fdc09b45e@o4504724052639744.ingest.sentry.io/4504724057227264",
+    // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+    // We recommend adjusting this value in production.
+    tracesSampleRate: 1.0,
+    enableNative: !isEmulator,
+  })
+})
+
 /**
  * This is the root component of our app.
  */
 export const App = () => (
-  <PersistentStateProvider>
-    <TypesafeI18n locale={detectDefaultLocale()}>
-      <ThemeProvider theme={theme}>
-        <GaloyClient>
-          <ErrorBoundary FallbackComponent={ErrorScreen}>
-            <NavigationContainerWrapper>
-              <RootSiblingParent>
-                <AppStateWrapper />
-                <NotificationComponent />
-                <RootStack />
-                <GaloyToast />
-              </RootSiblingParent>
-            </NavigationContainerWrapper>
-          </ErrorBoundary>
-          <ThemeSyncGraphql />
-        </GaloyClient>
-      </ThemeProvider>
-    </TypesafeI18n>
-  </PersistentStateProvider>
+  <Sentry.TouchEventBoundary>
+    <PersistentStateProvider>
+      <TypesafeI18n locale={detectDefaultLocale()}>
+        <ThemeProvider theme={theme}>
+          <GaloyClient>
+            <ErrorBoundary FallbackComponent={ErrorScreen}>
+              <NavigationContainerWrapper>
+                <RootSiblingParent>
+                  <AppStateWrapper />
+                  <NotificationComponent />
+                  <RootStack />
+                  <GaloyToast />
+                </RootSiblingParent>
+              </NavigationContainerWrapper>
+            </ErrorBoundary>
+            <ThemeSyncGraphql />
+          </GaloyClient>
+        </ThemeProvider>
+      </TypesafeI18n>
+    </PersistentStateProvider>
+  </Sentry.TouchEventBoundary>
 )
