2d39bf694c429f833feaeda1f942f28d37cd0ef0
Kartik Shah <krtk.6160@gmail.com>
2023-02-23 00:30:28 +0530

    chore: update build scripts

--- ci/tasks/build-ios.sh
+++ ci/tasks/build-ios.sh
@@ -70,7 +70,15 @@ then
       | jq -r '.app_url'
   )
   echo browserstack_app_id:$BROWSERSTACK_APP_ID
-  GALOY_TEST_TOKENS=$GALOY_TEST_TOKENS && GALOY_TOKEN_2=$GALOY_TOKEN_2 && yarn test:browserstack:ios
+  GALOY_TEST_TOKENS=$GALOY_TEST_TOKENS && GALOY_TOKEN_2=$GALOY_TOKEN_2 && yarn test:browserstack:ios | tee browserstack_output.log
+  error_code=$?
+  SESSION_ID=$(cat browserstack_output.log | grep sessionId | head -n1 | sed -n "s/^.*'\(.*\)'.*$/\1/ p")
+  echo "Session ID"
+  echo $SESSION_ID
+  VIDEO_URL=$(curl -s -u "$BROWSERSTACK_USER:$BROWSERSTACK_ACCESS_KEY" -X GET "https://api-cloud.browserstack.com/app-automate/sessions/$SESSION_ID.json" | jq -r '.automation_session.video_url')
+  echo "Video URL"
+  echo $VIDEO_URL
+  exit $error_code
 elif [[ "$status" == "failed" ]]
 then
   echo "build failed"
