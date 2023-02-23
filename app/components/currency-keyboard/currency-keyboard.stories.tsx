23d23c884eaa82c822f5bb7db5b032adef465c04
nicolasburtey <nicolasburtey@users.noreply.github.com>
2023-02-23 14:23:26 +0000

    chore: importing currency keyboard (#1245)
    Co-authored-by: Nicolas Burtey <nb@galoy.io>

---
+++ app/components/currency-keyboard/currency-keyboard.stories.tsx
@@ -0,0 +1,16 @@
+import * as React from "react"
+import { storiesOf } from "@storybook/react-native"
+import { Story, StoryScreen, UseCase } from "../../../.storybook/views"
+import { CurrencyKeyboard } from "./currency-keyboard"
+
+declare let module
+
+storiesOf("Currency Keyboard", module)
+  .addDecorator((fn) => <StoryScreen>{fn()}</StoryScreen>)
+  .add("Style Presets", () => (
+    <Story>
+      <UseCase text="Currency Keyboard">
+        <CurrencyKeyboard onPress={(pressed) => console.log(pressed)} />
+      </UseCase>
+    </Story>
+  ))
