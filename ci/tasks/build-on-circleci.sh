904c7501adbb8b07360cc061cabbd722c6001ad4
Sandipan Dey <hey@sandipan.dev>
2023-02-24 16:53:50 +0530

    updates

--- ci/tasks/build-on-circleci.sh
+++ ci/tasks/build-on-circleci.sh
@@ -27,14 +27,14 @@ workflow_id=$(
   curl -s --request GET \
     --url https://circleci.com/api/v2/pipeline/$pipeline_id/workflow \
     --header "Circle-Token: $CIRCLECI_TOKEN" \
-    | jq -r '.items[] | select(.name == "build_ios_and_upload_to_bucket") | .id'
+    | jq -r '.items[] | select(.name == "build_$PLATFORM_and_upload_to_bucket") | .id'
 )
 
 pipeline_number=$(
   curl -s --request GET \
     --url https://circleci.com/api/v2/pipeline/$pipeline_id/workflow \
     --header "Circle-Token: $CIRCLECI_TOKEN" \
-    | jq -r '.items[] | select(.name == "build_ios_and_upload_to_bucket") | .pipeline_number'
+    | jq -r '.items[] | select(.name == "build_$PLATFORM_and_upload_to_bucket") | .pipeline_number'
 )
 
 echo workflow_id:$workflow_id
@@ -43,13 +43,13 @@ job_number=$(
   curl -s --request GET \
     --url https://circleci.com/api/v2/workflow/$workflow_id/job \
     --header "Circle-Token: $CIRCLECI_TOKEN" \
-    | jq -r '.items[] | select(.name == "build_ios") | .job_number'
+    | jq -r '.items[] | select(.name == "build_$PLATFORM") | .job_number'
 )
 
 echo job_number:$job_number
 
 echo "-------------------------------------------------------------------------------------------------------------------------------"
-echo "Waiting for CircleCI to finish Building iOS...."
+echo "Waiting for CircleCI to finish Building $PLATFORM...."
 echo "Follow Build Here: https://app.circleci.com/pipelines/github/GaloyMoney/galoy-mobile/$pipeline_number/workflows/$workflow_id/jobs/$job_number"
 echo "-------------------------------------------------------------------------------------------------------------------------------"
 
