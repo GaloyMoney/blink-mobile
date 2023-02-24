739558c0e682c0dce2587de2e0c335ad413fbf22
Sandipan Dey <hey@sandipan.dev>
2023-02-24 11:58:57 +0530

    feat: choose commit prerelease script

--- ci/tasks/choose-commit-prerelease.sh
+++ ci/tasks/choose-commit-prerelease.sh
@@ -1,1 +1,31 @@
 #!/bin/bash
+
+set -eu
+
+APK_COMMIT=$(cat ./built-dev-apk/version)
+IPA_COMMIT=$(cat ./built-dev-ipa/version)
+
+if [[ $APK_COMMIT == $IPA_COMMIT ]]; then
+  echo "Both APK and IPA are from same commit, no comparision necessary"
+fi
+
+pushd repo
+
+APK_TIMESTAMP=$(git show -s --format=%ct $APK_COMMIT)
+IPA_TIMESTAMP=$(git show -s --format=%ct $IPA_COMMIT)
+
+if [[ $APK_TIMESTAMP > $IPA_TIMESTAMP ]]; then
+  echo "APK at a greater timestamp than IPA, picking APK commit to prerelease"
+  git checkout $APK_COMMIT
+else
+  echo "IPA at a greater timestamp than APK, picking IPA commit to prerelease"
+  git checkout $IPA_COMMIT
+fi
+
+CHOSEN_COMMITID=$(git rev-parse --short HEAD)
+echo "Chosen CommitID: $CHOSEN_COMMITID"
+
+popd
+
+mv built-dev-android/app-universal-release.apk built-dev-android/BitcoinBeach-pre-$CHOSEN_COMMITID.apk
+mv built-dev-ipa/Bitcoin\ Beach.ipa built-dev-ipa/BitcoinBeach-pre-$CHOSEN_COMMITID.ipa
