6e3ece6f35dc265b7a64aed357bec7fd945c9929
Sandipan Dey <hey@sandipan.dev>
2023-02-24 10:00:36 +0530

    save

--- ci/tasks/dev-build-android.sh
+++ ci/tasks/dev-build-android.sh
@@ -41,33 +41,35 @@ job_number=$(
 
 echo job_number:$job_number
 
-
+echo "-------------------------------------------------------------------------------------------------------------------------------"
 echo "Waiting for CircleCI to finish Building Android...."
-echo "Live Build Here: https://app.circleci.com/pipelines/github/GaloyMoney/galoy-mobile/$pipeline_number/workflows/$workflow_id/jobs/$job_number"
-echo "[x] Sleeping for 5 mins"
-sleep 300
+echo "Follow Build Here: https://app.circleci.com/pipelines/github/GaloyMoney/galoy-mobile/$pipeline_number/workflows/$workflow_id/jobs/$job_number"
+echo "-------------------------------------------------------------------------------------------------------------------------------"
+
+echo "[•] Sleeping for 8 mins"
+sleep 480
 
 set +e
-for i in {1..60}; do
-  echo "Attempt ${i} to fetch job status"
+for i in {1..120}; do
+  echo "[•] Attempt ${i} to fetch job status"
   status=$(
     curl -s --request GET \
       --url https://circleci.com/api/v2/project/gh//GaloyMoney/galoy-mobile/job/$job_number \
       | jq -r '.status'
   )
-  if [[ $status != "running" && $status != "queued" ]]; then break; fi;
   echo "status:$status";
+  if [[ $status != "running" && $status != "queued" ]]; then break; fi;
   sleep 5
 done
 set -e
 
-echo "Final Status: $status"
+echo "[•] Final Status: $status"
 
 if [[ "$status" == "success" ]]
 then
-  echo "build succeeded!"
+  echo "[✓] Build succeeded!"
   exit 0
 else
-  echo "build failed!"
+  echo "[✗] Build failed!"
   exit 1
 fi
--- ci/tasks/dev-build-ios.sh
+++ ci/tasks/dev-build-ios.sh
@@ -42,33 +42,35 @@ job_number=$(
 
 echo job_number:$job_number
 
+echo "-------------------------------------------------------------------------------------------------------------------------------"
 echo "Waiting for CircleCI to finish Building iOS...."
-echo "Live Build Here: https://app.circleci.com/pipelines/github/GaloyMoney/galoy-mobile/$pipeline_number/workflows/$workflow_id/jobs/$job_number"
+echo "Follow Build Here: https://app.circleci.com/pipelines/github/GaloyMoney/galoy-mobile/$pipeline_number/workflows/$workflow_id/jobs/$job_number"
+echo "-------------------------------------------------------------------------------------------------------------------------------"
 
-echo "[x] Sleeping for 15 mins"
-sleep 900
+echo "[x] Sleeping for 12 mins"
+sleep 720
 
 set +e
 for i in {1..60}; do
-  echo "Attempt ${i} to fetch job status"
+  echo "[x] Attempt ${i} to fetch job status"
   status=$(
     curl -s --request GET \
       --url https://circleci.com/api/v2/project/gh//GaloyMoney/galoy-mobile/job/$job_number \
       | jq -r '.status'
   )
-  if [[ $status != "running" && $status != "queued" ]]; then break; fi;
   echo "status:$status";
+  if [[ $status != "running" && $status != "queued" ]]; then break; fi;
   sleep 5
 done
 set -e
 
-echo "Final Status: $status"
+echo "[•] Final Status: $status"
 
 if [[ "$status" == "success" ]]
 then
-  echo "build succeeded!"
+  echo "[✓] Build succeeded!"
   exit 0
 else
-  echo "build failed!"
+  echo "[✗] Build failed!"
   exit 1
 fi
