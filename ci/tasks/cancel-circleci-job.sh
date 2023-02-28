8fe2639faaf8533d75e574a478d39b103671f757
Sandipan Dey <hey@sandipan.dev>
2023-02-28 13:04:43 +0530

    ci(fix): circleci url

--- ci/tasks/cancel-circleci-job.sh
+++ ci/tasks/cancel-circleci-job.sh
@@ -3,5 +3,5 @@
 job_number=$(cat job-number/number)
 
 curl --request POST \
-  --url https://circleci.com/api/v2/project/job/$job_number/cancel \
+  --url https://circleci.com/api/v2/project/gh//GaloyMoney/galoy-mobile/job/$job_number/cancel \
   --header "Circle-Token: $CIRCLECI_TOKEN"
