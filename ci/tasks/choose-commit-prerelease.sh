110e6158b2b93a50cacbe68f9cefb8412aa7b310
Sandipan Dey <hey@sandipan.dev>
2023-03-01 02:15:28 +0530

    fix: branching

--- ci/tasks/choose-commit-prerelease.sh
+++ ci/tasks/choose-commit-prerelease.sh
@@ -20,7 +20,7 @@ IPA_TIMESTAMP=$(git show -s --format=%ct $IPA_COMMIT)
 if [[ $APK_TIMESTAMP > $IPA_TIMESTAMP ]]; then
   echo "APK at a greater timestamp than IPA, picking APK commit to prerelease"
   git checkout $APK_COMMIT
-else if [[ $APK_TIMESTAMP < $IPA_TIMESTAMP ]]; then
+elif [[ $APK_TIMESTAMP < $IPA_TIMESTAMP ]]; then
   echo "IPA at a greater timestamp than APK, picking IPA commit to prerelease"
   git checkout $IPA_COMMIT
 else
