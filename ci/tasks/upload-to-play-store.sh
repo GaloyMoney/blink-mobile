6dbc52139a9d9f0452097be61afcb5f731862749
Sandipan Dey <hey@sandipan.dev>
2023-02-28 07:52:17 +0530

    wasnt base64

--- ci/tasks/upload-to-play-store.sh
+++ ci/tasks/upload-to-play-store.sh
@@ -6,7 +6,7 @@ cp built-prod-apk/app-universal-release.apk repo/android
 
 pushd repo/android
 
-echo $ANDROID_SERVICE_ACCOUNT_UPLOAD | base64 -d > galoyapp-2e25e160d4ba.json
+echo $ANDROID_SERVICE_ACCOUNT_UPLOAD > galoyapp-2e25e160d4ba.json
 
 bundle install
 bundler exec fastlane play_store_upload
