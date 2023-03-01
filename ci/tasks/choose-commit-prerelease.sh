e3d732b14a26c394f41a6a437362d59fad691753
Sandipan Dey <hey@sandipan.dev>
2023-03-01 12:12:40 +0530

    ci: upload to app store via circleci (#1292)
    * ci: fix choose commit prerelease task
    
    * fix: message
    
    * fix: branching
    
    * chore: adds upload to app store job in circleci config
    
    * chore: circleci driver script for upload

--- ci/tasks/choose-commit-prerelease.sh
+++ ci/tasks/choose-commit-prerelease.sh
@@ -2,10 +2,10 @@
 
 set -eu
 
-[[ "$(cat ./built-dev-apk/url)" =~ "dev/android/galoy-mobile-.+-v(.+)/apk" ]]
+[[ "$(cat ./built-dev-apk/url)" =~ dev/android/galoy-mobile-.+-v(.+)/apk ]]
 APK_COMMIT=${BASH_REMATCH[1]}
 
-[[ "$(cat ./built-dev-ipa/url)" =~ "dev/ios/galoy-mobile-.+-v(.+)/Bitcoin" ]]
+[[ "$(cat ./built-dev-ipa/url)" =~ dev/ios/galoy-mobile-.+-v(.+)/Bitcoin ]]
 IPA_COMMIT=${BASH_REMATCH[1]}
 
 if [[ $APK_COMMIT == $IPA_COMMIT ]]; then
@@ -20,8 +20,10 @@ IPA_TIMESTAMP=$(git show -s --format=%ct $IPA_COMMIT)
 if [[ $APK_TIMESTAMP > $IPA_TIMESTAMP ]]; then
   echo "APK at a greater timestamp than IPA, picking APK commit to prerelease"
   git checkout $APK_COMMIT
-else
+elif [[ $APK_TIMESTAMP < $IPA_TIMESTAMP ]]; then
   echo "IPA at a greater timestamp than APK, picking IPA commit to prerelease"
+  git checkout $IPA_COMMIT
+else
   git checkout $IPA_COMMIT
 fi
 
--- ci/tasks/upload-to-app-store.sh
+++ ci/tasks/upload-to-app-store.sh
@@ -2,9 +2,76 @@
 
 set -eu
 
-cp built-prod-ipa/Bitcoin\ Beach.ipa repo/ios
+git_ref=$(cat ./built-prod-ipa/version) # tag
+ipa_gcs_url=$(cat ./built-prod-ipa/url)
 
-pushd repo/ios
+pipeline_id=$(
+  curl -s --request POST \
+    --url https://circleci.com/api/v2/project/gh//GaloyMoney/galoy-mobile/pipeline \
+    --header "Circle-Token: $CIRCLECI_TOKEN" \
+    --header 'content-type: application/json' \
+    --data '{"branch":"main","parameters":{ "task": "upload_to_app_store", "gcs_url": "'$ipa_gcs_url'", "git_ref": "'"$git_ref"'" }}' \
+    | jq -r '.id'
+)
 
-bundle install
-bundle exec fastlane app_store_upload
+echo pipeline_id:$pipeline_id
+sleep 1
+
+workflow_id=$(
+  curl -s --request GET \
+    --url https://circleci.com/api/v2/pipeline/$pipeline_id/workflow \
+    --header "Circle-Token: $CIRCLECI_TOKEN" \
+    | jq --arg name "upload_to_app_store" -r '.items[] | select(.name == $name) | .id'
+)
+
+pipeline_number=$(
+  curl -s --request GET \
+    --url https://circleci.com/api/v2/pipeline/$pipeline_id/workflow \
+    --header "Circle-Token: $CIRCLECI_TOKEN" \
+    | jq --arg name "upload_to_app_store" -r '.items[] | select(.name == $name) | .pipeline_number'
+)
+
+echo workflow_id:$workflow_id
+
+job_number=$(
+  curl -s --request GET \
+    --url https://circleci.com/api/v2/workflow/$workflow_id/job \
+    --header "Circle-Token: $CIRCLECI_TOKEN" \
+    | jq --arg name "upload_to_app_store" -r '.items[] | select(.name == $name) | .job_number'
+)
+
+echo $job_number > ../job-number/number
+echo job_number:$job_number
+
+echo "-------------------------------------------------------------------------------------------------------------------------------"
+echo "Waiting for CircleCI to finish Uploading IPA...."
+echo "Follow Job Here: https://app.circleci.com/pipelines/github/GaloyMoney/galoy-mobile/$pipeline_number/workflows/$workflow_id/jobs/$job_number"
+echo "-------------------------------------------------------------------------------------------------------------------------------"
+
+echo "[•] Sleeping for 2 mins"
+sleep 120
+
+set +e
+for i in {1..60}; do
+  echo "[x] Attempt ${i} to fetch job status"
+  status=$(
+    curl -s --request GET \
+      --url https://circleci.com/api/v2/project/gh//GaloyMoney/galoy-mobile/job/$job_number \
+      | jq -r '.status'
+  )
+  echo "status:$status";
+  if [[ $status != "running" && $status != "queued" ]]; then break; fi;
+  sleep 5
+done
+set -e
+
+echo "[•] Final Status: $status"
+
+if [[ "$status" == "success" ]]
+then
+  echo "[✓] Upload succeeded!"
+  exit 0
+else
+  echo "[✗] Upload failed!"
+  exit 1
+fi
