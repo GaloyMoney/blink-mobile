c5d1c2e03f3447413aa35129f9b6efbb6bcced73
Nicolas Burtey <nb@galoy.io>
2023-03-07 09:38:33 +0000

    chore: fix eslint

--- app/components/transaction-item/transaction-item.tsx
+++ app/components/transaction-item/transaction-item.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Text, View, } from "react-native"
+import { Text, View } from "react-native"
 import EStyleSheet from "react-native-extended-stylesheet"
 import Icon from "react-native-vector-icons/Ionicons"
 
