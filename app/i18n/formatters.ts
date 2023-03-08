1b20a5ea7e25122681f48652b8fc0dc947f76857
nicolasburtey <nicolasburtey@users.noreply.github.com>
2023-03-08 11:27:25 +0000

    feat: adding Afrikans translation (#1406)
    * feat: adding Afrikans translation
    
    * Update README.md
    
    ---------
    
    Co-authored-by: Nicolas Burtey <nb@galoy.io>

--- app/i18n/formatters.ts
+++ app/i18n/formatters.ts
@@ -2,8 +2,7 @@ import type { FormattersInitializer } from "typesafe-i18n"
 import type { Locales, Formatters } from "./i18n-types"
 
 export const initFormatters: FormattersInitializer<Locales, Formatters> = (
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  locale: Locales,
+  _locale: Locales,
 ) => {
   const formatters: Formatters = {
     sats: (value: unknown): unknown => {
