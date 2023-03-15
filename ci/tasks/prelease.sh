c057b0707e9ac8f99e1ae0538e5ca4e4944265c6
Sandipan Dey <hey@sandipan.dev>
2023-03-15 14:06:22 +0530

    chore: don't cut prerelease if there is no diff

--- ci/tasks/prelease.sh
+++ ci/tasks/prelease.sh
@@ -26,6 +26,11 @@ echo "-------------------------------"
 cat artifacts/gh-release-notes.md
 echo "-------------------------------"
 
+if [[ $(cat artifacts/gh-release-notes.md | wc -l) == 0 ]]; then
+  echo "Nothing to Release"
+  exit 0
+fi
+
 # ------------ BUMP VERSION ------------
 
 echo -n "Prev Version: "
