bafcc2401259c5a0367f9cb57cb75e506a5625b7
Sandipan Dey <hey@sandipan.dev>
2023-03-16 20:28:08 +0530

    fix: use that version sync concept

--- ci/tasks/bump-testflight-to-beta-pr.sh
+++ ci/tasks/bump-testflight-to-beta-pr.sh
@@ -4,14 +4,14 @@ set -eu
 
 . pipeline-tasks/ci/tasks/helpers.sh
 
-if [[ $(cat ./built-prod-apk/version) != $(cat ./built-prod-ipa/version) ]]; then
+if [[ $(version_part_from_build_url_apk $(cat ./built-prod-apk/url)) != $(version_part_from_build_url_ipa $(cat ./built-prod-ipa/url)) ]]; then
   echo "Version mismatch, one of the upload tasks must be running!"
   exit 1
 fi
 
 pushd deployments
 
-cat ../built-prod-apk/version > beta-version
+cat ../version_part_from_build_url_ipa $(cat ../built-prod-ipa/url) > beta-version
 
 OLD_BETA_VERSION=$(cat ../beta-version/version)
 NEW_BETA_VERSION=$(cat beta-version)
