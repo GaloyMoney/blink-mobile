6056896b20eaf8c6ad29919f71840b157618e91c
Ayush Jha <97244608+Ayush0054@users.noreply.github.com>
2023-03-22 16:16:27 +0530

    deleted support.ts

--- app/config/support.ts
+++
@@ -1,6 +0,0 @@
-export const WHATSAPP_CONTACT_NUMBER = "+50369835117"
-export const GALOY_PAY_DOMAIN = "https://ln.bitcoinbeach.com/"
-export const BLOCKCHAIN_EXPLORER_URL = "https://mempool.space/tx/"
-export const CONTACT_EMAIL_ADDRESS = "support@bbw.sv"
-export const GITHUB_ISSUE_TICKET =
-  "https://github.com/GaloyMoney/galoy-mobile/issues/new?assignees=&labels=&template=bug_report.yml&title=%5BBug%5D%3A+"
