51f62bd2829ce88908a4be90916ddf1ad2eb9859
Ayush Jha <97244608+Ayush0054@users.noreply.github.com>
2023-03-22 16:34:03 +0530

    Update appinfo.ts

--- app/config/appinfo.ts
+++ app/config/appinfo.ts
@@ -1,5 +1,7 @@
 export const WHATSAPP_CONTACT_NUMBER = "+50369835117"
 export const CONTACT_EMAIL_ADDRESS = "support@bbw.sv"
+export const GITHUB_ISSUE_TICKET =
+  "https://github.com/GaloyMoney/galoy-mobile/issues/new?assignees=&labels=&template=bug_report.yml&title=%5BBug%5D%3A+"  
 export const APP_STORE_LINK =
   "https://apps.apple.com/app/bitcoin-beach-wallet/id1531383905"
 export const PLAY_STORE_LINK =
@@ -10,7 +12,6 @@ export const PREFIX_LINKING = [
   "https://pay.bbw.sv",
   "bitcoinbeach://",
 ]
-export const GITHUB_ISSUE_TICKET =
-  "https://github.com/GaloyMoney/galoy-mobile/issues/new?assignees=&labels=&template=bug_report.yml&title=%5BBug%5D%3A+"  
+
 // FIXME this should come from globals.lightningAddressDomainAliases
 export const LNURL_DOMAINS = ["ln.bitcoinbeach.com", "pay.bbw.sv"]
