93840e49d7d85520fb9464267ea30a31216b6f67
Nicolas Burtey <nb@galoy.io>
2023-03-24 13:48:19 +0100

    chore: make storybook working again

--- app/components/large-button/large-button.stories.tsx
+++ app/components/large-button/large-button.stories.tsx
@@ -4,23 +4,27 @@ import { Story, StoryScreen, UseCase } from "../../../.storybook/views"
 import { LargeButton } from "./large-button"
 import { IconTransaction } from "../icon-transactions"
 import { WalletCurrency } from "@app/graphql/generated"
+import { createCache } from "../../graphql/cache"
+import { MockedProvider } from "@apollo/client/testing"
 
 storiesOf("Large Button", module)
   .addDecorator((fn) => <StoryScreen>{fn()}</StoryScreen>)
   .add("Style Presets", () => (
-    <Story>
-      <UseCase text="Dollar" usage="The primary.">
-        <LargeButton
-          icon={
-            <IconTransaction
-              isReceive={false}
-              walletCurrency={WalletCurrency.BTC}
-              pending={false}
-              onChain={false}
-            />
-          }
-          title="Open cash account"
-        />
-      </UseCase>
-    </Story>
+    <MockedProvider mocks={[]} cache={createCache()}>
+      <Story>
+        <UseCase text="Dollar" usage="The primary.">
+          <LargeButton
+            icon={
+              <IconTransaction
+                isReceive={false}
+                walletCurrency={WalletCurrency.BTC}
+                pending={false}
+                onChain={false}
+              />
+            }
+            title="Open cash account"
+          />
+        </UseCase>
+      </Story>
+    </MockedProvider>
   ))
