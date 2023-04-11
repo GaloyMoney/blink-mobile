6716e3a2ac36af6925e8d41c9444ddf3c3e8154e
Nicolas Burtey <nb@galoy.io>
2023-04-11 12:07:06 +0100

    chore: update blink name in the app

--- app/config/appinfo.ts
+++ app/config/appinfo.ts
@@ -1,7 +1,7 @@
 import { AndroidMarket } from "react-native-rate"
 
 export const WHATSAPP_CONTACT_NUMBER = "+50369835117"
-export const CONTACT_EMAIL_ADDRESS = "support@bbw.sv"
+export const CONTACT_EMAIL_ADDRESS = "support@blink.sv"
 export const APP_STORE_LINK =
   "https://apps.apple.com/app/bitcoin-beach-wallet/id1531383905"
 export const PLAY_STORE_LINK =
@@ -10,11 +10,12 @@ export const PREFIX_LINKING = [
   "https://ln.bitcoinbeach.com",
   "https://pay.mainnet.galoy.io",
   "https://pay.bbw.sv",
+  "https://pay.blink.sv",
   "bitcoinbeach://",
 ]
 
 // FIXME this should come from globals.lightningAddressDomainAliases
-export const LNURL_DOMAINS = ["ln.bitcoinbeach.com", "pay.bbw.sv"]
+export const LNURL_DOMAINS = ["ln.bitcoinbeach.com", "pay.bbw.sv", "blink.sv"]
 
 export const ratingOptions = {
   AppleAppID: "1531383905",
