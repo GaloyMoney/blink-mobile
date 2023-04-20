5b713bec76eb284adb0799eb7f9e4623d97b794f
Sandipan Dey <hey@sandipan.dev>
2023-04-20 22:53:36 +0530

    chore: use csf3 syntax for currency bubble stories

--- app/components/atomic/galoy-currency-bubble/galoy-currency-bubble.stories.tsx
+++ app/components/atomic/galoy-currency-bubble/galoy-currency-bubble.stories.tsx
@@ -1,23 +1,32 @@
 /* eslint-disable */
 
-
 import React from "react"
-import { storiesOf } from "@storybook/react-native"
-import { Story, StoryScreen, UseCase } from "../../../../.storybook/views"
+import { Story, UseCase } from "../../../../.storybook/views"
 import { GaloyCurrencyBubble } from "."
 import { WalletCurrency } from "@app/graphql/generated"
 
-storiesOf("Galoy Currency Bubble", module)
-  .addDecorator((fn) => <StoryScreen>{fn()}</StoryScreen>)
-  .add("Style Presets", () => (
-    <Story>
-      <UseCase style={{ flexDirection: "row" }} text="Medium">
-        <GaloyCurrencyBubble size="medium" currency={WalletCurrency.Btc} />
-        <GaloyCurrencyBubble size="medium" currency={WalletCurrency.Usd} />
-      </UseCase>
-      <UseCase text="Large" style={{ flexDirection: "row" }}>
-        <GaloyCurrencyBubble size="large" currency={WalletCurrency.Btc} />
-        <GaloyCurrencyBubble size="large" currency={WalletCurrency.Usd} />
-      </UseCase>
-    </Story>
-  ))
+const UseCaseWrapper = ({ children, text, style }) => (
+  <UseCase style={style} text={text}>
+    {children}
+  </UseCase>
+)
+
+export default {
+  title: "Galoy Currency Bubble",
+  component: GaloyCurrencyBubble,
+}
+
+export const StylePresets = () => (
+  <Story>
+    <UseCaseWrapper style={{ flexDirection: "row" }} text="Medium">
+      <GaloyCurrencyBubble size="medium" currency={WalletCurrency.Btc} />
+      <GaloyCurrencyBubble size="medium" currency={WalletCurrency.Usd} />
+    </UseCaseWrapper>
+    <UseCaseWrapper style={{ flexDirection: "row" }} text="Large">
+      <GaloyCurrencyBubble size="large" currency={WalletCurrency.Btc} />
+      <GaloyCurrencyBubble size="large" currency={WalletCurrency.Usd} />
+    </UseCaseWrapper>
+  </Story>
+)
+
+StylePresets.storyName = "Style Presets"
