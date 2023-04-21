a4e9c25a2f1ca32ea1b00df5e23b0de31a69c84d
Sandipan Dey <hey@sandipan.dev>
2023-04-21 08:36:03 +0530

    chore: use csf3 syntax for icon stories

--- app/components/atomic/galoy-icon/galoy-icon.stories.tsx
+++ app/components/atomic/galoy-icon/galoy-icon.stories.tsx
@@ -1,8 +1,7 @@
 /* eslint-disable */
 
 import React from "react"
-import { storiesOf } from "@storybook/react-native"
-import { Story, StoryScreen, UseCase } from "../../../../.storybook/views"
+import { Story, UseCase } from "../../../../.storybook/views"
 import { GaloyIcon, IconNames, IconNamesType } from "."
 import { StyleSheet, Text, View } from "react-native"
 import { light } from "../../../rne-theme/colors"
@@ -39,55 +38,60 @@ const IconDisplay = ({
 
 const iconNamesSlice = IconNames.slice(0, 6)
 
-storiesOf("Galoy Icon", module)
-  .addDecorator((fn) => <StoryScreen>{fn()}</StoryScreen>)
-  .add("Default", () => (
-    <Story>
-      <UseCase text="Size 16">
-        <View style={styles.iconContainer}>
-          {IconNames.map((name) => (
-            <IconDisplay name={name as IconNamesType} size={16} />
-          ))}
-        </View>
-      </UseCase>
-      <UseCase text="Size 20">
-        <View style={styles.iconContainer}>
-          {iconNamesSlice.map((name) => (
-            <IconDisplay name={name as IconNamesType} size={20} />
-          ))}
-        </View>
-      </UseCase>
-      <UseCase text="Size 24">
-        <View style={styles.iconContainer}>
-          {iconNamesSlice.map((name) => (
-            <IconDisplay name={name as IconNamesType} size={24} />
-          ))}
-        </View>
-      </UseCase>
-      <UseCase text="Size 32">
-        <View style={styles.iconContainer}>
-          {iconNamesSlice.map((name) => (
-            <IconDisplay name={name as IconNamesType} size={32} />
-          ))}
-        </View>
-      </UseCase>
-      <UseCase text="Different Color">
-        <View style={styles.iconContainer}>
-          {iconNamesSlice.map((name) => (
-            <IconDisplay name={name as IconNamesType} size={32} color={light.primary} />
-          ))}
-        </View>
-      </UseCase>
-      <UseCase text="Fill">
-        <View style={styles.iconContainer}>
-          {iconNamesSlice.map((name) => (
-            <IconDisplay
-              name={name as IconNamesType}
-              size={32}
-              backgroundColor={light.primary9}
-            />
-          ))}
-        </View>
-      </UseCase>
-    </Story>
-  ))
+export default {
+  title: "Galoy Icon",
+  component: GaloyIcon,
+}
+
+export const Default = () => (
+  <Story>
+    <UseCase text="Size 16">
+      <View style={styles.iconContainer}>
+        {IconNames.map((name) => (
+          <IconDisplay name={name as IconNamesType} size={16} />
+        ))}
+      </View>
+    </UseCase>
+    <UseCase text="Size 20">
+      <View style={styles.iconContainer}>
+        {iconNamesSlice.map((name) => (
+          <IconDisplay name={name as IconNamesType} size={20} />
+        ))}
+      </View>
+    </UseCase>
+    <UseCase text="Size 24">
+      <View style={styles.iconContainer}>
+        {iconNamesSlice.map((name) => (
+          <IconDisplay name={name as IconNamesType} size={24} />
+        ))}
+      </View>
+    </UseCase>
+    <UseCase text="Size 32">
+      <View style={styles.iconContainer}>
+        {iconNamesSlice.map((name) => (
+          <IconDisplay name={name as IconNamesType} size={32} />
+        ))}
+      </View>
+    </UseCase>
+    <UseCase text="Different Color">
+      <View style={styles.iconContainer}>
+        {iconNamesSlice.map((name) => (
+          <IconDisplay name={name as IconNamesType} size={32} color={light.primary} />
+        ))}
+      </View>
+    </UseCase>
+    <UseCase text="Fill">
+      <View style={styles.iconContainer}>
+        {iconNamesSlice.map((name) => (
+          <IconDisplay
+            name={name as IconNamesType}
+            size={32}
+            backgroundColor={light.primary9}
+          />
+        ))}
+      </View>
+    </UseCase>
+  </Story>
+)
+
+Default.storyName = "Default"
