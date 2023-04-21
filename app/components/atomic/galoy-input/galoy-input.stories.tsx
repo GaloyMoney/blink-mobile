22bbcbf104f1358c54b75436572068bfdf07d177
Sandipan Dey <hey@sandipan.dev>
2023-04-21 08:33:53 +0530

    chore: use csf3 syntax for input redesigned stories

--- app/components/atomic/galoy-input/galoy-input.stories.tsx
+++ app/components/atomic/galoy-input/galoy-input.stories.tsx
@@ -1,9 +1,7 @@
 import React from "react"
 import { StyleSheet, Text } from "react-native"
 
-import { storiesOf } from "@storybook/react-native"
-
-import { Story, StoryScreen, UseCase } from "../../../../.storybook/views"
+import { Story, UseCase } from "../../../../.storybook/views"
 import { GaloyIcon } from "../galoy-icon"
 import { GaloyInputRedesigned } from "./galoy-redesigned-input"
 
@@ -17,47 +15,52 @@ const styles = StyleSheet.create({
   },
 })
 
-storiesOf("Galoy Input Redsigned", module)
-  .addDecorator((fn) => <StoryScreen>{fn()}</StoryScreen>)
-  .add("Style Presets", () => (
-    <Story>
-      <UseCase text="Default">
-        <GaloyInputRedesigned placeholder={"placeholder"} label={"Form Label"} />
-      </UseCase>
-      <UseCase text="Focused input">
-        <GaloyInputRedesigned
-          label={"Form Label"}
-          placeholder={"placeholder"}
-          initIsFocused={true}
-        />
-      </UseCase>
-      <UseCase text="Input with CustomIcon">
-        <GaloyInputRedesigned
-          label={"Form Label"}
-          placeholder={"placeholder"}
-          leftIcon={<GaloyIcon name="bitcoin" size={30} color="blue" />}
-        />
-      </UseCase>
-      <UseCase text="Input with TextIcon and No Label">
-        <GaloyInputRedesigned
-          placeholder={"placeholder"}
-          leftIcon={<Text style={styles.textStyleIcon}>usd</Text>}
-        />
-      </UseCase>
-      <UseCase text="Input with padding">
-        <GaloyInputRedesigned
-          label={"Form Label"}
-          inputContainerStyle={styles.paddedInput}
-          caption={"caption"}
-          placeholder={"placeholder"}
-        />
-      </UseCase>
-      <UseCase text="Error">
-        <GaloyInputRedesigned
-          label={"Input Label"}
-          placeholder={"placeholder"}
-          errorMessage={"Ooops! something went wrong!"}
-        />
-      </UseCase>
-    </Story>
-  ))
+export default {
+  title: "Galoy Input Redesigned",
+  component: GaloyInputRedesigned,
+}
+
+export const StylePresets = () => (
+  <Story>
+    <UseCase text="Default">
+      <GaloyInputRedesigned placeholder={"placeholder"} label={"Form Label"} />
+    </UseCase>
+    <UseCase text="Focused input">
+      <GaloyInputRedesigned
+        label={"Form Label"}
+        placeholder={"placeholder"}
+        initIsFocused={true}
+      />
+    </UseCase>
+    <UseCase text="Input with CustomIcon">
+      <GaloyInputRedesigned
+        label={"Form Label"}
+        placeholder={"placeholder"}
+        leftIcon={<GaloyIcon name="bitcoin" size={30} color="blue" />}
+      />
+    </UseCase>
+    <UseCase text="Input with TextIcon and No Label">
+      <GaloyInputRedesigned
+        placeholder={"placeholder"}
+        leftIcon={<Text style={styles.textStyleIcon}>usd</Text>}
+      />
+    </UseCase>
+    <UseCase text="Input with padding">
+      <GaloyInputRedesigned
+        label={"Form Label"}
+        inputContainerStyle={styles.paddedInput}
+        caption={"caption"}
+        placeholder={"placeholder"}
+      />
+    </UseCase>
+    <UseCase text="Error">
+      <GaloyInputRedesigned
+        label={"Input Label"}
+        placeholder={"placeholder"}
+        errorMessage={"Ooops! something went wrong!"}
+      />
+    </UseCase>
+  </Story>
+)
+
+StylePresets.storyName = "Style Presets"
