262dce43225d012fe9ede5bdcee2800f7b0d2800
aditiansingh <aditiansingh@gmail.com>
2023-05-24 16:33:16 +0530

    spelling correction

--- app/components/payment-destination-display/payment-destination-display.tsx
+++ app/components/payment-destination-display/payment-destination-display.tsx
@@ -43,13 +43,13 @@ export const PaymentDestinationDisplay = ({
 
   // we assume this is a bitcoin address or lightning invoice
   // not a username
-  const firtSix = destination.slice(0, 6)
+  const firstSix = destination.slice(0, 6)
   const middle = destination.slice(6, -6)
   const lastSix = destination.slice(-6)
 
   return (
     <Text style={styles.primaryTextStyle} numberOfLines={1} ellipsizeMode={"middle"}>
-      <Text style={styles.highlight}>{firtSix}</Text>
+      <Text style={styles.highlight}>{firstSix}</Text>
       {middle}
       <Text style={styles.highlight}>{lastSix}</Text>
     </Text>
