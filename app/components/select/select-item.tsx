271ce96ceaf7b9afc5d6b2b6fe44dc4656f729bb
Sandipan Dey <hey@sandipan.dev>
2023-06-01 15:08:15 +0530

    fix: test

--- app/components/select/select-item.tsx
+++ app/components/select/select-item.tsx
@@ -1,6 +1,8 @@
 // This is a dummy item for better DX while writing the select component
 // See ./item.ts for the correct item
 
+import React from "react"
+
 import { ListItemProps } from "@rneui/base"
 
 export type SelectItemProps = {
