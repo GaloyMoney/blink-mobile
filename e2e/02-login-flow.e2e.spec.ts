88eec0b96154e17bff0d1f3b328d514857c05ff8
aditiansingh <aditiansingh@gmail.com>
2023-06-04 22:59:56 +0530

    fix:fixed login-flow for e2e tests

--- e2e/02-login-flow.e2e.spec.ts
+++ e2e/02-login-flow.e2e.spec.ts
@@ -107,6 +107,7 @@ describe("Login Flow", () => {
   })
 
   it("navigates back to move home screen", async () => {
+    await scrollDown()
     await scrollUp()
     const phoneSetting = await $(selector(LL.common.phoneNumber(), "StaticText"))
     await phoneSetting.waitForDisplayed({ timeout })
