5e09af52a6527f1c58d56421d4d67e3abaa0480a
nicolasburtey <nicolasburtey@users.noreply.github.com>
2023-06-15 09:24:52 +0100

    fix: translate dates (#2320)
    * feat: translate dates
    
    also remove dependency on moment()
    
    * chore: add 0 for less than 10 seconds to get double digits
    
    ---------
    
    Co-authored-by: Nicolas Burtey <nb@galoy.io>

--- app/utils/polyfill.ts
+++
@@ -1,9 +0,0 @@
-import { Platform } from "react-native"
-
-// Polyfill for Intl.NumberFormat.
-// See: https://stackoverflow.com/a/55861894
-if (Platform.OS === "android") {
-  // only android needs polyfill
-  require("intl") // import intl object
-  require("intl/locale-data/jsonp/en-US") // load the required locale details
-}
