c6fee7422f552b358f4f4dae1d3922999e1e2cae
Sandipan Dey <hey@sandipan.dev>
2023-08-16 13:07:09 +0530

    chore: introduce circles modal

---
+++ app/components/introducing-circles-modal/introducing-circles-modal.tsx
@@ -0,0 +1,111 @@
+import * as React from "react"
+import { Pressable, View } from "react-native"
+import Modal from "react-native-modal"
+
+import { useI18nContext } from "@app/i18n/i18n-react"
+import { makeStyles, useTheme, Text } from "@rneui/themed"
+
+import { GaloyPrimaryButton } from "../atomic/galoy-primary-button"
+import { GaloyIcon } from "../atomic/galoy-icon"
+import { useNavigation } from "@react-navigation/native"
+import { StackNavigationProp } from "@react-navigation/stack"
+import {
+  PrimaryStackParamList,
+  RootStackParamList,
+} from "@app/navigation/stack-param-lists"
+import { BottomTabNavigationProp } from "@react-navigation/bottom-tabs"
+
+const useStyles = makeStyles(({ colors }) => ({
+  scrollViewStyle: {
+    padding: 20,
+    paddingTop: 40,
+    display: "flex",
+    flexDirection: "column",
+    rowGap: 30,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  peopleIcon: {
+    color: colors.primary,
+  },
+  cross: {
+    position: "absolute",
+    top: 0,
+    right: 30,
+    padding: 10,
+    marginTop: -10,
+  },
+  modalCard: {
+    backgroundColor: colors.grey5,
+    borderRadius: 16,
+    paddingVertical: 10,
+  },
+  cardTitleContainer: {
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    rowGap: 10,
+    paddingHorizontal: 10,
+  },
+  cardActionsContainer: {
+    flexDirection: "column",
+  },
+}))
+
+type Props = {
+  isVisible: boolean
+  setIsVisible: (isVisible: boolean) => void
+}
+
+export const IntroducingCirclesModal: React.FC<Props> = ({ isVisible, setIsVisible }) => {
+  const { LL } = useI18nContext()
+  const navigation = useNavigation<StackNavigationProp<RootStackParamList>>()
+  const primaryNavigator =
+    navigation.getParent<BottomTabNavigationProp<PrimaryStackParamList>>()
+
+  const {
+    theme: { colors },
+  } = useTheme()
+  const styles = useStyles()
+
+  const acknowledgeModal = () => {
+    setIsVisible(false)
+  }
+
+  const navigateToCircles = () => {
+    setIsVisible(false)
+    primaryNavigator.navigate("People")
+  }
+
+  return (
+    <Modal
+      isVisible={isVisible}
+      backdropOpacity={0.8}
+      backdropColor={colors.white}
+      onBackdropPress={acknowledgeModal}
+    >
+      <View style={styles.modalCard}>
+        <View style={styles.scrollViewStyle}>
+          <Pressable style={styles.cross} onPress={acknowledgeModal}>
+            <GaloyIcon name="close" size={22} />
+          </Pressable>
+          <GaloyIcon name="people" color={colors.primary} size={50} />
+          <View style={styles.cardTitleContainer}>
+            <Text type="h1" bold>
+              Introducing Circles
+            </Text>
+            <Text type="p1">
+              Your circles grow as you welcome others to Blink – by sending somebody their
+              first sats.
+            </Text>
+          </View>
+          <View style={styles.cardActionsContainer}>
+            <View>
+              <GaloyPrimaryButton title="View My Circles" onPress={navigateToCircles} />
+            </View>
+          </View>
+        </View>
+      </View>
+    </Modal>
+  )
+}
---
+++ app/components/invite-modal/invite-modal.tsx
@@ -0,0 +1,113 @@
+import * as React from "react"
+import { Image, Linking, ScrollView, View } from "react-native"
+import Modal from "react-native-modal"
+
+import { useI18nContext } from "@app/i18n/i18n-react"
+import { makeStyles, useTheme, Text } from "@rneui/themed"
+
+import StablesatsImage from "../../assets/images/stable-sats.png"
+import { GaloyPrimaryButton } from "../atomic/galoy-primary-button"
+import { GaloySecondaryButton } from "../atomic/galoy-secondary-button"
+
+const useStyles = makeStyles(({ colors }) => ({
+  imageContainer: {
+    height: 150,
+    marginBottom: 16,
+  },
+  stableSatsImage: {
+    flex: 1,
+  },
+  scrollViewStyle: {
+    paddingHorizontal: 12,
+  },
+  modalCard: {
+    backgroundColor: colors.white,
+    borderRadius: 16,
+    paddingVertical: 18,
+  },
+  cardTitleContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginBottom: 16,
+  },
+  cardBodyContainer: {
+    marginBottom: 16,
+  },
+  termsAndConditionsText: {
+    textDecorationLine: "underline",
+  },
+  cardActionsContainer: {
+    flexDirection: "column",
+  },
+  marginBottom: {
+    marginBottom: 10,
+  },
+}))
+
+const STABLESATS_LINK = "https://www.stablesats.com"
+const STABLESATS_TERMS_LINK = "https://www.bbw.sv/terms"
+
+type Props = {
+  isVisible: boolean
+  setIsVisible: (isVisible: boolean) => void
+}
+
+export const InviteModal: React.FC<Props> = ({ isVisible, setIsVisible }) => {
+  const { LL } = useI18nContext()
+  const {
+    theme: { colors },
+  } = useTheme()
+  const styles = useStyles()
+
+  const acknowledgeModal = () => {
+    setIsVisible(false)
+  }
+
+  return (
+    <Modal
+      isVisible={isVisible}
+      backdropOpacity={0.3}
+      backdropColor={colors.grey3}
+      onBackdropPress={acknowledgeModal}
+    >
+      <View style={styles.modalCard}>
+        <ScrollView style={styles.scrollViewStyle}>
+          <View style={styles.imageContainer}>
+            <Image
+              source={StablesatsImage}
+              style={styles.stableSatsImage}
+              resizeMode="contain"
+            />
+          </View>
+          <View style={styles.cardTitleContainer}>
+            <Text type={"h2"}>{LL.StablesatsModal.header()}</Text>
+          </View>
+          <View style={styles.cardBodyContainer}>
+            <Text type="p2">
+              {LL.StablesatsModal.body()}{" "}
+              <Text
+                style={styles.termsAndConditionsText}
+                onPress={() => Linking.openURL(STABLESATS_TERMS_LINK)}
+              >
+                {LL.StablesatsModal.termsAndConditions()}
+              </Text>
+            </Text>
+          </View>
+          <View style={styles.cardActionsContainer}>
+            <View style={styles.marginBottom}>
+              <GaloyPrimaryButton
+                title={LL.common.backHome()}
+                onPress={acknowledgeModal}
+              />
+            </View>
+
+            <GaloySecondaryButton
+              title={LL.StablesatsModal.learnMore()}
+              onPress={() => Linking.openURL(STABLESATS_LINK)}
+            />
+          </View>
+        </ScrollView>
+      </View>
+    </Modal>
+  )
+}
