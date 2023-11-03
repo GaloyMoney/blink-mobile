0f25aa79dd78008318d0f5ed709d121df07ad255
Sandipan Dey <hey@sandipan.dev>
2023-11-03 14:24:08 +0530

    chore: redundant log

--- app/screens/send-bitcoin-screen/scanning-qrcode-screen.tsx
+++ app/screens/send-bitcoin-screen/scanning-qrcode-screen.tsx
@@ -119,7 +119,6 @@ export const ScanningQRCodeScreen: React.FC = () => {
 
         if (destination.valid) {
           if (destination.destinationDirection === DestinationDirection.Send) {
-            console.log(JSON.stringify({ destination }, null, 2))
             navigation.replace("sendBitcoinDetails", {
               paymentDestination: destination,
             })
