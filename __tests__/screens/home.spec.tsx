2b40d6f96ce925f538938aba216925e95e319250
Nicolas Burtey <nb@galoy.io>
2023-11-21 15:04:00 -0600

    fix: tests

--- __tests__/screens/home.spec.tsx
+++ __tests__/screens/home.spec.tsx
@@ -4,6 +4,23 @@ import { HomeScreen } from "../../app/screens/home-screen"
 import { act, render } from "@testing-library/react-native"
 import { ContextForScreen } from "./helper"
 
+jest.mock("@react-native-firebase/app-check", () => {
+  return () => ({
+    initializeAppCheck: jest.fn(),
+    getToken: jest.fn(),
+    newReactNativeFirebaseAppCheckProvider: () => ({
+      configure: jest.fn(),
+    }),
+  })
+})
+
+jest.mock("react-native-config", () => {
+  return {
+    APP_CHECK_ANDROID_DEBUG_TOKEN: "token",
+    APP_CHECK_IOS_DEBUG_TOKEN: "token",
+  }
+})
+
 it("HomeAuthed", async () => {
   render(
     <ContextForScreen>
--- __tests__/screens/send-details.spec.tsx
+++ __tests__/screens/send-details.spec.tsx
@@ -5,6 +5,23 @@ import { ContextForScreen } from "./helper"
 
 import { Intraledger } from "../../app/screens/send-bitcoin-screen/send-bitcoin-details-screen.stories"
 
+jest.mock("@react-native-firebase/app-check", () => {
+  return () => ({
+    initializeAppCheck: jest.fn(),
+    getToken: jest.fn(),
+    newReactNativeFirebaseAppCheckProvider: () => ({
+      configure: jest.fn(),
+    }),
+  })
+})
+
+jest.mock("react-native-config", () => {
+  return {
+    APP_CHECK_ANDROID_DEBUG_TOKEN: "token",
+    APP_CHECK_IOS_DEBUG_TOKEN: "token",
+  }
+})
+
 it("SendScreen Details", async () => {
   render(
     <ContextForScreen>
