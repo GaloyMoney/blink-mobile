17103ed1ea752b78af04b688c183f0364936e334
Sandipan Dey <hey@sandipan.dev>
2023-12-04 15:14:10 +0530

    feat: test animation

--- app/screens/developer-screen/lottie-animation-screen.tsx
+++ app/screens/developer-screen/lottie-animation-screen.tsx
@@ -1,5 +1,7 @@
-import { Text } from "@rneui/themed"
+import LottieView from "lottie-react-native"
+
+import TestAnimation from "@app/assets/animations/test.json"
 
 export const LottieAnimationScreen: React.FC = () => {
-  return <Text>Animation</Text>
+  return <LottieView style={{ height: "100%" }} source={TestAnimation} autoPlay loop />
 }
