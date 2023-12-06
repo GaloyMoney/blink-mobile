c011e5a316c404d32218cefae6cb569fc0873027
Sandipan Dey <hey@sandipan.dev>
2023-12-07 00:04:08 +0530

    Update app/i18n/en/index.ts
    Co-authored-by: nicolasburtey <nicolasburtey@users.noreply.github.com>

--- app/i18n/en/index.ts
+++ app/i18n/en/index.ts
@@ -2253,7 +2253,7 @@ const en: BaseTranslation = {
     clipboardError: "Error getting value from clipboard",
   },
   SendBitcoinScreen: {
-    willBeSentToMempoolBy: "Transaction will be submitted to mempool by",
+    willBeSentToMempoolBy: "Transaction should be submitted to mempool by",
     amount: "Amount",
     amountExceed: "Amount exceeds your balance of {balance: string}",
     amountExceedsLimit: "Amount exceeds your remaining daily limit of {limit: string}",
