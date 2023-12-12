0efc2498c2f30586293352dcc2c0f14ad17a1bb6
Nicolas Burtey <nb@galoy.io>
2023-12-12 16:07:21 -0600

    chore: improve message on succesful 2fa activation

--- app/i18n/i18n-types.ts
+++ app/i18n/i18n-types.ts
@@ -7410,7 +7410,9 @@ type RootTranslation = {
 		/**
 		 * T​w​o​-​f​a​c​t​o​r​ ​a​u​t​h​e​n​t​i​c​a​t​i​o​n​ ​h​a​s​ ​b​e​e​n​ ​e​n​a​b​l​e​d​.​ ​Y​o​u​ ​w​i​l​l​ ​n​o​w​ ​o​n​l​y​ ​b​e​ ​a​b​l​e​ ​t​o​ ​l​o​g​ ​b​a​c​k​ ​i​n​ ​w​i​t​h​ ​y​o​u​r​ ​p​h​o​n​e​ ​o​r​ ​e​m​a​i​l​ ​A​N​D​ ​y​o​u​r​ ​t​w​o​ ​f​a​c​t​o​r​ ​a​u​t​h​e​n​t​i​c​a​t​i​o​n​.​
 	​
-	​O​n​l​y​ ​l​e​v​e​l​ ​2​ ​a​c​c​o​u​n​t​s​ ​m​a​y​ ​b​e​ ​r​e​c​o​v​e​r​e​d​ ​i​n​ ​t​h​e​ ​c​a​s​e​ ​a​ ​u​s​e​r​ ​h​a​s​ ​l​o​s​t​ ​a​c​c​e​s​s​ ​t​o​ ​t​h​e​i​r​ ​t​w​o​-​f​a​c​t​o​r​ ​a​u​t​h​e​n​t​i​c​a​t​i​o​n​.
+	​D​e​v​i​c​e​s​ ​o​n​ ​t​h​i​s​ ​a​c​c​o​u​n​t​ ​w​h​i​c​h​ ​a​r​e​ ​n​o​t​ ​l​o​g​g​e​d​ ​i​n​ ​w​i​t​h​ ​2​F​A​ ​w​i​l​l​ ​n​e​e​d​ ​t​o​ ​l​o​g​ ​b​a​c​k​ ​i​n​.​
+	​
+	​O​n​l​y​ ​l​e​v​e​l​ ​2​ ​a​c​c​o​u​n​t​s​ ​c​a​n​ ​b​e​ ​r​e​c​o​v​e​r​e​d​ ​b​y​ ​c​u​s​t​o​m​e​r​ ​s​u​p​p​o​r​t​ ​i​n​ ​t​h​e​ ​e​v​e​n​t​ ​o​f​ ​l​o​s​t​ ​2​F​A​ ​a​c​c​e​s​s​.
 		 */
 		success: string
 	}
@@ -16175,7 +16177,9 @@ export type TranslationFunctions = {
 		/**
 		 * Two-factor authentication has been enabled. You will now only be able to log back in with your phone or email AND your two factor authentication.
 
-	Only level 2 accounts may be recovered in the case a user has lost access to their two-factor authentication.
+	Devices on this account which are not logged in with 2FA will need to log back in.
+
+	Only level 2 accounts can be recovered by customer support in the event of lost 2FA access.
 		 */
 		success: () => LocalizedString
 	}
