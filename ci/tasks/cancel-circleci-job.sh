cd976b5daf2bcd1360f5b740cae7ce5a32bfd196
Sandipan Dey <hey@sandipan.dev>
2024-02-28 00:40:08 +0530

    chore: deprecate circleci

--- ci/tasks/cancel-circleci-job.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-job_number=$(cat job-number/number)
-
-curl --request POST \
-  --url https://circleci.com/api/v2/project/gh//GaloyMoney/galoy-mobile/job/$job_number/cancel \
-  --header "Circle-Token: $CIRCLECI_TOKEN"
