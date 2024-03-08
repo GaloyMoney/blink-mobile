af2b4c121ffb19c52bcadcbc365790c2ca4fede6
sandipndev <hey@sandipan.dev>
2024-03-09 03:05:46 +0530

    ci: pipeline updates

--- ci/tasks/build-ios.sh
+++ ci/tasks/build-ios.sh
@@ -17,8 +17,8 @@ git checkout $GIT_REF
 yarn global add node-gyp
 yarn install
 
-tmpfile=$(mktemp /tmp/wwdr-cert.cer)
-curl -f -o $tmpfile https://www.apple.com/certificateauthority/AppleWWDRCAG3.cer && security import $tmpfile ~/Library/Keychains/login.keychain-db
+tmpfile=$(mktemp /tmp/wwdr-cert.cer) || true
+curl -f -o $tmpfile https://www.apple.com/certificateauthority/AppleWWDRCAG3.cer && security import $tmpfile ~/Library/Keychains/login.keychain-db || true
 
 pushd ios
 sed -i'' -e "s/MARKETING_VERSION.*/MARKETING_VERSION = $PUBLIC_VERSION;/g" GaloyApp.xcodeproj/project.pbxproj
