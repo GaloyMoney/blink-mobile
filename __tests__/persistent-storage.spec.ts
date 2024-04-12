a9952a83434f77dca1ccec5f3b27f2c7e10cbeac
2624789 <2624789@users.noreply.github.com>
2024-04-12 07:35:40 -0500

    Fixed persistent storage tests

--- __tests__/persistent-storage.spec.ts
+++ __tests__/persistent-storage.spec.ts
@@ -1,31 +1,31 @@
 import {
-  defaultPersistentState,
-  migrateAndGetPersistentState,
+  defaultLocalStorageState,
+  migrateAndGetLocalStorageState,
 } from "../app/store/persistent-state/state-migrations"
 
 it("uses default state when none is present", async () => {
-  const state = await migrateAndGetPersistentState({})
-  expect(state).toEqual(defaultPersistentState)
+  const state = await migrateAndGetLocalStorageState({})
+  expect(state).toEqual(defaultLocalStorageState)
 })
 
 it("migrates persistent state", async () => {
-  const state = await migrateAndGetPersistentState({
+  const state = await migrateAndGetLocalStorageState({
     schemaVersion: 0,
     isUsdDisabled: true,
   })
   expect(state).toEqual({
-    ...defaultPersistentState,
+    ...defaultLocalStorageState,
   })
 })
 
 it("returns default when schema is not present", async () => {
-  const state = await migrateAndGetPersistentState({
+  const state = await migrateAndGetLocalStorageState({
     schemaVersion: -2,
   })
-  expect(state).toEqual(defaultPersistentState)
+  expect(state).toEqual(defaultLocalStorageState)
 })
 
-it("migration from 5 to 6", async () => {
+it("migration from 5 to 7", async () => {
   const state5 = {
     schemaVersion: 5,
     galoyInstance: { id: "Main" },
@@ -33,12 +33,11 @@ it("migration from 5 to 6", async () => {
   }
 
   const state6 = {
-    schemaVersion: 6,
+    schemaVersion: 7,
     galoyInstance: { id: "Main" },
-    galoyAuthToken: "myToken",
   }
 
-  const res = await migrateAndGetPersistentState(state5)
+  const res = await migrateAndGetLocalStorageState(state5)
 
   expect(res).toStrictEqual(state6)
 })
