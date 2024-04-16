ee4ba9025a7f6a92bb14863189668c22497ccf4b
sandipndev <hey@sandipan.dev>
2024-04-17 00:25:19 +0530

    ok

--- ci/tasks/screen-record-simulator.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-while ! xcrun simctl list devices | grep "(Booted)"; do
-  sleep 1
-  echo "Waiting for Simulator device to come online..."
-done
-
-xcrun simctl io booted recordVideo screenRecord.mov
