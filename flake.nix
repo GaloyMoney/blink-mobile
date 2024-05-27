d8d4207bb454325e7b1cf2908a04c61377b0df30
Prakhar Agarwal <prakharagarwal3031@gmail.com>
2024-05-28 00:50:30 +0530

    chore: update to react native 0.74.1
    Signed-off-by: Prakhar Agarwal <prakharagarwal3031@gmail.com>

--- flake.nix
+++ flake.nix
@@ -90,6 +90,7 @@
                 platform-tools
                 platforms-android-34
                 ndk-25-1-8937393
+                ndk-26-1-10909125
                 cmake-3-22-1
 
                 # Some dependencies we use are on old versions
