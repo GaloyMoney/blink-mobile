ef57b60be8514a3c2dd7e2c9a331a5d7f4f36111
nodirbek75 <nodirbek7077@gmail.com>
2024-08-10 11:00:24 +0500

    upgrade version code for android and ios, point to Main instance in production

--- app/store/persistent-state/state-migrations.ts
+++ app/store/persistent-state/state-migrations.ts
@@ -244,7 +244,7 @@ export type PersistentState = PersistentState_7
 
 export const defaultPersistentState: PersistentState = {
   schemaVersion: 7,
-  galoyInstance: { id: "Staging" },
+  galoyInstance: { id: __DEV__ ? "Staging" : "Main" },
   galoyAuthToken: "",
   hasInitializedBreezSDK: false,
   introVideoCount: 0,
