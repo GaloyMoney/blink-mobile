288071e6a391e06e4c6d3d974aa0bfe498d03dc0
Juan P Lopez <dolcalmi@gmail.com>
2024-12-03 11:42:54 -0500

    fix: format issues

--- app/i18n/sk/index.ts
+++ app/i18n/sk/index.ts
@@ -1,7 +1,8 @@
-import { Translation } from "../i18n-types"
-import en from '../en'
-import rawTranslated from '../raw-i18n/translations/sk.json'
 import merge from "lodash.merge"
+
+import en from "../en"
+import { Translation } from "../i18n-types"
+import rawTranslated from "../raw-i18n/translations/sk.json"
 
 const translated: Translation = merge({}, en as Translation, rawTranslated)
 
