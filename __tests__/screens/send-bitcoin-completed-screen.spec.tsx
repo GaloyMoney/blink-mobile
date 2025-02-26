a4fe8b035a4d21dafa9fe238f501a6c5ffcc5d96
Alfonso Esau Gomez <alfonsoesaugomez@gmail.com>
2025-02-26 00:20:04 -0600

    test(send bitcoin screen): LUD-09 tests (message|url|url + message)

--- __tests__/screens/send-bitcoin-completed-screen.spec.tsx
+++ __tests__/screens/send-bitcoin-completed-screen.spec.tsx
@@ -1,13 +1,17 @@
 import React from "react"
-import { render, screen, waitFor } from "@testing-library/react-native"
+import { fireEvent, render, screen, waitFor } from "@testing-library/react-native"
 import { loadLocale } from "@app/i18n/i18n-util.sync"
-
+import { i18nObject } from "@app/i18n/i18n-util"
 import {
   Success,
   Queued,
   Pending,
+  SuccessLUD09Message,
+  SuccessLUD09URL,
+  SuccessLUD09URLWithDesc,
 } from "../../app/screens/send-bitcoin-screen/send-bitcoin-completed-screen.stories"
 import { ContextForScreen } from "./helper"
+import { Linking } from "react-native"
 
 jest.mock("react-native-in-app-review", () => ({
   isAvailable: () => true,
@@ -15,7 +19,12 @@ jest.mock("react-native-in-app-review", () => ({
 }))
 
 describe("SendBitcoinCompletedScreen", () => {
-  beforeEach(() => loadLocale("en"))
+  let LL: ReturnType<typeof i18nObject>
+
+  beforeEach(() => {
+    loadLocale("en")
+    LL = i18nObject("en")
+  })
 
   it("renders the Success state correctly", async () => {
     render(
@@ -54,5 +63,57 @@ describe("SendBitcoinCompletedScreen", () => {
     expect(pendingTextElement.props.children).toEqual(
       expect.stringContaining("The payment has been sent"),
     )
+  })
+
+  it("render successAction - LUD 09 - message", async () => {
+    const message = "Thanks for your support."
+
+    render(
+      <ContextForScreen>
+        <SuccessLUD09Message />
+      </ContextForScreen>,
+    )
+
+    expect(screen.getByText(message)).toBeTruthy()
+    expect(screen.getByText(LL.SendBitcoinScreen.note())).toBeTruthy()
+  })
+
+  it("render successAction - LUD 09 - URL", async () => {
+    const url = "https://es.blink.sv"
+
+    render(
+      <ContextForScreen>
+        <SuccessLUD09URL />
+      </ContextForScreen>,
+    )
+
+    const button = screen.getByText(LL.ScanningQRCodeScreen.openLinkTitle())
+
+    expect(button).toBeTruthy()
+
+    fireEvent.press(button)
+
+    expect(Linking.openURL).toHaveBeenCalledWith(url)
+  })
+
+  it("render successAction - LUD 09 - URL with description", async () => {
+    const url = "https://es.blink.sv"
+    const description = "Example URL + description"
+
+    render(
+      <ContextForScreen>
+        <SuccessLUD09URLWithDesc />
+      </ContextForScreen>,
+    )
+
+    expect(screen.getByText(description)).toBeTruthy()
+
+    const button = screen.getByText(LL.ScanningQRCodeScreen.openLinkTitle())
+
+    expect(button).toBeTruthy()
+
+    fireEvent.press(button)
+
+    expect(Linking.openURL).toHaveBeenCalledWith(url)
   })
 })
