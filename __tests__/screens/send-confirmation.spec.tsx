ff70e8d2f6d0295e17288a71642bcbb58581e31a
Alfonso Esau Gomez <alfonsoesaugomez@gmail.com>
2025-02-28 14:45:50 -0600

    test: send bitcoin confirmation screen test

--- __tests__/screens/send-confirmation.spec.tsx
+++ __tests__/screens/send-confirmation.spec.tsx
@@ -1,26 +1,86 @@
 import React from "react"
-
-import { act, render } from "@testing-library/react-native"
-
-import { Intraledger } from "../../app/screens/send-bitcoin-screen/send-bitcoin-confirmation-screen.stories"
+import { act, render, screen } from "@testing-library/react-native"
+import { loadLocale } from "@app/i18n/i18n-util.sync"
+import { i18nObject } from "@app/i18n/i18n-util"
+import {
+  Intraledger,
+  LightningLnURL,
+} from "@app/screens/send-bitcoin-screen/send-bitcoin-confirmation-screen.stories"
 import { ContextForScreen } from "./helper"
 
-it("SendScreen Confirmation", async () => {
-  const { findByLabelText } = render(
-    <ContextForScreen>
-      <Intraledger />
-    </ContextForScreen>,
-  )
+jest.mock("@app/graphql/generated", () => ({
+  ...jest.requireActual("@app/graphql/generated"),
+  useSendBitcoinConfirmationScreenQuery: jest.fn(() => ({
+    data: {
+      me: {
+        id: "mocked-user-id",
+        defaultAccount: {
+          id: "mocked-account-id",
+          wallets: [
+            {
+              id: "btc-wallet-id",
+              balance: 500000,
+              walletCurrency: "BTC",
+            },
+            {
+              id: "usd-wallet-id",
+              balance: 10000,
+              walletCurrency: "USD",
+            },
+          ],
+        },
+      },
+    },
+  })),
+}))
 
-  // it seems we need multiple act because the component re-render multiple times
-  // probably this could be debug with why-did-you-render
-  await act(
-    () =>
-      new Promise((resolve) => {
-        setTimeout(resolve, 10)
-      }),
-  )
+describe("SendBitcoinConfirmationScreen", () => {
+  let LL: ReturnType<typeof i18nObject>
 
-  const { children } = await findByLabelText("Successful Fee")
-  expect(children).toEqual(["₦0 ($0.00)"])
+  beforeEach(() => {
+    loadLocale("en")
+    LL = i18nObject("en")
+  })
+
+  it("Send Screen Confirmation - Intraledger Payment", async () => {
+    const { findByLabelText } = render(
+      <ContextForScreen>
+        <Intraledger />
+      </ContextForScreen>,
+    )
+
+    // it seems we need multiple act because the component re-render multiple times
+    // probably this could be debug with why-did-you-render
+    await act(
+      () =>
+        new Promise((resolve) => {
+          setTimeout(resolve, 10)
+        }),
+    )
+
+    const { children } = await findByLabelText("Successful Fee")
+    expect(children).toEqual(["₦0 ($0.00)"])
+  })
+
+  it("Send Screen Confirmation - Lightning lnurl Payment", async () => {
+    const lnurl = "lnurl1dp68gurn8ghj7mr..."
+
+    render(
+      <ContextForScreen>
+        <LightningLnURL />
+      </ContextForScreen>,
+    )
+
+    await act(
+      () =>
+        new Promise((resolve) => {
+          setTimeout(resolve, 10)
+        }),
+    )
+
+    expect(screen.getByText(lnurl)).toBeTruthy()
+    expect(screen.getByText("₦100 ($100.00)")).toBeTruthy()
+    expect(screen.getByTestId("slider")).toBeTruthy()
+    expect(LL.SendBitcoinConfirmationScreen.slideToConfirm()).toBeTruthy()
+  })
 })
--- app/screens/send-bitcoin-screen/send-bitcoin-confirmation-screen.stories.tsx
+++ app/screens/send-bitcoin-screen/send-bitcoin-confirmation-screen.stories.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { Satoshis } from "lnurl-pay/dist/types/types"
 
 import { MockedProvider } from "@apollo/client/testing"
 import { DisplayCurrency, toUsdMoneyAmount } from "@app/types/amounts"
@@ -11,6 +12,7 @@ import { IsAuthedContextProvider } from "../../graphql/is-authed-context"
 import mocks from "../../graphql/mocks"
 import { ConvertMoneyAmount } from "./payment-details/index.types"
 import * as PaymentDetails from "./payment-details/intraledger"
+import * as PaymentDetailsLightning from "./payment-details/lightning"
 import SendBitcoinConfirmationScreen from "./send-bitcoin-confirmation-screen"
 
 export default {
@@ -63,3 +65,62 @@ const route = {
 } as const
 
 export const Intraledger = () => <SendBitcoinConfirmationScreen route={route} />
+
+const successActionMessageMock = {
+  tag: "message",
+  message: "Thank you for your support.",
+  description: null,
+  url: null,
+  ciphertext: null,
+  iv: null,
+  decipher: () => null,
+}
+
+const lnUrlMock = {
+  callback: "https://example.com/lnurl/callback",
+  metadata: [["text/plain", "Pay to user@example.com"]],
+  min: 1000 as Satoshis,
+  max: 1000000 as Satoshis,
+  fixed: false,
+  metadataHash: "",
+  identifier: "user@example.com",
+  description: "Payment for services",
+  image: "https://example.com/image.png",
+  commentAllowed: 0,
+  rawData: {},
+}
+
+const defaultLightningParams: PaymentDetailsLightning.CreateLnurlPaymentDetailsParams<WalletCurrency> =
+  {
+    lnurl: "lnurl1dp68gurn8ghj7mr...",
+    lnurlParams: lnUrlMock,
+    paymentRequest: "lnbc1m1psh8d8zpp5qk3z7t...",
+    paymentRequestAmount: {
+      currency: "BTC",
+      currencyCode: "BTC",
+      amount: 10000,
+    },
+    unitOfAccountAmount: {
+      currency: "USD",
+      amount: 5.0,
+      currencyCode: "USD",
+    },
+    successAction: successActionMessageMock,
+    convertMoneyAmount: convertMoneyAmountMock,
+    sendingWalletDescriptor: btcSendingWalletDescriptor,
+  }
+
+const { createLnurlPaymentDetails } = PaymentDetailsLightning
+const paymentDetailLightning = createLnurlPaymentDetails(defaultLightningParams)
+
+const routeLightning = {
+  key: "sendBitcoinConfirmationScreen",
+  name: "sendBitcoinConfirmation",
+  params: {
+    paymentDetail: paymentDetailLightning,
+  },
+} as const
+
+export const LightningLnURL = () => (
+  <SendBitcoinConfirmationScreen route={routeLightning} />
+)
