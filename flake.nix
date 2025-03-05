feb2bacc4d42f58dde383aba6b8f43dab737bca2
Juan P Lopez <dolcalmi@gmail.com>
2025-03-04 22:37:47 -0500

    chore: update nixos stable version

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
     android.url = "github:tadfisher/android-nixpkgs";
     ruby-nix.url = "github:bobvanderlinden/nixpkgs-ruby";
