7ceb0fb4fa9938e00bf0c6b10eb0a3a6ad5da03f
Esaú Gómez <alfonsoesaugomez@gmail.com>
2025-03-19 11:15:49 -0600

    Revert "feat(send-bitcoin-completed): disable auto-redirect after successful payment"
    This reverts commit 7dee4afde67b98b4215e82900fa54e00ca07bee1.

--- app/screens/send-bitcoin-screen/send-bitcoin-completed-screen.tsx
+++ app/screens/send-bitcoin-screen/send-bitcoin-completed-screen.tsx
@@ -104,6 +104,7 @@ const SendBitcoinCompletedScreen: React.FC<Props> = ({ route }) => {
   }, [LL, client, appConfig])
 
   const FEEDBACK_DELAY = 3000
+  const CALLBACK_DELAY = 3000
   useEffect(() => {
     if (!feedbackModalShown) {
       const feedbackTimeout = setTimeout(() => {
@@ -113,7 +114,11 @@ const SendBitcoinCompletedScreen: React.FC<Props> = ({ route }) => {
         clearTimeout(feedbackTimeout)
       }
     }
-  }, [client, feedbackModalShown, LL, requestFeedback])
+    if (!showSuggestionModal) {
+      const navigateToHomeTimeout = setTimeout(navigation.popToTop, CALLBACK_DELAY)
+      return () => clearTimeout(navigateToHomeTimeout)
+    }
+  }, [client, feedbackModalShown, LL, showSuggestionModal, navigation, requestFeedback])
 
   const MainIcon = () => {
     switch (status) {
