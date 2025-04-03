b863415632344db19e9b8e1d366f8e33dfc6ef38
Esaú Gómez <alfonsoesaugomez@gmail.com>
2025-04-03 13:46:12 -0600

    test: settings screen initials tests

---
+++ __tests__/screens/settings-screen/settings.spec.tsx
@@ -0,0 +1,70 @@
+import React from "react"
+import { render, fireEvent, waitFor, screen } from "@testing-library/react-native"
+import { loadLocale } from "@app/i18n/i18n-util.sync"
+import { i18nObject } from "@app/i18n/i18n-util"
+import { SwitchAccountComponent } from "@app/screens/settings-screen/settings/swiitch-account.stories"
+import { GetUsernamesDocument } from "@app/graphql/generated"
+import KeyStoreWrapper from "@app/utils/storage/secureStorage"
+import mocks from "@app/graphql/mocks"
+import { ContextForScreen } from "../helper"
+
+jest.mock("@app/utils/storage/secureStorage", () => ({
+  KeyStoreWrapper: {
+    getSessionTokens: jest.fn().mockResolvedValue(["mock-token-1"]),
+    saveSessionToken: jest.fn(),
+    removeTokenFromSession: jest.fn(),
+  },
+}))
+
+const mocksWithUsername = [
+  ...mocks,
+  {
+    request: {
+      query: GetUsernamesDocument,
+    },
+    result: {
+      data: {
+        me: {
+          id: "70df9822-efe0-419c-b864-c9efa99872ea",
+          username: "test1",
+          phone: null,
+          __typename: "User",
+        },
+      },
+    },
+  },
+]
+
+/**
+ * TODO: FIX this suit test
+ */
+
+// eslint-disable-next-line jest/no-disabled-tests
+describe.skip("Settings", () => {
+  let LL: ReturnType<typeof i18nObject>
+
+  beforeEach(() => {
+    loadLocale("en")
+    LL = i18nObject("en")
+  })
+
+  it("SwitchAccount expands and shows loading, then user profiles", async () => {
+    render(
+      <ContextForScreen>
+        <SwitchAccountComponent mock={mocksWithUsername} />
+      </ContextForScreen>,
+    )
+
+    const switchBtn = screen.getByText(LL.AccountScreen.switchAccount())
+    fireEvent.press(switchBtn)
+
+    // The spinner should be displayed
+    expect(screen.getByTestId("ActivityIndicator")).toBeTruthy()
+
+    await waitFor(() => {
+      expect(screen.getByText("test1")).toBeTruthy()
+    })
+
+    expect(KeyStoreWrapper.getSessionTokens).toHaveBeenCalled()
+  })
+})
